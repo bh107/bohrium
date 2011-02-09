9aae8bd051de20cfd021ea2d4070f51aa3fd11b0
Mads R. B. Kristensen <madsbk@gmail.com>
2011-02-09 16:50:12 +0100

    Added an optional argument that specifies the Python interpreter to use when building the numpy bridge library.

--- bridge/numpy/build.sh
+++ bridge/numpy/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #Bash script for building the NumPy bridge.
 
-#set Environment variables.
+#Set Environment variables.
 CFLAGS="-I../../lib -I../../vm/svi -I../../util"
 LDFLAGS="-L../../lib -lcphvb -L../../vm/svi -lsvi -L../../util -lcphvbutil"
+
+#Set Python Interpreter.
+if [ $# = 1 ]
+then
+    PYTHON="$1"
+else
+    PYTHON="/usr/bin/python"
+fi
 
 #Change current directory to where numpy and this script is located.
 cd `dirname "$0"`
 
 #Call NumPy build script
-python setup.py build
+$PYTHON setup.py build
