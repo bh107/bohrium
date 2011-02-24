1732209e9ce88694f0fa2e6533d07ef1843935b2
Mads R. B. Kristensen <madsbk@gmail.com>
2011-02-24 11:29:16 +0100

    added cphvb_ve_simple.c.

---
+++ ve/simple/cphvb_ve_simple.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011 Mads R. B. Kristensen <madsbk@gmail.com>
+ *
+ * This file is part of cphVB.
+ *
+ * cphVB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cphVB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cphVB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cphvb_ve_simple.h>
+
+
+cphvb_error cphvb_ve_simple_init(void)
+{
+    return CPHVB_SUCCESS;
+}
+
+
+cphvb_error cphvb_ve_simple_execute(cphvb_int32 instruction_count,
+                                    char* instruction_list)
+{
+    return CPHVB_SUCCESS;
+}
+
+
+cphvb_error cphvb_ve_simple_shutdown(void)
+{
+    return CPHVB_SUCCESS;
+}
