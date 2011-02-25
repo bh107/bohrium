6f3287cb9217a603293f6797a8306b15ae1129be
Troels Blum <troels@blum.dk>
2011-02-25 18:46:56 +0100

    PTXparameter

---
+++ ve/cuda/PTXparameter.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2011 Troels Blum <troels@blum.dk>
+ *
+ * This file is part of cphVB.
+ *
+ * cphVB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cphVB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cphVB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PTXtype.hpp"
+#include "PTXparameter.hpp"
+
+PTXparameter::PTXparameter(cphvb_type type_, cphvb_constant value_) :
+    type(ptxType(type_)),
+    value(value_) {}
+
