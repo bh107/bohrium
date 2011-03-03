fd41196cf2e7b753185274a86b2a066e2ff88ac8
Mads R. B. Kristensen <madsbk@gmail.com>
2011-03-03 16:21:28 +0100

    The build script for the NumPy-Bridge now takes two arguments -- D to specify the VEM to use and C to specify the python interpreter to use.

--- bridge/numpy/build.sh
+++ bridge/numpy/build.sh
@@ -4,18 +4,35 @@
 #Change current directory to where numpy and this script is located.
 cd `dirname "$0"`
 
-#Set Environment variables.
+#Set the default environment variables.
 ROOT="../.."
 export CFLAGS="-I$ROOT/include"
-export LDFLAGS="-L$ROOT/core -lcphvb -L$ROOT/vem/node -lcphvb_vem_node -L$ROOT/ve/simple -lcphvb_ve_simple"
+export LDFLAGS="-L$ROOT/core -lcphvb $LDFLAGS"
+export LDFLAGS="-L$ROOT/vem/node -lcphvb_vem_node $LDFLAGS"
+export LDFLAGS="-L$ROOT/ve/simple -lcphvb_ve_simple $LDFLAGS"
+PYTHON="/usr/bin/python"
 
-#Set Python Interpreter.
-if [ $# = 1 ]
-then
-    PYTHON="$1"
-else
-    PYTHON="/usr/bin/python"
-fi
+while getopts "D:C:" opt; do
+  case $opt in
+    D)
+      HIGH=`echo $OPTARG  | tr '[a-z]' '[A-Z]'`
+      LOW=`echo $OPTARG  | tr '[A-Z]' '[a-z]'`
+      export CFLAGS="-D$HIGH $CFLAGS"
+      export LDFLAGS="-L$ROOT/vem/$LOW -lcphvb_vem_$LOW $LDFLAGS"
+      ;;
+    C)
+      PYTHON="$OPTARG"
+      ;;
+    \?)
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 
 #Call NumPy build script
 $PYTHON setup.py build
