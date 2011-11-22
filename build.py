1c4d4b12bd8a34defbbd7146eb03abed82bd82d1
Mads R. B. Kristensen <madsbk@gmail.com>
2011-11-22 14:41:18 +0100

    Added a new build script

---
+++ build.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+"""
+ * Copyright 2011 Mads R. B. Kristensen <madsbk@gmail.com>
+ *
+ * This file is part of CphVB <https://github.com/cphvb>.
+ *
+ * DistNumPy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DistNumPy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DistNumPy. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+import os
+from os.path import join
+import getopt
+import subprocess
+
+def build(name, dir, fatal=False):
+    print "***Building %s***"%name
+    try:
+        p = subprocess.Popen(["make"], cwd=join(install_dir, dir))
+        err = p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
+
+    if fatal:
+        if err != 0:
+            print "An build error in %s is fatal. Exiting."%name
+            sys.exit(-1)
+    else:
+        if err != 0:
+            print "An build error in %s is not fatal. Continuing."%name
+
+def clean(name, dir):
+    print "***Cleaning %s***"%name
+    try:
+        p = subprocess.Popen(["make", "clean"], cwd=join(install_dir, dir))
+        err = p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
+
+def install(name, dir, fatal=False):
+    print "***Installing %s***"%name
+    try:
+        p = subprocess.Popen(["make", "install"], cwd=join(install_dir, dir))
+        err = p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
+
+    if fatal:
+        if err != 0:
+            print "An build error in %s is fatal. Exiting."%name
+            sys.exit(-1)
+    else:
+        if err != 0:
+            print "An build error in %s is not fatal. Continuing."%name
+
+def uninstall(name, dir):
+    print "***Uninstalling %s***"%name
+    try:
+        p = subprocess.Popen(["make", "uninstall"], cwd=join(install_dir, dir))
+        err = p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
+
+def ldconfig():
+    print "***Configure ldconfig***"
+    print "sudo ldconfig"
+    try:
+        p = subprocess.Popen(["sudo", "ldconfig"], cwd=join(install_dir))
+        err = p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
+
+if __name__ == "__main__":
+    debug = False
+    prefix = "/usr/local/lib"
+    try:
+        install_dir = os.path.abspath(os.path.dirname(__file__))
+    except NameError:
+        print "The build script cannot run interactively."
+        sys.exit(-1)
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"d",["debug"])
+    except getopt.GetoptError, err:
+        print str(err)
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("-d", "--debug"):
+            debug = True
+        else:
+            assert False, "unhandled option"
+    try:
+        cmd = args[0]
+    except IndexError:
+        print "No command given"
+        sys.exit(-1)
+
+    if cmd == "build":
+        build("INIPARSER", "iniparser", True)
+        build("CORE", "core", True)
+        build("VE-CUDA", "ve/cuda", False)
+        build("VE-SIMPLE", "ve/simple", False)
+        build("VE-SCORE", "ve/score", True)
+        build("VE-MCORE", "ve/mcore", False)
+        build("VEM-NODE", "vem/node", True)
+        build("BRIDGE-NUMPY", "bridge/numpy", True)
+    elif cmd == "clean":
+        clean("INIPARSER", "iniparser")
+        clean("CORE", "core")
+        clean("VE-CUDA", "ve/cuda")
+        clean("VE-SIMPLE", "ve/simple")
+        clean("VE-SCORE", "ve/score")
+        clean("VE-MCORE", "ve/mcore")
+        clean("VEM-NODE", "vem/node")
+        clean("BRIDGE-NUMPY", "bridge/numpy")
+    elif cmd == "install":
+        install("INIPARSER", "iniparser", True)
+        install("CORE", "core", True)
+        install("VE-CUDA", "ve/cuda", False)
+        install("VE-SIMPLE", "ve/simple", True)
+        install("VE-SCORE", "ve/score",True)
+        install("VE-MCORE", "ve/mcore",False)
+        install("VEM-NODE", "vem/node", True)
+        install("BRIDGE-NUMPY", "bridge/numpy",True)
+        ldconfig()
+    elif cmd == "uninstall":
+        uninstall("INIPARSER", "iniparser")
+        uninstall("CORE", "core")
+        uninstall("VE-CUDA", "ve/cuda")
+        uninstall("VE-SIMPLE", "ve/simple")
+        uninstall("VE-SCORE", "ve/score")
+        uninstall("VE-MCORE", "ve/mcore")
+        uninstall("VEM-NODE", "vem/node")
+        uninstall("BRIDGE-NUMPY", "bridge/numpy")
