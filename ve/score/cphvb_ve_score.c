b2d1ab8d3269fef76c2e7e47bf41744483759716
Mads R. B. Kristensen <madsbk@gmail.com>
2011-12-12 10:51:14 +0100

    Implemented reduce in VE-SCORE

--- ve/score/cphvb_ve_score.c
+++ ve/score/cphvb_ve_score.c
@@ -77,6 +77,66 @@ cphvb_error cphvb_ve_score_reg_func(char *lib, char *fun, cphvb_intp *id) {
 //follows the function signature defined by cphvb_userfunc_impl.
 cphvb_error cphvb_reduce(cphvb_userfunc *arg)
 {
-    printf("cphvb_ve_score_reduce\n");
+    cphvb_reduce_type *a = (cphvb_reduce_type *) arg;
+    cphvb_instruction inst;
+    cphvb_intp i,j;
+    cphvb_index coord[CPHVB_MAXDIM];
+    memset(coord, 0, a->operand[1]->ndim * sizeof(cphvb_index));
+
+    if(cphvb_operands(a->opcode) != 3)
+    {
+        fprintf(stderr, "Reduce only support binary operations.\n");
+        exit(-1);
+    }
+
+    //Make sure that the array memory is allocated.
+    if(cphvb_malloc_array_data(a->operand[0]) != CPHVB_SUCCESS ||
+       cphvb_malloc_array_data(a->operand[1]) != CPHVB_SUCCESS)
+    {
+        return CPHVB_OUT_OF_MEMORY;
+    }
+
+    //We need a tmp copy of the arrays.
+    cphvb_array *out = a->operand[0];
+    cphvb_array *in  = a->operand[1];
+    cphvb_array tmp  = *in;
+    cphvb_intp step = in->stride[a->axis];
+    tmp.start = 0;
+    j=0;
+    for(i=0; i<in->ndim; ++i)//Remove the 'axis' dimension from in
+        if(i != a->axis)
+        {
+            tmp.shape[j] = in->shape[i];
+            tmp.stride[j] = in->stride[i];
+            ++j;
+        }
+    --tmp.ndim;
+
+    //We copy the first element to the output.
+    inst.status = CPHVB_INST_UNDONE;
+    inst.opcode = CPHVB_IDENTITY;
+    inst.operand[0] = out;
+    inst.operand[1] = &tmp;
+    cphvb_error err = dispatch(&inst);
+    if(err != CPHVB_SUCCESS)
+        return err;
+    tmp.start += step;
+
+    //Reduce over the 'axis' dimension.
+    //NB: the first element is already handled.
+    inst.status = CPHVB_INST_UNDONE;
+    inst.opcode = a->opcode;
+    inst.operand[0] = out;
+    inst.operand[1] = out;
+    inst.operand[2] = &tmp;
+    cphvb_intp axis_size = in->shape[a->axis];
+    for(i=1; i<axis_size; ++i)
+    {
+        cphvb_error err = dispatch(&inst);
+        if(err != CPHVB_SUCCESS)
+            return err;
+        tmp.start += step;
+    }
+
     return CPHVB_SUCCESS;
 }
