f31bca744a47d5dccd4dbf1e99a72b201ed168b3
Mads R. B. Kristensen <madsbk@gmail.com>
2012-02-15 14:24:18 +0100

    Added a bundler to the VE-SCORE

---
+++ ve/score/bundler.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2011 Simon A. F. Lund <safl@safl.dk>
+ *
+ * This file is part of cphVB.
+ *
+ * cphVB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cphVB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cphVB. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "bundler.hpp"
+
+/* Calculates the bundleable instructions.
+ *
+ * @inst The instruction list
+ * @size Size of the instruction list
+ * @return Number of consecutive bundeable instruction
+ */
+cphvb_intp bundle(cphvb_instruction insts[], cphvb_intp size)
+{
+    return 1;
+}
