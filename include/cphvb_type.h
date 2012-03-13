31d8a499a9b0702be720903de99eeed91f324352
Troels Blum <troels@blum.dk>
2012-03-13 14:48:19 +0100

    Corrected comment

--- include/cphvb_type.h
+++ include/cphvb_type.h
@@ -54,7 +54,7 @@ enum /* cphvb_type */
     CPHVB_FLOAT16,
     CPHVB_FLOAT32,
     CPHVB_FLOAT64,
-    CPHVB_INDEX, // Not a data type same as INT32 used for e.g. reduce dim
+    CPHVB_INDEX, // Not a data type. Used for e.g. reduce dim. Has the same size as a pointer on the architecture
     //NB: CPHVB_UNKNOWN must be the last element in this enum.
     CPHVB_UNKNOWN
 };
