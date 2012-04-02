43369b5b2f30009dcb0deac7b463156a91486d7c
Troels Blum <troels@blum.dk>
2012-04-02 11:16:56 +0200

    Modified for new random with dtype=

--- benchmark/MonteCarlo.py
+++ benchmark/MonteCarlo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cphvbnumpy as cnp
 import util
 
 B = util.Benchmark()
@@ -8,8 +9,9 @@ I = B.size[1]
 B.start()
 sum=0.0
 for i in xrange(I):
-    x = np.random.random([N], cphvb=B.cphvb)
-    y = np.random.random([N], cphvb=B.cphvb)
+    cnp.flush();
+    x = np.random.random([N], dtype=B.dtype, cphvb=B.cphvb)
+    y = np.random.random([N], dtype=B.dtype, cphvb=B.cphvb)
     np.square(x,x)
     np.square(y,y)
     np.add(x,y,x)
@@ -17,6 +19,6 @@ for i in xrange(I):
     sum += np.add.reduce(z)*4.0/N
 sum /= I
 B.stop()
-
+print "Pi: ", sum
 B.pprint()
 
