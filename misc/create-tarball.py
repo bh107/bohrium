55e15f0b2b6ad03334b316684b30dfe6c5b52e30
Mads R. B. Kristensen <madsbk@gmail.com>
2012-04-10 11:12:32 +0200

    Added a script that creates a tarball of HEAD that includes the NumPy Bridge

---
+++ misc/create-tarball.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+"""
+ * Copyright 2012 Mads R. B. Kristensen <madsbk@gmail.com>
+ *
+ * This file is part of CphVB <https://github.com/cphvb>.
+ *
+ * DistNumPy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DistNumPy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DistNumPy. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+import subprocess
+
+
+if __name__ == "__main__":
+    try:
+        (root,_) = os.path.split(os.path.abspath(os.path.dirname(__file__)))
+    except NameError:
+        print "This script cannot run interactively."
+        sys.exit(-1)
+    print root
+
+    VERSION = "v0.1"
+    NAME = "cphvb-%s"%VERSION
+
+    try:
+        p = subprocess.Popen(["git", "archive", "--format=tar", "--prefix=cphvb-dump/", "-o", "/tmp/cphvb-root-dump.tar", "HEAD"], cwd=root)
+        p.wait()
+        p = subprocess.Popen(["git", "archive", "--format=tar", "--prefix=cphvb-numpy-dump/", "-o", "/tmp/cphvb-numpy-dump.tar", "HEAD"], cwd=os.path.join(root,"bridge","numpy"))
+        p.wait()
+        p = subprocess.Popen(["tar", "-xf", "cphvb-root-dump.tar"], cwd="/tmp")
+        p.wait()
+        p = subprocess.Popen(["tar", "-xf", "cphvb-numpy-dump.tar"], cwd="/tmp")
+        p.wait()
+        p = subprocess.Popen(["rmdir", "cphvb-dump/bridge/numpy"], cwd="/tmp")
+        p.wait()
+        p = subprocess.Popen(["mv", "cphvb-numpy-dump","cphvb-dump/bridge/numpy"], cwd="/tmp")
+        p.wait()
+        p = subprocess.Popen(["mv", "cphvb-dump",NAME], cwd="/tmp")
+        p.wait()
+        p = subprocess.Popen(["tar", "-czf", "%s.tgz"%NAME, NAME], cwd="/tmp")
+        p.wait()
+        p = subprocess.Popen(["rm", "-R", "cphvb-root-dump.tar", "cphvb-numpy-dump.tar", NAME], cwd="/tmp")
+        p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
