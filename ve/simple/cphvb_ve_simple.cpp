301c63332dac60310172f5876e362c93c1745c58
Simon A. F. Lund <safl@safl.dk>
2012-05-05 16:09:18 +0200

    Deprecating codebase for current simple-ve.

---
+++ ve/simple/cphvb_ve_simple.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2011 Simon A. F. Lund <safl@safl.dk>
+ *
+ * This file is part of cphVB.
+ *
+ * cphVB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cphVB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cphVB. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <cphvb.h>
+#include "cphvb_ve_simple.h"
+
+static cphvb_com *myself = NULL;
+static cphvb_userfunc_impl reduce_impl = NULL;
+static cphvb_intp reduce_impl_id = 0;
+static cphvb_userfunc_impl random_impl = NULL;
+static cphvb_intp random_impl_id = 0;
+
+cphvb_error cphvb_ve_simple_init(cphvb_com *self)
+{
+    myself = self;
+    return CPHVB_SUCCESS;
+}
+
+cphvb_error cphvb_ve_simple_execute( cphvb_intp instruction_count, cphvb_instruction* instruction_list )
+{
+    cphvb_intp count, nops, i;
+    cphvb_instruction* inst;
+    cphvb_error ret = CPHVB_SUCCESS;
+
+    for(count=0; count < instruction_count; count++)
+    {
+        inst = &instruction_list[count];
+
+        if(inst->status == CPHVB_INST_DONE)     // SKIP instruction
+        {
+            continue;
+        }
+
+        nops = cphvb_operands(inst->opcode);    // Allocate memory for operands
+        for(i=0; i<nops; i++)
+        {
+            if (!cphvb_is_constant(inst->operand[i]))
+            {
+                if (cphvb_data_malloc(inst->operand[i]) != CPHVB_SUCCESS)
+                {
+                    return CPHVB_OUT_OF_MEMORY; // EXIT
+                }
+            }
+
+        }
+
+        switch(inst->opcode)                    // Dispatch instruction
+        {
+            case CPHVB_NONE:                    // NOOP.
+            case CPHVB_DISCARD:
+            case CPHVB_SYNC:
+                inst->status = CPHVB_INST_DONE;
+                break;
+
+            case CPHVB_USERFUNC:                // External libraries
+
+                if(inst->userfunc->id == reduce_impl_id)
+                {
+                    ret = reduce_impl(inst->userfunc, NULL);
+                    inst->status = (ret == CPHVB_SUCCESS) ? CPHVB_INST_DONE : CPHVB_INST_UNDONE;
+                }
+                else if(inst->userfunc->id == random_impl_id)
+                {
+                    ret = random_impl(inst->userfunc, NULL);
+                    inst->status = (ret == CPHVB_SUCCESS) ? CPHVB_INST_DONE : CPHVB_INST_UNDONE;
+                }
+                else                            // Unsupported userfunc
+                {
+                    ret = CPHVB_TYPE_NOT_SUPPORTED;
+                }
+
+                break;
+
+            default:                            // Built-in operations
+                ret = cphvb_compute_apply( inst );
+                inst->status = (ret == CPHVB_SUCCESS) ? CPHVB_INST_DONE : CPHVB_INST_UNDONE;
+        }
+
+        if (inst->status != CPHVB_INST_DONE)    // Instruction failed
+        {
+            return ret;                         // EXIT
+        }
+
+    }
+                                                // All instructions succeeded.
+    return ret;                                 // EXIT
+
+}
+
+cphvb_error cphvb_ve_simple_shutdown( void )
+{
+    return CPHVB_SUCCESS;
+}
+
+cphvb_error cphvb_ve_simple_reg_func(char *lib, char *fun, cphvb_intp *id) {
+
+    if(strcmp("cphvb_reduce", fun) && reduce_impl == NULL)
+    {
+        cphvb_com_get_func(myself, lib, fun, &reduce_impl);
+        reduce_impl_id = *id;
+    }
+    else if(strcmp("cphvb_random", fun) && random_impl == NULL)
+    {
+        cphvb_com_get_func(myself, lib, fun, &random_impl);
+        random_impl_id = *id;
+    }
+    return CPHVB_SUCCESS;
+}
+
+cphvb_error cphvb_reduce( cphvb_userfunc *arg, void* ve_arg)
+{
+    return cphvb_compute_reduce( arg, ve_arg );
+}
+
+cphvb_error cphvb_random( cphvb_userfunc *arg, void* ve_arg)
+{
+    return cphvb_compute_random( arg, ve_arg );
+}
