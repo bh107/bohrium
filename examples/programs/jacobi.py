5a9e02c1fdd44c479ec4b089f90fbd16a9313357
Troels Blum <troels@blum.dk>
2012-05-09 14:02:07 +0200

    more efficient version. without np.dot()

--- examples/programs/jacobi.py
+++ examples/programs/jacobi.py
@@ -6,14 +6,14 @@ import numpy.linalg as nla
 def jacobi(A, b, tol=0.0005):
     # solve Ax=b via the Jacobi method
     x = cnp.ones(np.shape(b), dtype=b.dtype, cphvb=b.cphvb)
-    D = cnp.diag(1/cnp.diag(A))
+    D = 1/cnp.diag(A)
     R = cnp.diag(cnp.diag(A)) - A
-    T = cla.dot(D,R)
-    C = cla.dot(D,b)
+    T = D[:,np.newaxis]*R
+    C = D*b
     error = tol + 1
     while error > tol:
         xo = x
-        x = cla.dot(T,x) + C
+        x = np.add.reduce(T*x,-1) + C
         error = nla.norm(x-xo)
     return x
 
