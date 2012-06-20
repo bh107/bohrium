8fcef35b379e4dfce61b73e0f60c38a2ef456bae
Mads R. B. Kristensen <madsbk@gmail.com>
2012-06-20 11:27:59 +0200

    The numpy test will now test all opcodes

--- test/numpy/test_primitives.py
+++ test/numpy/test_primitives.py
@@ -1,6 +1,32 @@
 import cphvbnumpy as np
 from numpytest import numpytest,gen_views,TYPES
 import random
+import os
+import json
+import re
+
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return ""
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
+
+def load_opcodes():
+    script_dir = os.path.abspath(os.path.dirname(__file__))
+    file_path  = os.path.join(script_dir,'..','..','core','codegen','opcodes.json')
+    f = open(file_path) 
+    ret = json.loads(comment_remover(f.read()))
+    f.close()
+    return ret
+
+ 
 
 
 class test_ufunc(numpytest):
@@ -8,80 +34,27 @@ class test_ufunc(numpytest):
     def __init__(self):
         numpytest.__init__(self)
         self.config['maxerror'] = 0.00001
-        self.config['dtypes'] = TYPES.NORMAL 
+        self.ops = load_opcodes()
+        
 
     def test_ufunc(self):
-        fun = [np.add,\
-            np.subtract,\
-            np.divide,\
-#           np.true_divide,\ #Not Supported by cphVB
-#           np.floor_divide,\#Not Supported by cphVB
-            np.multiply,\
-            np.greater,\
-            np.greater_equal,\
-            np.less,\
-            np.less_equal,\
-            np.not_equal,\
-            np.equal,\
-            np.logical_and,\
-            np.logical_or,\
-#           np.logical_xor,\ #Error in simple VE
-            np.logical_not,\
-            np.maximum,\
-            np.minimum,\
-            np.rint,\
-            np.sign,\
-#           np.conj,\        #Not Supported by cphVB
-            np.log,\
-            np.log2,\
-            np.log10,\
-            np.log1p,\
-            np.sqrt,\
-            np.square,\
-            np.reciprocal,\
-            np.hypot,\
-#           np.isfinite,\    #Not Supported by cphVB
-#           np.isinf,\       #Not Supported by cphVB
-#           np.isnan,\       #Not Supported by cphVB
-            np.signbit,\
-            np.floor,\
-            np.ceil,\
-            np.trunc,\
-            np.negative,\
-#           np.modf,\        #Not Supported by cphVB
-            ]
-        for f in fun:
-            A = self.array(100)  
-            cmd = "A = array(%s); np.%s(A,A)"%(A.shape,str(f)[8:-2])
-            #print cmd
-            yield (f(A,A),cmd)
+        for name in self.ops:
+            op = self.ops[name]
+            if op['system_opcode'] or name in ["CPHVB_IDENTITY","CPHVB_LDEXP","CPHVB_POWER","CPHVB_ARCSIN"]:#Ignore system opcodes
+                continue
+            fname = "np.%s"%name[6:].lower()
+            for in_type in op['types']:
+                for out_type in op['types'][in_type]:
+                    in_t  = "np.%s"%in_type[6:].lower() if in_type != "CPHVB_BOOL" else "bool"
+                    out_t = "np.%s"%out_type[6:].lower()if out_type != "CPHVB_BOOL" else "bool"
+                    cmd = "out = self.array((100),dtype=%s);"%(out_t)
+                    for i in xrange(1,op['nop']):
+                        cmd  += "in%d = self.array((100),dtype=%s);"%(i,in_t)
+                    cmd += "%s("%(fname)
+                    for i in xrange(1,op['nop']):
+                        cmd += "in%d,"%(i)
+                    cmd += "out)"
+                    print cmd
+                    exec cmd 
+                    yield (out,cmd)
 
-
-#Only float  
-#np.ldexp,\
-
-#Very Small Input
-"""
-            np.exp,\
-            np.exp2,\
-            np.expm1,\
-"""
-
-#Speciel math
-"""
-            np.arcsinh,\
-            np.arccosh,\
-            np.arctanh,\
-            np.sin,\
-            np.cos,\
-            np.tan,\
-            np.sinh,\
-            np.cosh,\
-            np.tanh,\
-            np.arcsin,\
-            np.arccos,\
-            np.arctan,\
-            np.arctan2,\
-            np.deg2rad,\
-            np.rad2deg,\
-"""
