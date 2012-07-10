887f862402527bd694f18b09ac8db5d65fe1a208
Mads R. B. Kristensen <madsbk@gmail.com>
2012-07-10 16:22:07 +0200

    Added test of reduce to the NumPy test suite

---
+++ test/numpy/test_reduce.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numpytest import numpytest,gen_views,TYPES
+
+
+class test_reduce(numpytest):
+    
+    def init(self):
+        for v in gen_views(3,64,12):
+            a = {}
+            self.axis = 0
+            exec v
+            yield (a,v)
+            for axis in xrange(1,a[0].ndim):
+                exec v
+                self.axis = axis
+                yield (a,v)
+                
+    def test_reduce(self,a):
+        cmd = "res = np.add.reduce(a[0],axis=%d)"%self.axis
+        exec cmd
+        return (res,cmd)
+
