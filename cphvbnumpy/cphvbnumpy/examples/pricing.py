5c10243aac7d3696861d9feddc2bbca089d9442a
Simon A. F. Lund <safl@safl.dk>
2012-09-14 15:52:54 +0200

    Numpyfication of american option-pricing code with a off-by-one error somewhere.

--- cphvbnumpy/cphvbnumpy/examples/pricing.py
+++ cphvbnumpy/cphvbnumpy/examples/pricing.py
@@ -31,26 +31,31 @@ for (i in n:1) {
 print(put[1,1])
 """
 import cphvbnumpy as np
+import math
 
-S0  = 100
+S0  = 100.0         # Pricing parameters
 r   = 0.03
 alpha   = 0.07
 sigma   = 0.20
-expiry  = 1
-strike  = 1
+expiry  = 1.0
+strike  = 100.0
 
-n   = expiry * 252
+n   = int(expiry * 252)
 dt  = expiry / n
-u   = np.exp( alpha*dt + sigma*np.sqrt(dt) )
-d   = np.exp( alpha*dt - sigma*np.sqrt(dt) )
-R   = np.exp( r*dt )
+u   = math.exp( alpha*dt + sigma*math.sqrt(dt) )
+d   = math.exp( alpha*dt - sigma*math.sqrt(dt) )
+R   = math.exp( r*dt )
 q   = (R-d)/(u-d)
 
-put = np.array( (n+1, n+1) )
-# put[,n+1] = ...
+asc     = np.array( range(0, n+1) )
+desc    = np.array( range(n,-1,-1) )
 
-for i in xrange(0, n):
-    for j in xrange(1, i):
-        S = S0*u*(j-1)*d^(i-j)
-        put[j][j] = max(max(strike-S,0), (q*put[j+1,][i+1]+(1-q)*put[j][i+1])/R)
+put = np.zeros( (n+1, n+1) )
+put[:,n] = np.maximum( strike - S0* (u**asc) * (d**desc), 0 )
 
+for i in xrange(n-1, -1, -1):
+    for j in xrange(0, i+1):
+        S = S0*u*(j-1)* (d**(i-j))
+        put[j,i] = max(max(strike-S, 0.0), (q*put[j+1,i+1]+(1-q) * put[j,i+1])/R)
+
+print put[0,0], put[1,1]
