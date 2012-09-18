c59c0f8eba41c5ebbc3e8e925754f851edbd60cd
Mads R. B. Kristensen <madsbk@gmail.com>
2012-09-18 14:52:15 +0200

    Now mapping supports multiple processes

--- vem/cluster/mapping.py
+++ vem/cluster/mapping.py
@@ -40,11 +40,12 @@ class array:
         return out
 
 
-NPROC = 2
+NPROC = 5
 
 def find_largest_chuck(stride, dims, offset, max_dims, d=0):
     totalsize = reduce(mul,base.dim)
     localsize = totalsize / NPROC
+    dims = list(dims)
 
     #Find a the largest possible dimension size
     while 1:
@@ -76,19 +77,48 @@ def find_largest_chuck(stride, dims, offset, max_dims, d=0):
         return dims
 
 
-def local_array(ary, rank=0, offset=-1):
+
+def local_array(ary, dim_offset=None):
+
+    if dim_offset is None:
+        dim_offset = [0]*len(ary.dim)
+    
+    rank = 0
+    incomplete_dim = 0
     totalsize = reduce(mul,ary.base.dim)
     localsize = totalsize / NPROC
 
-    #TODO: sort strides and dim
-
-    if offset == -1:
-        offset = ary.offset
+    #Find the least significant dimension not completely included in the last chuck
+    for d in xrange(len(ary.dim)-1,-1,-1):
+        if dim_offset[d] != 0 and dim_offset[d] != ary.dim[d]:
+            incomplete_dim = d
+            break
     
-    dim = find_largest_chuck(list(ary.stride),list(ary.dim),offset,list(ary.dim))
-    if dim == None:
-        return []    
+    #Compute the global offset based on the dimension offset
+    offset = ary.offset
+    for d in xrange(len(ary.dim)):
+        offset += dim_offset[d] * ary.stride[d]
+ 
+    #Find the rank
+    rank = offset / localsize
+    #Convert to local offset
+    offset = offset % localsize
 
+    #Find maximum dimension sizes
+    max_dim = [1]*len(ary.dim)
+    max_dim[incomplete_dim] = ary.dim[incomplete_dim] - dim_offset[incomplete_dim]
+    for d in xrange(incomplete_dim+1,len(max_dim)):
+        max_dim[d] = ary.dim[d]
+
+    assert reduce(mul,max_dim) > 0
+    
+    #Find largest chuck
+    dim = find_largest_chuck(ary.stride,max_dim,offset,max_dim,incomplete_dim)
+    if dim == None:
+        assert False
+
+    assert reduce(mul,dim) > 0
+ 
     A = array()
     A.rank = rank
     A.offset = offset
@@ -96,70 +126,44 @@ def local_array(ary, rank=0, offset=-1):
     A.base = ary.base
     A.dim = dim
 
-    if reduce(mul,A.dim) == reduce(mul,ary.dim):
-        return [A]
-
     #Find the least significant dimension not completely included in the last chuck
-    incomplete_dim = 0
     for d in xrange(len(ary.dim)-1,-1,-1):
-        if A.dim[d] != ary.dim[d]:
+        if dim[d] != ary.dim[d]:
             incomplete_dim = d
             break
-    
-    offset += A.dim[incomplete_dim] * ary.stride[incomplete_dim]
-    return [A] + local_array(ary, rank, offset)
 
-def local_array2(ary, pre):
-    totalsize = reduce(mul,base.dim)
-    localsize = totalsize / NPROC
+    #Update the dimension offsets
+    for d in xrange(incomplete_dim,-1,-1):
+        dim_offset[d] += dim[d]
+        if dim_offset[d] >= ary.dim[d]:
+            dim_offset[d] = 0
+            if d == 0:
+                print "EXIT - dim_offset: %s, max_dim: %s, dim: %s"%(dim_offset, max_dim, dim)
+                return [A]
+        else:
+            break 
 
-    ret = []
-    incomplete_dim = 0
-    for d in xrange(len(ary.dim)-1,-1,-1):
-        if pre.dim[d] != ary.dim[d]:
-            incomplete_dim = d
-            offset = pre.offset + pre.dim[incomplete_dim] * pre.stride[incomplete_dim] - localsize
-            
-            max_dim = [1]*len(ary.dim)
-            max_dim[incomplete_dim] = ary.dim[incomplete_dim] - pre.dim[incomplete_dim]
-            for d in xrange(incomplete_dim+1,len(max_dim)):
-                max_dim[d] = ary.dim[d]
+    print "dim_offset: %s, max_dim: %s, dim: %s, offset: %d"%(dim_offset, max_dim, dim, offset)
 
-            dim = find_largest_chuck(list(ary.stride),list(max_dim),offset,max_dim,incomplete_dim)
-            print dim, offset
-            
-            A = array()
-            A.rank = 1
-            A.offset = offset
-            A.stride = ary.stride
-            A.base = ary.base
-            A.dim = dim
-            ret.append(A)
+    return [A] + local_array(ary, dim_offset)
 
-    return ret
     
     
 base = array()
 base.offset = 0
 base.base = None
-base.dim = [36]
+base.dim = [32]
 base.stride = [1]
-
 
 A = array()
 A.base = base
-A.dim = [2,2,2]
-A.offset = 1
-A.stride = [14,3,1]
+A.dim = [4,2,2]
+A.offset = 3
+A.stride = [6,3,1]
 
-print "RANK 0:"
+
 ret = local_array(A)
 for i in xrange(len(ret)):
     print "chunk:  %d"%i
     print ret[i].pprint()
 
-print "RANK 1:"
-ret = local_array2(A,ret[-1])
-for i in xrange(len(ret)):
-    print "chunk:  %d"%i
-    print ret[i].pprint()
