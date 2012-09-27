e6ff04c757018db12335983faf81aae2563ce631
Simon A. F. Lund <safl@safl.dk>
2012-09-27 14:49:05 +0200

    Changed output format of press.py. Modified gen.diagrams.py to handle changes.

--- doc/benchpress/gen.diagrams.py
+++ doc/benchpress/gen.diagrams.py
@@ -6,57 +6,43 @@ from pylab import *
 import json
 import sys
 
+def stats( times ):
+    """ Returns: (avg, lowest, highest, deviation)"""
+    
+    return (sum(times)/float(len(times)), max(times), min(times), 0.0)
+
 def main( argv ):
 
     output = 'gfx'
 
-    data = json.load(open(argv[1]))
+    raw     = json.load(open(argv[1]))
+    meta    = raw['meta']
+    runs    = raw['runs']
 
-    bench = {}
     bases = {}
-    names = {
-        'jacobi_fixed': 'Jacobi Solver', 
-        'kNN':          'kNN', 
-        'stencil':      'Synthetic Stencil',
-        'swater':       'Shallow Water'
-    }
-                                                    # Sort out the best numpy time from the runs
-    for mark, cphvb, engine, run, time in data:
+    bench = {}
+    for mark, engine_lbl, engine, engine_args, cmd, times in runs:
 
-        if cphvb:       # skip times from cphVB
-            continue
+        t_avg, t_max, t_min, t_dev = stats(times)
+        if engine:                                  # Results with cphvb enabled.
 
-        mark = mark.split('.')[0]
-        if mark in bases:
-            if time < bases[mark]:
-                bases[mark] = time
-        else:
-            bases[mark] = time
-
-    for mark, cphvb, engine, run, time in data:     # Sort out the best cphvb time from the runs
-
-        if not cphvb:   # skip times from numpy
-            continue
-
-        mark = mark.split('.')[0]
-        if mark in bench:
-            engine = engine if cphvb else 'numpy'
-            if engine in bench[mark]:
-                if time < bench[mark][engine]:
-                    bench[mark][engine] = time
+            if mark in bench:
+                bench[mark][engine_lbl] = t_avg
             else:
-                bench[mark][engine] = time
-        else:
-            bench[mark] = {engine: time }
+                bench[mark] = { engine_lbl: t_avg }
+
+        else:                                       # Without cphvb = baseline.
+            bases[mark] = t_avg
 
     for mark in bench:
-                                                        # Runtime in relation to NumPy
-        rt = [(engine, 1/(bases[mark]/bench[mark][engine])) for engine in (bench[mark]) ] 
+                                                    # Runtime in relation to baseline
+        rt = [(engine_lbl, 1/(bases[mark]/bench[mark][engine_lbl])) for engine_lbl in (bench[mark]) ] 
         rt.sort()
         rt = rt[::-1]
         rt = [('numpy', bases[mark]/bases[mark])] + rt
-                                                        # Speed-up in relation to NumPy
-        su = [(engine, (bases[mark]/bench[mark][engine])) for engine in (bench[mark]) ] 
+
+                                                    # Speed-up in relation to baseline
+        su = [(engine_lbl, (bases[mark]/bench[mark][engine_lbl])) for engine_lbl in (bench[mark]) ] 
         su.sort()
         su = su[::-1]
         su = [('numpy', bases[mark]/bases[mark])] + su
@@ -68,8 +54,8 @@ def main( argv ):
 
         for graph, data in graphs:
 
-            lbl = [engine for engine, time in data]
-            val = [time for engine, time in data]
+            lbl = [engine_lbl for engine_lbl, time in data]
+            val = [time for engine_lbl, time in data]
             pos = arange(len(val))
 
             figure(1)
@@ -78,8 +64,7 @@ def main( argv ):
             ylabel('%s in relation to NumPy' % graph)
             xticks(pos, lbl)
             xlabel('Vector Engine')
-            name = names[mark] if mark in names else mark
-            title(name)
+            title(mark)
             grid(True)
             
             savefig("%s/%s_%s.pdf" % ( output, mark.lower(), graph.lower() ))
--- doc/benchpress/press.py
+++ doc/benchpress/press.py
@@ -1,87 +1,135 @@
 #!/usr/bin/env python
 from ConfigParser import SafeConfigParser
 from subprocess import Popen, PIPE
+from datetime import datetime
 import tempfile
 import json
 import os
 
+def meta():
+
+    return {
+        'date': str(datetime.now()),
+        'cpu':  open('/proc/cpuinfo','r').read(),
+        'os':   open('/proc/version','r').read()
+    }
+
 def main(config):
 
-    script_path= '../../benchmark/Python/'
-    out_path='./results'
+    script_path = '../../benchmark/Python/'.replace('/', os.sep)
+    out_path    ='./results'
 
-    parser = SafeConfigParser()
-    parser.read(config)
-
+    # Engines with various parameter setups
+    # (alias, runs, engine, env-vars)
+    runs    = 5
     engines = [
-        ('simple', 'numpy',         False,3, None),
-        ('simple', 'simple',        True, 3, None),
-        ('score',  'score_1',       True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"1"}),
-        ('score',  'score_16',      True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"32"}),
-        ('score',  'score_64',      True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"64"}),
-        ('score',  'score_512',     True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"512"}),
-        ('score',  'score_1024',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"1024"}),
-        ('score',  'score_2048',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"2048"}),
-        ('score',  'score_4096',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"2048"}),
-        ('score',  'score_8192',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"8192"}),
-        ('score',  'score_16384',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"16384"}),
-        ('score',  'score_32768',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"32768"}),
-        ('score',  'score_65536',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"65536"}),
-        ('score',  'score_131072',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"131072"}),
-        ('score',  'score_262144',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"262144"}),
-        ('score',  'score_524288',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"524288"}),
-        ('score',  'score_1048576',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"1048576"}),
-        ('score',  'score_2097152',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"2097152"}),
-        ('score',  'score_4194304',    True, 3, {"CPHVB_VE_SCORE_BLOCKSIZE":"4194304"}),
+        ('numpy',       runs,  None,        None),
+        ('simple',      runs,  'simple',    None),
+        ('score',       runs,  'score', None),
+        ('mcore',       runs,  'mcore', None),
+
+        ('score_1',     runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"1"}),
+        ('score_2',     runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"2"}),
+        ('score_4',     runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"4"}),
+        ('score_16',     runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"16"}),
+        ('score_32',    runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"32"}),
+        ('score_64',    runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"64"}),
+        ('score_512',   runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"512"}),
+        ('score_1024',  runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"1024"}),
+        ('score_2048',  runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"2048"}),
+        ('score_4096',  runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"4096"}),
+        ('score_8192',  runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"8192"}),
+        ('score_16384', runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"16384"}),
+        ('score_32768', runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"32768"}),
+        ('score_65536', runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"65536"}),
+        ('score_131072',    runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"131072"}),
+        ('score_262144',    runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"262144"}),
+        ('score_524288',    runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"524288"}),
+        ('score_1048576',   runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"1048576"}),
+        ('score_2097152',   runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"2097152"}),
+        ('score_4194304',   runs,  'score',    {"CPHVB_VE_SCORE_BLOCKSIZE":"4194304"}),
     ]
-    bench   = [
-        ('simplest.py',     '--size=100000000*1'),
-        ('cache.py',        '--size=10485760*10*1'),
-        ('jacobi_fixed.py', '--size=7168*7168*4'),
-        ('MonteCarlo.py',   '--size=100000000*1'),
-        ('swater.py',       '--size=3600*1'),
-        ('stencil.py',      '--size=10240*1024*10'),
-        ('twonine.py',      '--size=10240*1024*10'),
-        ('kNN.py',          '--size=10000*120')
+
+    # Scripts and their arguments
+    # (alias, script, parameters)
+    scripts   = [
+        ('Jacobi Fixed',    'jacobi_fixed.py', '--size=7168*7168*4'),
+        ('Monte Carlo',     'MonteCarlo.py',   '--size=100000000*1'),
+        ('Shallow Water',   'swater.py',       '--size=2200*1'),
+        ('kNN',             'kNN.py',          '--size=10000*120'),
+        ('Stencil Synth',   'stencil.py',      '--size=10240*1024*10'),
+
+        ('Cache Synth',     'cache.py',        '--size=10485760*10*1'),
+        ('Stencil Synth2',  'twonine.py',      '--size=10240*1024*10'),
+        ('Stencil Synth3',  'simplest.py',     '--size=100000000*1')
     ]
     
-    run     = [0]
-    using   = [0,1,2,3]
-    #run = [0,2,3,4]
+    benchmark = {                   # Define a benchmark which runs
+        'scripts': [7],             # these scripts
+        'engines': [12]             # using these engines
+    }   
+                                    # DEFAULT BENCHMARK
+    benchmark = {                   # Define a benchmark which runs
+        'scripts': [0,1,2,3,4],     # these scripts
+        'engines': [0,1,2,3]        # using these engines
+    } 
 
-    # Not running monte-carlo since it is cphvb_reduce cannot currently handle
-    # the mixed type operation which it generates.
+    parser = SafeConfigParser()     # Parser to modify the cphvb configuration file.
+    parser.read(config)             # Read current configuration
 
-    times = []
+    results = {
+        'meta': meta(),
+        'runs': []
+    }
+    with tempfile.NamedTemporaryFile(delete=False, dir=out_path, prefix='benchmark-') as fd:
+        print "Benchmarks are written to: %s." % fd.name
+        for mark, script, arg in (scripts[snr] for snr in benchmark['scripts']):
+            for alias, runs, engine, env in (engines[enr] for enr in benchmark['engines']):
 
-    for r in run:
-        for engine, engine_str, cphvb, runs, env in engines:
-            parser.set("node", "children", engine)
-            parser.write(open(config, 'wb'))
+                cphvb = False
+                if engine:                                  # Enable cphvb with the given engine.
+                    cphvb = True
+                    parser.set("node", "children", engine)  
+                    parser.write(open(config, 'wb'))
 
-            envs = None
-            if env:
-                envs = os.environ.copy()
-                envs.update(env)
+                envs = None                                 # Populate environment variables
+                if env:
+                    envs = os.environ.copy()
+                    envs.update(env)
+                                                            # Setup process + arguments
+                args        = ['python', script_path + script, arg, '--cphvb=%s' % cphvb ]
+                args_str    = ' '.join(args)
+                print "{ %s - %s ( %s ),\n  %s" % ( mark, alias, engine, args_str )
 
-            args = ['python', script_path+bench[r][0], bench[r][1], '--cphvb=%s' % cphvb ]
-            print '-{[',engine,',', cphvb,',',' '.join(args[1:]), '.'
-            for i in xrange(1,runs+1):
-                p = Popen(
-                    args,
-                    stdin=PIPE,
-                    stdout=PIPE,
-                    env=envs
-                )
-                out, err = p.communicate()
-                print "RUN",i, out, err, out.split(' ')[-1]
-                text = ' '.join(bench[r])
-                times.append( (text, cphvb, engine, i, float(out.split(' ')[-1] .rstrip()) ))
+                times = []
+                for i in xrange(1, runs+1):
 
-            print "]}-"
+                    p = Popen(                              # Run the command
+                        args,
+                        stdin=PIPE,
+                        stdout=PIPE,
+                        env=envs
+                    )
+                    out, err = p.communicate()              # Grab the output
+                    elapsed = 0.0
+                    if err:
+                        print "ERR: Something went wrong %s" % err
+                    else:
+                        elapsed = float(out.split(' ')[-1] .rstrip())
 
-    f = tempfile.NamedTemporaryFile(delete=False, dir=out_path, prefix='benchmark-')
-    json.dump(times, f, indent=4)
+                    print "  %d/%d, " % (i, runs), elapsed
+                    
+                    times.append( elapsed )
+
+                print "}"
+                                                            # Accumulate results
+                results['runs'].append(( mark, alias, engine, env, args_str, times ))
+
+                fd.truncate(0)                              # Store the results in a file...
+                fd.seek(0)
+                fd.write(json.dumps(results, indent=4))
+                fd.flush()
+                os.fsync(fd)
 
 if __name__ == "__main__":
     main(os.getenv('HOME')+os.sep+'.cphvb'+os.sep+'config.ini')
