64fea8fda99545bcfce09b5144e4a5e8e75a786c
Simon A. F. Lund <safl@safl.dk>
2012-10-14 21:08:41 +0200

    Revert changes to benchmarks.

--- benchmark/Python/MonteCarlo.py
+++ benchmark/Python/MonteCarlo.py
@@ -14,20 +14,21 @@ cnp.flush();
 B.start()
 sum=0.0
 for i in xrange(I):
-    #cnp.flush();
-    #x = np.random.random(B.size, dtype=B.dtype, cphvb=B.cphvb)
-    #y = np.random.random(B.size, dtype=B.dtype, cphvb=B.cphvb)
+    cnp.flush();
+    x = np.random.random(B.size, dtype=B.dtype, cphvb=B.cphvb)
+    y = np.random.random(B.size, dtype=B.dtype, cphvb=B.cphvb)
 
     np.square(x,x)
     np.square(y,y)
     np.add(x,y,x)
     z = np.less_equal(x, 1.0)
-    print z[:10]
-#    while z.ndim > 1:
-#        z = np.add.reduce(z)
-#    sum += np.add.reduce(z)*4.0/N
-#sum /= I
+    while z.ndim > 1:
+        z = np.add.reduce(z)
+    sum += np.add.reduce(z)*4.0/N
+
+sum /= I
 B.stop()
-print "Pi: ", sum
 B.pprint()
+
+print sum
 
--- benchmark/Python/knn.naive.py
+++ benchmark/Python/knn.naive.py
@@ -2,12 +2,7 @@ import cphvbnumpy as np
 import cphvbbridge as cb
 import util
 
-def main():
-
-    B = util.Benchmark()
-    N   = B.size[0]                             # Size of the dataset
-    F   = B.size[1]                             # Number of features in the dataset
-    K   = B.size[2] if B.size[2] < N else N     # The K number of neighbors to find
+def data_range(B, N, F):
 
     # Data is a list of N-dimensional coordinates / features in euclidean space.
     data_flat   = np.array(range(0,N+1)*F, dtype=B.dtype)
@@ -16,17 +11,46 @@ def main():
 
     # Target is a single coordinate / feature in N-dimensional euclidian-space.
     x = np.array([[N/2]]*F, dtype=B.dtype)
-    x.cphvb = B.cphvb
+    x.cphvb     = B.cphvb
 
-    cb.flush()                                  # Measuring...
+    return data, x
+
+
+def data_image(B, N, F):
+
+    try:
+        import Image
+        img     = Image.open("knn.input.bmp")
+        data    = np.array(img.getdata()).T.copy()
+        x       = np.array([[0],[250],[0]])
+        return data, x
+    except Exception as e:
+        print "Failed using image data-set, reverting to range. Err=[%s]" % e
+
+    return data_range(B, N, F)
+
+def main():
+
+    B = util.Benchmark()
+    N   = B.size[0]                             # Size of the dataset
+    F   = B.size[1]                             # Number of features in the dataset
+    K   = B.size[2] if B.size[2] < N else N     # The K number of neighbors to find
+
+    #data, x = data_image(B, N, F)
+    data, x = data_range(B, N, F)               # Grab a data-set
+    F = len(x)
+    cb.flush()                                  # Flush & Measure
     B.start()
 
     sqd = np.sqrt(((data - x)**2).sum(axis=0))  # The naive kNN-implementation
-    idx = np.argsort(sqd)                       # Get the indexes
-    nn  = data[:,idx[:K]]                       # Get the corresponding elements
 
     B.stop()                                    # Print the timing results
     B.pprint()
+
+
+    idx = np.argsort(sqd)                       # Get the indexes
+    nn  = data[:,idx[:K]]                       # Get the corresponding elements
+
 
     print nn
 
