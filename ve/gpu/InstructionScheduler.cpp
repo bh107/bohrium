81df96ad45772bb7b1c8e47732849fe30fe95951
Kenneth Skovhede <kenneth@hexad.dk>
2013-04-18 14:05:51 +0200

    Added missing break

--- ve/gpu/InstructionScheduler.cpp
+++ ve/gpu/InstructionScheduler.cpp
@@ -84,6 +84,7 @@ bh_error InstructionScheduler::schedule(bh_intp instructionCount,
 	            else
 	            	res = BH_TYPE_NOT_SUPPORTED;
 
+				break;
             default:
                 res = ufunc(inst);
                 break;
