7e33e43f8653cc261707f68fcadde948c36ddfce
Simon A. F. Lund <safl@safl.dk>
2013-04-30 23:54:22 +0200

    cppb: Changed cppb/monte-carlo-pi to reflect changes in bridge.

--- bridge/cpp/examples/monte_carlo_pi.cpp
+++ bridge/cpp/examples/monte_carlo_pi.cpp
@@ -6,19 +6,19 @@ using namespace bh;
 
 double monte_carlo_pi(int samples, int iterations)
 {
-    multi_array<double> x, y, m, c, sum(1), res(1); // Operands
-    sum = (double)0.0;                              // Acculumate across iterations
+    multi_array<double> x, y, m, c, accu(1), res(1);// Operands
+    accu = (double)0.0;                             // Acculumate across iterations
     for(int i=0; i<iterations; ++i) {
         x = random<double>(samples);                // Sample random numbers
         y = random<double>(samples);
         m = (sqrt(x*x + y*y)<=1.0).as<double>();    // Model
-        c = m.reduce(ADD, 0);                       // Count
+        c = reduce(m, ADD, 0);                      // Count
 
-        sum += (c*4.0) / (double)samples;           // Approximate
+        accu += (c*4.0) / (double)samples;          // Approximate
     }
-    sum /= (double)iterations;
+    accu /= (double)iterations;
     
-    return *sum.begin();
+    return scalar(accu);
 }
 
 int main(int argc, char* argv[])
