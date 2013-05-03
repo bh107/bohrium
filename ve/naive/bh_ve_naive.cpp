0bc699522e882b319a7d03ac3c1e55f5e3e3f13a
Kenneth Skovhede <kenneth@hexad.dk>
2013-05-03 13:53:56 +0200

    More merge fixup

--- ve/naive/bh_ve_naive.cpp
+++ ve/naive/bh_ve_naive.cpp
@@ -22,16 +22,12 @@ If not, see <http://www.gnu.org/licenses/>.
 #include <bh_vcache.h>
 
 static bh_component *myself = NULL;
-static bh_userfunc_impl reduce_impl = NULL;
-static bh_intp reduce_impl_id = 0;
 static bh_userfunc_impl random_impl = NULL;
 static bh_intp random_impl_id = 0;
 static bh_userfunc_impl matmul_impl = NULL;
 static bh_intp matmul_impl_id = 0;
 static bh_userfunc_impl nselect_impl = NULL;
 static bh_intp nselect_impl_id = 0;
-static bh_userfunc_impl aggregate_impl = NULL;
-static bh_intp aggregate_impl_id = 0;
 
 static bh_intp vcache_size   = 10;
 
@@ -86,11 +82,7 @@ bh_error bh_ve_naive_execute( bh_intp instruction_count, bh_instruction* instruc
 
             case BH_USERFUNC:                    // External libraries
 
-                if (inst->userfunc->id == reduce_impl_id) {
-
-                    res = reduce_impl(inst->userfunc, NULL);
-
-                } else if(inst->userfunc->id == random_impl_id) {
+                if(inst->userfunc->id == random_impl_id) {
 
                     res = random_impl(inst->userfunc, NULL);
 
@@ -101,10 +93,6 @@ bh_error bh_ve_naive_execute( bh_intp instruction_count, bh_instruction* instruc
                 } else if(inst->userfunc->id == nselect_impl_id) {
 
                     res = nselect_impl(inst->userfunc, NULL);
-
-                } else if(inst->userfunc->id == aggregate_impl_id) {
-
-                    res = aggregate_impl(inst->userfunc, NULL);
 
                 } else {                            // Unsupported userfunc
                 
@@ -139,24 +127,7 @@ bh_error bh_ve_naive_shutdown( void )
 
 bh_error bh_ve_naive_reg_func(char *fun, bh_intp *id) 
 {
-    if(strcmp("bh_reduce", fun) == 0)
-    {
-    	if (reduce_impl == NULL)
-    	{
-			bh_component_get_func(myself, fun, &reduce_impl);
-			if (reduce_impl == NULL)
-				return BH_USERFUNC_NOT_SUPPORTED;
-
-			reduce_impl_id = *id;
-			return BH_SUCCESS;			
-        }
-        else
-        {
-        	*id = reduce_impl_id;
-        	return BH_SUCCESS;
-        }
-    }
-    else if(strcmp("bh_random", fun) == 0)
+    if(strcmp("bh_random", fun) == 0)
     {
     	if (random_impl == NULL)
     	{
@@ -207,30 +178,8 @@ bh_error bh_ve_naive_reg_func(char *fun, bh_intp *id)
             return BH_SUCCESS;
         }
     }
-    else if(strcmp("bh_aggregate", fun) == 0)
-    {
-        if (aggregate_impl == NULL)
-        {
-            bh_component_get_func(myself, fun, &aggregate_impl);
-            if (aggregate_impl == NULL)
-                return BH_USERFUNC_NOT_SUPPORTED;
-            
-            aggregate_impl_id = *id;
-            return BH_SUCCESS;
-        }
-        else
-        {
-            *id = aggregate_impl_id;
-            return BH_SUCCESS;
-        }
-    }
         
     return BH_USERFUNC_NOT_SUPPORTED;
-}
-
-bh_error bh_reduce( bh_userfunc *arg, void* ve_arg)
-{
-    return bh_compute_reduce_naive( arg, ve_arg );
 }
 
 bh_error bh_random( bh_userfunc *arg, void* ve_arg)
@@ -247,9 +196,3 @@ bh_error bh_nselect( bh_userfunc *arg, void* ve_arg)
 {
     return bh_compute_nselect( arg, ve_arg );
 }
-
-bh_error bh_aggregate( bh_userfunc *arg, void* ve_arg)
-{
-    return bh_compute_aggregate( arg, ve_arg );
-}
-
