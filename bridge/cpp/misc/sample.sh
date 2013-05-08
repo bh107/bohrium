80010aff50f47692c0dfa639e314f44243205e18
Simon A. F. Lund <safl@safl.dk>
2013-05-08 13:59:05 +0200

    cppb: Minor change to the sampling-script.

--- bridge/cpp/misc/sample.sh
+++ bridge/cpp/misc/sample.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-/usr/bin/time python ../../../benchmark/Python/black_scholes.py --size=1000*10 --bohrium=True | .././symbolize.py > bs_python.txt
-/usr/bin/time ../../../bridge/cpp/bin/black_scholes | .././symbolize.py > bs_cpp.txt
-
+killall gvimdiff
+/usr/bin/time python ../../../benchmark/Python/black_scholes.py --size=1000*10 --bohrium=True | ../../../misc/tools/symbolize.py > bs_python.txt
+/usr/bin/time ../../../benchmark/cpp/bin/black_scholes --size=1000*10 | ../../../misc/tools/symbolize.py > bs_cpp.txt
+meld bs_python.txt bs_cpp.txt &
