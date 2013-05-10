8bc6de9716c1f4f9d9e6f0b1f334e04504179dbe
Simon A. F. Lund <safl@safl.dk>
2013-05-10 15:13:22 +0200

    New branch for JIT-toolchain stuff.

---
+++ ve/dynamite/bh_ve_dynamite.cpp
@@ -0,0 +1,198 @@
+/*
+This file is part of Bohrium and copyright (c) 2012 the Bohrium
+team <http://www.bh107.org>.
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the 
+GNU Lesser General Public License along with Bohrium. 
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <bh.h>
+#include "bh_ve_dynamite.h"
+#include <bh_vcache.h>
+
+static bh_component *myself = NULL;
+static bh_userfunc_impl random_impl = NULL;
+static bh_intp random_impl_id = 0;
+static bh_userfunc_impl matmul_impl = NULL;
+static bh_intp matmul_impl_id = 0;
+static bh_userfunc_impl nselect_impl = NULL;
+static bh_intp nselect_impl_id = 0;
+
+static bh_intp vcache_size   = 10;
+
+bh_error bh_ve_dynamite_init(bh_component *self)
+{
+    myself = self;
+
+    char *env = getenv("BH_CORE_VCACHE_SIZE");     // Override block_size from environment-variable.
+    if(env != NULL)
+    {
+        vcache_size = atoi(env);
+    }
+    if(vcache_size <= 0)                        // Verify it
+    {
+        fprintf(stderr, "BH_CORE_VCACHE_SIZE (%ld) should be greater than zero!\n", (long int)vcache_size);
+        return BH_ERROR;
+    }
+
+    bh_vcache_init( vcache_size );
+    return BH_SUCCESS;
+}
+
+bh_error bh_ve_dynamite_execute( bh_intp instruction_count, bh_instruction* instruction_list )
+{
+    bh_intp count;
+    bh_instruction* inst;
+    bh_error res = BH_SUCCESS;
+
+    for (count=0; count < instruction_count; count++) {
+
+        inst = &instruction_list[count];
+        #ifdef DEBUG
+        bh_pprint_instr(inst);
+        #endif
+
+        res = bh_vcache_malloc( inst );          // Allocate memory for operands
+        if ( res != BH_SUCCESS ) {
+            printf("Unhandled error returned by bh_vcache_malloc() called from bh_ve_dynamite_execute()\n");
+            return res;
+        }
+                                                    
+        switch (inst->opcode) {                     // Dispatch instruction
+
+            case BH_NONE:                        // NOOP.
+            case BH_DISCARD:
+            case BH_SYNC:
+                res = BH_SUCCESS;
+                break;
+            case BH_FREE:                        // Store data-pointer in malloc-cache
+                res = bh_vcache_free( inst );
+                break;
+
+            case BH_USERFUNC:                    // External libraries
+
+                if(inst->userfunc->id == random_impl_id) {
+
+                    res = random_impl(inst->userfunc, NULL);
+
+                } else if(inst->userfunc->id == matmul_impl_id) {
+
+                    res = matmul_impl(inst->userfunc, NULL);
+
+                } else if(inst->userfunc->id == nselect_impl_id) {
+
+                    res = nselect_impl(inst->userfunc, NULL);
+
+                } else {                            // Unsupported userfunc
+                
+                    res = BH_USERFUNC_NOT_SUPPORTED;
+
+                }
+
+                break;
+
+            default:                            // Built-in operations
+                res = bh_compute_apply_dynamite( inst );
+
+        }
+
+        if (res != BH_SUCCESS) {    // Instruction failed
+            break;
+        }
+
+    }
+
+	return res;
+}
+
+bh_error bh_ve_dynamite_shutdown( void )
+{
+    // De-allocate the malloc-cache
+    bh_vcache_clear();
+    bh_vcache_delete();
+
+    return BH_SUCCESS;
+}
+
+bh_error bh_ve_dynamite_reg_func(char *fun, bh_intp *id) 
+{
+    if(strcmp("bh_random", fun) == 0)
+    {
+    	if (random_impl == NULL)
+    	{
+			bh_component_get_func(myself, fun, &random_impl);
+			if (random_impl == NULL)
+				return BH_USERFUNC_NOT_SUPPORTED;
+
+			random_impl_id = *id;
+			return BH_SUCCESS;			
+        }
+        else
+        {
+        	*id = random_impl_id;
+        	return BH_SUCCESS;
+        }
+    }
+    else if(strcmp("bh_matmul", fun) == 0)
+    {
+    	if (matmul_impl == NULL)
+    	{
+            bh_component_get_func(myself, fun, &matmul_impl);
+            if (matmul_impl == NULL)
+                return BH_USERFUNC_NOT_SUPPORTED;
+            
+            matmul_impl_id = *id;
+            return BH_SUCCESS;			
+        }
+        else
+        {
+        	*id = matmul_impl_id;
+        	return BH_SUCCESS;
+        }
+    }
+    else if(strcmp("bh_nselect", fun) == 0)
+    {
+        if (nselect_impl == NULL)
+        {
+            bh_component_get_func(myself, fun, &nselect_impl);
+            if (nselect_impl == NULL)
+                return BH_USERFUNC_NOT_SUPPORTED;
+            
+            nselect_impl_id = *id;
+            return BH_SUCCESS;
+        }
+        else
+        {
+            *id = nselect_impl_id;
+            return BH_SUCCESS;
+        }
+    }
+        
+    return BH_USERFUNC_NOT_SUPPORTED;
+}
+
+bh_error bh_random( bh_userfunc *arg, void* ve_arg)
+{
+    return bh_compute_random( arg, ve_arg );
+}
+
+bh_error bh_matmul( bh_userfunc *arg, void* ve_arg)
+{
+    return bh_compute_matmul( arg, ve_arg );
+}
+
+bh_error bh_nselect( bh_userfunc *arg, void* ve_arg)
+{
+    return bh_compute_nselect( arg, ve_arg );
+}
---
+++ ve/dynamite/snippets/traverser.c
@@ -0,0 +1,83 @@
+/*
+This file is part of Bohrium and copyright (c) 2012 the Bohrium
+team <http://www.bh107.org>.
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the 
+GNU Lesser General Public License along with Bohrium. 
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "stdlib.h"
+#include "string.h"
+#include "assert.h"
+
+#define DYNAMITE_MAXDIM 16
+
+void traverse_aaa(size_t a0_start, size_t* a0_stride, float* a0_data,
+              size_t a1_start, size_t* a1_stride, float* a1_data,
+              size_t a2_start, size_t* a2_stride, float* a2_data,
+              size_t* shape,
+              size_t ndim,
+              size_t nelements)
+{
+    assert(a0_data != NULL); // Ensure that data is allocated
+    assert(a1_data != NULL);
+    assert(a2_data != NULL);
+
+    size_t j,           // Traversal variables
+            last_dim    = ndim-1,
+            last_e      = nelements-1;
+
+    size_t coord[DYNAMITE_MAXDIM];
+    size_t cur_e = 0;
+
+    size_t off0;        // Stride-offset
+    size_t off1;
+    size_t off2;
+
+    memset(coord, 0, DYNAMITE_MAXDIM * sizeof(size_t));
+
+    while (cur_e <= last_e) {
+        off0 = a0_start;                       // Reset offset
+        off1 = a1_start;
+        off2 = a2_start;
+
+        for (j=0; j<=last_dim; ++j) {           // Compute offset based on coordinate
+            off0 += coord[j] * a0_stride[j];
+            off1 += coord[j] * a1_stride[j];
+            off2 += coord[j] * a2_stride[j];
+        }
+                                                // Iterate over "last" / "innermost" dimension
+        for (; (coord[last_dim] < shape[last_dim]) && (cur_e <= last_e); coord[last_dim]++, cur_e++) {
+            //(off0+d0) = (off1+d1) {{OPERATOR}} (off2+d2);
+            *(off0+a0_data) = *(off1+a1_data) + *(off2+a2_data);
+
+            off0 += a0_stride[last_dim];
+            off1 += a1_stride[last_dim];
+            off2 += a2_stride[last_dim];
+        }
+
+        if (coord[last_dim] >= shape[last_dim]) {
+            coord[last_dim] = 0;
+            for(j = last_dim-1; j >= 0; --j) {  // Increment coordinates for the remaining dimensions
+                coord[j]++;
+                if (coord[j] < shape[j]) {      // Still within this dimension
+                    break;
+                } else {                        // Reached the end of this dimension
+                    coord[j] = 0;               // Reset coordinate
+                }                               // Loop then continues to increment the next dimension
+            }
+        }
+    }
+}
+
