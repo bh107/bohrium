7acf30482e8283bdf61f681ebe5d0cc8bc90e456
Simon A. F. Lund <safl@safl.dk>
2013-05-23 15:55:30 +0200

    cppb: Added syntactic sugar for armadillo lovers.

---
+++ bridge/cpp/bh/sugar/armadillo.hpp
@@ -0,0 +1,38 @@
+// stuff for armadillo lovers...
+/*
+This file is part of cphVB and copyright (c) 2012 the cphVB team:
+http://bohrium.bitbucket.org
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the 
+GNU Lesser General Public License along with Bohrium. 
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __BOHRIUM_BRIDGE_CPP_ARMADILLO
+#define __BOHRIUM_BRIDGE_CPP_ARMADILLO
+namespace bh {
+
+typedef multi_array<double> vec;
+typedef multi_array<float> fvec;
+
+typedef multi_array<double> mat;
+typedef multi_array<float> fmat;
+
+typedef multi_array<double> colvec;
+typedef multi_array<float> fcolvec;
+
+// More stuff can be done here
+
+}
+#endif
+
