91cc6d056cddf38c7738395100a8ed75633500ff
Simon A. F. Lund <safl@safl.dk>
2013-07-11 11:43:22 +0200

    cppb: Nothing...

--- bridge/cpp/bh/runtime.hpp
+++ bridge/cpp/bh/runtime.hpp
@@ -171,10 +171,9 @@ size_t Runtime::execute()
     
     bh_ir* bhir;
     bh_error status = bh_graph_create(&bhir, queue, queue_size);
-    if (status == BH_SUCCESS)
-    {
-        status = vem_execute(bhir);   // Send instructions to Bohrium
-        queue_size = 0;                                     // Reset size of the queue
+    if (status == BH_SUCCESS) {
+        status = vem_execute(bhir);     // Send instructions to Bohrium
+        queue_size = 0;                 // Reset size of the queue
     }
     
     bh_graph_destroy(bhir);
