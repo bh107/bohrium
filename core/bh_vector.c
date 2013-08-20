22358480c04082e045494bfbce2f5ef32b45af2c
Mads R. B. Kristensen <madsbk@gmail.com>
2013-08-20 10:23:48 +0200

    Minor cleanup of the vector "class"

--- core/bh_vector.c
+++ core/bh_vector.c
@@ -133,8 +133,6 @@ void *bh_vector_reserve(void *vector, bh_intp n)
  *
  * If n is greater than the current container size, the content is expanded
  * by inserting at the end as many elements as needed to reach a size of n.
- * If val is specified, the new elements are initialized as copies of val,
- * otherwise, they are value-initialized.
  *
  * If n is also greater than the current container capacity, an automatic
  * reallocation of the allocated storage space takes place.
