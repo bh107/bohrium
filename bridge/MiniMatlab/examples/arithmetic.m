902c81ac89b0eaf06ff56e356da3011d897d03a1
gisli <gislieir@gmail.com>
2013-09-19 15:52:37 +0200

    Adding MiniMatlab interpreter plus inserting some missing inline keywords.

---
+++ bridge/MiniMatlab/examples/arithmetic.m
@@ -0,0 +1,13 @@
+% Testing arithmetic operations: all should give 99.0000
+A = 24 + 50 + 25; 					% simple addition
+A
+B = 198 - 54 - 45					% simple subtraction
+C = 33 * 3							% simple multiplication
+D = 198 / 2							% simple division
+E = +42 + 33 + 8 * 3 				% checking associativity
+F = 7 * 11 + 23 - 1 				% checking associativity
+G = -33 * -3 						% using negative numbers
+H = (1 + 2) * (16 + 17) 			% parentheses
+I = 99 * 2 + 198 / -2 				% associativity and negative numbers
+J = (-100 / 2 - 98 / 2) * -1 		% associativity, negative numbers and parentheses
+K = 33 * 3 * 2 / 2 					% associativity (multiplication and division)
