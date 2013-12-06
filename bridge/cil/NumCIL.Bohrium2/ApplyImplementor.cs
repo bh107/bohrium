e4e52376d53c3099f9a6cf5345b45e6279463bee
Kenneth Skovhede <kenneth@hexad.dk>
2013-12-06 15:50:02 +0100

    Wired the apply implementor to all methods

--- bridge/cil/NumCIL.Bohrium2/ApplyImplementor.cs
+++ bridge/cil/NumCIL.Bohrium2/ApplyImplementor.cs
@@ -46,36 +46,76 @@ namespace NumCIL.Bohrium2
 
         public bool ApplyBinaryConvOp<Ta, Tb, C>(C op, NdArray<Ta> in1, NdArray<Ta> in2, NdArray<Tb> @out) where C : struct, IBinaryConvOp<Ta, Tb>
         {
+            object h;
+            if (_implementors.TryGetValue(typeof(Tb), out h))
+            {
+                var i = (ITypedApplyImplementor<Tb>)h;
+                return i.ApplyBinaryConvOp<Ta>(op.GetType(), in1, in2, @out);
+            }
+            
             DEBUG_FALLBACK(op.GetType(), new Type[] {in1.DataAccessor.GetType(), in2.DataAccessor.GetType(), @out.DataAccessor.GetType() });
             return false;
         }
 
         public bool ApplyUnaryConvOp<Ta, Tb, C>(C op, NdArray<Ta> in1, NdArray<Tb> @out) where C : struct, IUnaryConvOp<Ta, Tb>
         {
+            object h;
+            if (_implementors.TryGetValue(typeof(Tb), out h))
+            {
+                var i = (ITypedApplyImplementor<Tb>)h;
+                return i.ApplyUnaryConvOp<Ta>(op.GetType(), in1, @out);
+            }
+            
             DEBUG_FALLBACK(op.GetType(), new Type[] {in1.DataAccessor.GetType(), @out.DataAccessor.GetType() });
             return false;
         }
 
         public bool ApplyNullaryOp<T, C>(C op, NdArray<T> @out) where C : struct, INullaryOp<T>
         {
+            object h;
+            if (_implementors.TryGetValue(typeof(T), out h))
+            {
+                var i = (ITypedApplyImplementor<T>)h;
+                return i.ApplyNullaryOp(op.GetType(), @out);
+            }
             DEBUG_FALLBACK(op.GetType(), new Type[] { @out.DataAccessor.GetType() });
             return false;
         }
 
         public bool ApplyReduce<T, C>(C op, long axis, NdArray<T> in1, NdArray<T> @out) where C : struct, IBinaryOp<T>
         {
+            object h;
+            if (_implementors.TryGetValue(typeof(T), out h))
+            {
+                var i = (ITypedApplyImplementor<T>)h;
+                return i.ApplyReduce(op.GetType(), axis, in1, @out);
+            }
+            
             DEBUG_FALLBACK(op.GetType(), new Type[] {in1.DataAccessor.GetType(), @out.DataAccessor.GetType() });
             return false;
         }
 
         public bool ApplyMatmul<T, CADD, CMUL>(CADD addop, CMUL mulop, NdArray<T> in1, NdArray<T> in2, NdArray<T> @out = null) where CADD : struct, IBinaryOp<T> where CMUL : struct, IBinaryOp<T>
         {
+            object h;
+            if (_implementors.TryGetValue(typeof(T), out h))
+            {
+                var i = (ITypedApplyImplementor<T>)h;
+                return i.ApplyMatmul(addop.GetType(), mulop.GetType(), in1, in2, @out);
+            }
             DEBUG_FALLBACK(addop.GetType(), new Type[] { mulop.GetType(), in1.DataAccessor.GetType(), in2.DataAccessor.GetType(), @out.DataAccessor.GetType() });
             return false;
         }
 
         public bool ApplyAggregate<T, C>(C op, NdArray<T> in1, out T result) where C : struct, IBinaryOp<T>
         {
+            object h;
+            if (_implementors.TryGetValue(typeof(T), out h))
+            {
+                var i = (ITypedApplyImplementor<T>)h;
+                return i.ApplyAggregate(op.GetType(), in1, out result);
+            }
+            
             DEBUG_FALLBACK(op.GetType(), new Type[] {in1.DataAccessor.GetType(), typeof(T) });
             result = default(T);
             return false;
