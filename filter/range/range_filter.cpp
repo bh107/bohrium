7174d344f19395bdef1320178eb4da9492fc9ec5
Simon A. F. Lund <safl@safl.dk>
2013-12-09 10:38:38 +0100

    filter-range: nothing really...

--- filter/range/range_filter.cpp
+++ filter/range/range_filter.cpp
@@ -26,11 +26,48 @@ If not, see <http://www.gnu.org/licenses/>.
 
 using namespace std;
 
+typedef enum { NONE, RANGE, FUSABLE, STREAMABLE } tag_t;
+
+bh_error void ref_count(bh_ir* bhir, dag)
+{
+    for(bh_intp i=0; i< bhir->dag_list[dag].nnode; ++i) {
+        bh_intp id = bhir->dag_list[dag].node_map[i];
+        if (id < 0) {
+            id = -1*id-1;
+            bh_error err = ref_count(bhir, );
+            if (err != BH_SUCCESS)
+                return err;
+        } else {
+            bh_error err = func(&bhir->instr_list[id]);
+            if (err != BH_SUCCESS)
+                return err;
+        }
+    }
+    return BH_SUCCESS;
+}
+
 void range_filter(bh_ir* bhir)
 {
-    bh_intp nodes[] = {0};
-    cout << "### Range-filter searching through. ndag="<< bhir->ndag  << "." << endl;
-    bh_dag_split(bhir, 1, nodes, 0, -1);  
+    cout << "### range-filter on bhir->ninstr = " << bhir->ninstr << endl;
+    for(bh_intp dag=0; dag<bhir->ndag; ++dag) {
+
+        cout << "Looking at dag[" << dag << "] " << dag+1 << " of "<< bhir->ndag << \
+                ", containing " << bhir->dag_list[dag].nnode << " node(s)" << endl;
+
+        if (bhir->dag_list[dag].tag != NONE) {      // Already tagged, skipping it.
+            cout << "Skipping it..." << endl;
+            continue;
+        }
+
+        for(bh_intp max_nodes=bhir->dag_list[dag].nnode; max_nodes>0; max_nodes--) {
+
+            bh_intp nodes[] = {0};
+            bh_dag_split(bhir, 1, nodes, dag, -1);  // Sub-graphing
+            bh_intp dag_idx = bhir->ndag-1;         // Index of sub-graph
+            bhir->dag_list[dag_idx].tag = RANGE;    // Tagging
+
+        }
+    }
     cout << "###" << endl;
 }
 
