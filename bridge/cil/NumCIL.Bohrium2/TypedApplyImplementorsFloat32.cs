5db775a9ee3917655369f6b9587ec06ea6717a66
Kenneth Skovhede <kenneth@hexad.dk>
2013-12-11 12:12:24 +0100

    Removed scalar support, we just send pinned arrays

--- bridge/cil/NumCIL.Bohrium2/TypedApplyImplementorsFloat32.cs
+++ bridge/cil/NumCIL.Bohrium2/TypedApplyImplementorsFloat32.cs
@@ -28,186 +28,28 @@ namespace NumCIL.Bohrium2
             new Tuple<Type, Func<multi32, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMax), PInvoke.bh_multi_array_float32_maximum),
             new Tuple<Type, Func<multi32, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMin), PInvoke.bh_multi_array_float32_minimin),
         };
+        
 
-        private Tuple<Type, Func<float, multi32, multi32>>[] m_binOpsScalarLhs = 
-        {
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IAdd), PInvoke.bh_multi_array_float32_add_scalar_lhs),
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.ISub), PInvoke.bh_multi_array_float32_subtract_scalar_lhs),
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMul), PInvoke.bh_multi_array_float32_multiply_scalar_lhs),
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IDiv), PInvoke.bh_multi_array_float32_divide_scalar_lhs),
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMod), PInvoke.bh_multi_array_float32_modulo_scalar_lhs),
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMax), PInvoke.bh_multi_array_float32_maximum_scalar_lhs),
-            new Tuple<Type, Func<float, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMin), PInvoke.bh_multi_array_float32_minimin_scalar_lhs),
-        };
-
-        private Tuple<Type, Func<multi32, float, multi32>>[] m_binOpsScalarRhs = 
-        {
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.IAdd), PInvoke.bh_multi_array_float32_add_scalar_rhs),
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.ISub), PInvoke.bh_multi_array_float32_subtract_scalar_rhs),
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.IMul), PInvoke.bh_multi_array_float32_multiply_scalar_rhs),
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.IDiv), PInvoke.bh_multi_array_float32_divide_scalar_rhs),
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.IMod), PInvoke.bh_multi_array_float32_modulo_scalar_rhs),
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.IMax), PInvoke.bh_multi_array_float32_maximum_scalar_rhs),
-            new Tuple<Type, Func<multi32, float, multi32>>(typeof(NumCIL.Generic.Operators.IMin), PInvoke.bh_multi_array_float32_minimin_scalar_rhs),
-        };
-        
-        private Tuple<Type, Action<multi32, multi32>>[] m_binOpsInPlace = 
-        {
-            new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.IAdd), PInvoke.bh_multi_array_float32_add_in_place),
-            new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.ISub), PInvoke.bh_multi_array_float32_subtract_in_place),
-            new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMul), PInvoke.bh_multi_array_float32_multiply_in_place),
-            new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.IDiv), PInvoke.bh_multi_array_float32_divide_in_place),
-            new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMod), PInvoke.bh_multi_array_float32_modulo_in_place),
-            //new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMax), PInvoke.bh_multi_array_float32_maximum_in_place),
-            //new Tuple<Type, Action<multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMin), PInvoke.bh_multi_array_float32_minimin_in_place),
-        };
-        
-        private Tuple<Type, Action<multi32, float>>[] m_binOpsInPlaceScalarRhs = 
-        {
-            new Tuple<Type, Action<multi32, float>>(typeof(NumCIL.Generic.Operators.IAdd), PInvoke.bh_multi_array_float32_add_in_place_scalar_rhs),
-            new Tuple<Type, Action<multi32, float>>(typeof(NumCIL.Generic.Operators.ISub), PInvoke.bh_multi_array_float32_subtract_in_place_scalar_rhs),
-            new Tuple<Type, Action<multi32, float>>(typeof(NumCIL.Generic.Operators.IMul), PInvoke.bh_multi_array_float32_multiply_in_place_scalar_rhs),
-            new Tuple<Type, Action<multi32, float>>(typeof(NumCIL.Generic.Operators.IDiv), PInvoke.bh_multi_array_float32_divide_in_place_scalar_rhs),
-            new Tuple<Type, Action<multi32, float>>(typeof(NumCIL.Generic.Operators.IMod), PInvoke.bh_multi_array_float32_modulo_in_place_scalar_rhs),
-            //new Tuple<Type, Action<multi32, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMax), PInvoke.bh_multi_array_float32_maximum_in_place_scalar_rhs),
-            //new Tuple<Type, Action<multi32, multi32, multi32>>(typeof(NumCIL.Generic.Operators.IMin), PInvoke.bh_multi_array_float32_minimin_in_place_scalar_rhs),
-        };
-        
         private Dictionary<Type, Func<multi32, multi32, multi32>> m_binOpLookup = new Dictionary<Type, Func<multi32, multi32, multi32>>();
-        private Dictionary<Type, Func<float, multi32, multi32>> m_binOpScalarLhsLookup = new Dictionary<Type, Func<float, multi32, multi32>>();
-        private Dictionary<Type, Func<multi32, float, multi32>> m_binOpScalarRhsLookup = new Dictionary<Type, Func<multi32, float, multi32>>();
         private Dictionary<Type, Func<multi32, multi32>> m_unOpLookup = new Dictionary<Type, Func<multi32, multi32>>();
-        private Dictionary<Type, Action<multi32, float>> m_binOpInPlaceScalarRhsLookup = new Dictionary<Type, Action<multi32, float>>();
-        private Dictionary<Type, Action<multi32, multi32>> m_binOpInPlaceLookup = new Dictionary<Type, Action<multi32, multi32>>();
 
         #region ITypedApplyImplementor implementation
 
         public bool ApplyBinaryOp(Type c, NdArray<float> in1, NdArray<float> in2, NdArray<float> @out)
         {
-            var isScalarIn1 = @in1.DataAccessor.GetType() == typeof(DefaultAccessor<float>) && @in1.DataAccessor.Length == 1;
-            var isScalarIn2 = @in2.DataAccessor.GetType() == typeof(DefaultAccessor<float>) && @in2.DataAccessor.Length == 1;
-            
-            Action<NdArray<float>, NdArray<float>, NdArray<float>> exec = null;
-            
-            if (exec == null && isScalarIn2 && in1.DataAccessor == @out.DataAccessor && @out.DataAccessor is Bohrium2.DataAccessor_float32)
+            Func<multi32, multi32, multi32> m;
+            // This lookup prevents a linear scan of the supported operands
+            if (!m_binOpLookup.TryGetValue(c, out m))
             {
-                Action<multi32, float> minplacerhs;
-                
-                if (!m_binOpInPlaceScalarRhsLookup.TryGetValue(c, out minplacerhs))
-                {
-                    minplacerhs = (from n in m_binOpsInPlaceScalarRhs
-                                                  where n.Item1.IsAssignableFrom(c)
-                                                  select n.Item2).FirstOrDefault();
-                     
-                    m_binOpInPlaceScalarRhsLookup[c] = minplacerhs;
-                }
-                
-                if (minplacerhs != null)
-                    exec = (_in1, _in2, _out) => {
-                        using (var v0 = new PInvoke.bh_multi_array_float32_p(_out))
-                            minplacerhs(v0, in2.DataAccessor[0]);
-                    };
+                m = (from n in m_binOps
+                                    where n.Item1.IsAssignableFrom(c)
+                                    select n.Item2).FirstOrDefault();
+                m_binOpLookup[c] = m;
             }
-
-            if (exec == null && in1.DataAccessor == @out.DataAccessor && @out.DataAccessor is Bohrium2.DataAccessor_float32)
+        
+            if (m == null)
             {
-                Action<multi32, multi32> minplace;
-                
-                if (!m_binOpInPlaceLookup.TryGetValue(c, out minplace))
-                {
-                    minplace = (from n in m_binOpsInPlace
-                                                  where n.Item1.IsAssignableFrom(c)
-                                                  select n.Item2).FirstOrDefault();
-                     
-                    m_binOpInPlaceLookup[c] = minplace;
-                }
-                
-                if (minplace != null)
-                    exec = (_in1, _in2, _out) => {
-                        using (var v0 = new PInvoke.bh_multi_array_float32_p(_out))
-                        using (var v2 = new PInvoke.bh_multi_array_float32_p(_in2))
-                            minplace(v0, v2);
-                    };
-            }
-            
-
-            // Not sure this extra code is worth the trouble for dealing with scalars
-            if (exec == null && isScalarIn1)
-            {
-                Func<float, multi32, multi32> mlhs;
-                
-                if (!m_binOpScalarLhsLookup.TryGetValue(c, out mlhs))
-                {
-                    mlhs = (from n in m_binOpsScalarLhs
-                                           where n.Item1.IsAssignableFrom(c)
-                                           select n.Item2).FirstOrDefault();
-                     
-                    m_binOpScalarLhsLookup[c] = mlhs;
-                }
-                
-                if (mlhs != null)
-                {
-                    exec = (_in1, _in2, _out) =>
-                    {
-                        using (var v2 = new PInvoke.bh_multi_array_float32_p(_in2))
-                        using (var v0 = new PInvoke.bh_multi_array_float32_p(_out))
-                            PInvoke.bh_multi_array_float32_assign_array(v0, mlhs(_in1.DataAccessor[0], v2));
-                    };
-                }
-            }
-
-            if (isScalarIn2 && exec == null)
-            {
-                Func<multi32, float, multi32> mrhs;
-                
-                if (!m_binOpScalarRhsLookup.TryGetValue(c, out mrhs))
-                {
-                    mrhs = (from n in m_binOpsScalarRhs
-                                           where n.Item1.IsAssignableFrom(c)
-                                           select n.Item2).FirstOrDefault();
-                     
-                    m_binOpScalarRhsLookup[c] = mrhs;
-                }
-                
-                if (mrhs != null)
-                {
-                    exec = (_in1, _in2, _out) =>
-                    {
-                        using (var v1 = new PInvoke.bh_multi_array_float32_p(_in1))
-                        using (var v0 = new PInvoke.bh_multi_array_float32_p(_out))
-                            PInvoke.bh_multi_array_float32_assign_array(v0, mrhs(v1, _in2.DataAccessor[0]));
-                    };
-
-                }
-            }
-            
-            if (exec == null)
-            {
-                Func<multi32, multi32, multi32> m;
-                // This lookup prevents a linear scan of the supported operands
-                if (!m_binOpLookup.TryGetValue(c, out m))
-                {
-                    m = (from n in m_binOps
-                                        where n.Item1.IsAssignableFrom(c)
-                                        select n.Item2).FirstOrDefault();
-                    m_binOpLookup[c] = m;
-                }
-            
-                if (m != null)
-                {                
-                    exec = (_in1, _in2, _out) =>
-                    {
-                        using (var v1 = new PInvoke.bh_multi_array_float32_p(@in1))
-                        using (var v2 = new PInvoke.bh_multi_array_float32_p(@in2))
-                        using (var v0 = new PInvoke.bh_multi_array_float32_p(@out))
-                            PInvoke.bh_multi_array_float32_assign_array(v0, m(v1, v2));
-                    };  
-                }              
-            }
-
-            if (exec == null)
-            {
-                Console.WriteLine("No registered match for: {0}", c.FullName);
+                Console.WriteLine("No registered match for: {0}: {1}", c.FullName, @out.DataAccessor.GetType().FullName, in1.DataAccessor.GetType().FullName, in2.DataAccessor.GetType().FullName);
                 return false;
             }
                 
@@ -215,14 +57,17 @@ namespace NumCIL.Bohrium2
             // If the input is used, no special action is performed until 
             // a sync is executed, then all the BH queue is flushed and 
             // the GC handles released
-            
-            // If the output is CIL-managed, we must sync immediately
-            
-            exec(@in1, @in2, @out);            
+                        
+            using (var v1 = new PInvoke.bh_multi_array_float32_p(@in1))
+            using (var v2 = new PInvoke.bh_multi_array_float32_p(@in2))
+            using (var v0 = new PInvoke.bh_multi_array_float32_p(@out))
+                PInvoke.bh_multi_array_float32_assign_array(v0, m(v1, v2));
+                
             if (@out.DataAccessor is DataAccessor_float32)
                 ((DataAccessor_float32)@out.DataAccessor).SetDirty();
             else
             {
+                // If the output is CIL-managed, we must sync immediately
                 Utility.Flush();
                 PinnedArrayTracker.Release();
             }
@@ -282,7 +127,7 @@ namespace NumCIL.Bohrium2
             
             if (exec == null)
             {
-                Console.WriteLine("No registered match for: {0}", c.FullName);
+                Console.WriteLine("No registered match for: {0}: ", c.FullName, @out.DataAccessor.GetType().FullName, in1.DataAccessor.GetType().FullName);
                 return false;
             }
             
