05d38bb761438e89cb4a18b6173e863ebe55a6ea
Mads R. B. Kristensen <madsbk@gmail.com>
2013-12-12 16:09:41 +0100

    Cleanup of the bridge-c Makefile:wq

--- bridge/c/codegen/gen.py
+++ bridge/c/codegen/gen.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import json
 import os
+from os.path import join, exists
 from pprint import pprint
 from Cheetah.Template import Template
+import stat
 
-def render( gens, tmpl_dir, output_dir ):
+def render( gens, tmpl_dir, output_dir, mtime):
+
+    if not exists(output_dir):
+        os.mkdir(output_dir)
 
     prev_output_fn   = gens[0][1]
     prev_output      = ""
@@ -17,13 +22,19 @@ def render( gens, tmpl_dir, output_dir ):
         last = count == c
 
         if (output_fn != prev_output_fn ):
-            open( output_dir + prev_output_fn, 'w').write( str(prev_output) )
+            with open(output_dir + prev_output_fn, 'w') as f:
+                f.write(str(prev_output))
+                f.close()
+                set_timestamp(f.name, (mtime,mtime))
             prev_output = ""
 
         prev_output += str(t_tmpl)
 
         if last:
-            open( output_dir + output_fn, 'w').write( str(prev_output) )
+            with open(output_dir + output_fn, 'w') as f:
+                f.write(str(prev_output))
+                f.close()
+                set_timestamp(f.name, (mtime,mtime))
 
         prev_output_fn = output_fn
 
@@ -34,17 +45,38 @@ def map_type(typename, types):
 
     return "ERR"
 
+def get_timestamp(f):
+    st = os.stat(f)
+    atime = st[stat.ST_ATIME] #access time
+    mtime = st[stat.ST_MTIME] #modification time
+    return (atime,mtime)
+
+def set_timestamp(f,timestamp):
+    os.utime(f,timestamp)
+
 def main():
 
     script_dir  = "." + os.sep + "codegen" + os.sep
     output_dir  = script_dir + "output" + os.sep
     tmpl_dir    = script_dir + "templates" + os.sep
 
-    reductions  = json.loads(open(script_dir+'reductions.json').read())
-    opcodes     = json.loads(open(script_dir+'..'+ os.sep+'..'+ os.sep +'..'+ os.sep +'core'+ os.sep +'codegen'+ os.sep +'opcodes.json').read())
-    types       = json.loads(open(script_dir+'..'+ os.sep+'..'+ os.sep + 'cpp' + os.sep + 'codegen' + os.sep +'element_types.json').read())
+    paths = {'reductions': join(script_dir,'reductions.json'),
+             'opcodes'   : join(script_dir,'..','..','..','core','codegen','opcodes.json'),
+             'types'     : join(script_dir,'..','..','cpp','codegen','element_types.json'),
+             'operators' : join(script_dir,'operators.json'),
+             'self'      : join(script_dir,'gen.py')}
 
-    operators   = json.loads(open(script_dir +'operators.json').read())
+    reductions  = json.loads(open(paths['reductions']).read())
+    opcodes     = json.loads(open(paths['opcodes']).read())
+    types       = json.loads(open(paths['types']).read())
+    operators   = json.loads(open(paths['operators']).read())
+
+    #Find the latest modification time
+    mtime = 0
+    for _,p in paths.iteritems():
+        t = get_timestamp(p)
+        if t[1] > mtime:
+            mtime = t[1]
 
     op_map  = []
     for name, opcode, t, inplace in operators:
@@ -52,7 +84,7 @@ def main():
 
         typesigs = [x["types"] for x in opcodes if x['opcode'] == opcode and not x['system_opcode']]
         typesigs = typesigs[0] if typesigs else []
-        
+
         new_typesigs = []
         for ttt in typesigs:
             if 'BH_UINT8' in ttt: # UINT8 not supported by cpp-bridge
@@ -82,7 +114,7 @@ def main():
         ('implementation.ctpl',             'bh_c_implementation.cpp',              op_map),
     ]
 
-    render( gens, tmpl_dir, output_dir )
+    render( gens, tmpl_dir, output_dir, mtime)
 
 if __name__ == "__main__":
     main()
