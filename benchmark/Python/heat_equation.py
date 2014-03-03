25fc03063bd6c811cf8165f9e9c5c8dc9bc70959
Mads R. B. Kristensen <madsbk@gmail.com>
2014-03-03 18:15:34 +0100

    added heat equation

---
+++ benchmark/Python/heat_equation.py
@@ -0,0 +1,17 @@
+import bohrium as np
+import bohrium.examples.jacobi_stencil as js
+import util
+
+B = util.Benchmark()
+H = B.size[0]
+W = B.size[1]
+I = B.size[2]
+
+ft = js.freezetrap(H,W,dtype=B.dtype,bohrium=B.bohrium)
+
+B.start()
+ft = js.solve(ft,max_iterations=I)
+B.stop()
+if B.verbose:
+    print ft
+B.pprint()
