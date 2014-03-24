7edcc5f7c4a8ef29f342040a991ec7ffc90c88af
Mads R. B. Kristensen <madsbk@gmail.com>
2014-03-24 14:04:00 +0100

    Expose all data types in __init__()

--- bridge/bhpy/__init__.py
+++ bridge/bhpy/__init__.py
@@ -1,7 +1,12 @@
 from array_create import *
 from ufunc import ufuncs
 from ndarray import check
+from _info import numpy_types
 
 #Expose all ufuncs
 for f in ufuncs:
     exec "%s = f"%f.info['np_name']
+
+#Expose all data types
+for t in numpy_types:
+    exec "%s = t"%t.__str__()
