b8ab1329efd565a085305662edeca03f023f13e3
Simon A. F. Lund <safl@safl.dk>
2014-03-31 14:59:18 +0200

    test: Reverted "test_types.py" to an older version due to faulty inclusion in the previous commit.

--- test/numpy/test_types.py
+++ test/numpy/test_types.py
@@ -5,19 +5,13 @@ from numpytest import numpytest,TYPES
 class test_different_inputs(numpytest):
     
     def init(self):
-        count = 0
         for t1 in TYPES.ALL:
-            for t2 in TYPES.ALL:                
+            for t2 in TYPES.ALL:
                 a = {}
                 v =  "a[0] = self.array((10),%s);"%(t1)
                 v += "a[1] = self.array((10),%s);"%(t2)
                 exec v
                 yield (a,v)
-                #count = count + 1
-                #if count == 2:
-                #    break
-            #if count == 2:
-            #    break
 
     def test_typecast(self,a):
         cmd = "res = a[0] + a[1]"
