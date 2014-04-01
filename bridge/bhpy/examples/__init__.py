a6f01441ddb443f141b2adf204c4b25cc501af0b
Mads R. B. Kristensen <madsbk@gmail.com>
2014-04-01 11:07:10 +0200

    added examples

---
+++ bridge/bhpy/examples/__init__.py
@@ -0,0 +1,14 @@
+"""
+Examples
+========
+
+Some code examples
+
+"""
+import gameoflife
+import jacobi_stencil
+import k_nearest_neighbor
+import shallow_water
+import sor
+import black_scholes
+import ndstencil
