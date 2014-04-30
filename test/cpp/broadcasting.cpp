d106a73e55b1f5ebbdd35d272f41b3b803fd4341
Simon A. F. Lund <safl@safl.dk>
2014-04-30 13:29:30 +0200

    cppb: Fixed namespace stuff...

--- test/cpp/broadcasting.cpp
+++ test/cpp/broadcasting.cpp
@@ -18,7 +18,7 @@ GNU Lesser General Public License along with bohrium.
 If not, see <http://www.gnu.org/licenses/>.
 */
 #include "bxx/bohrium.hpp"
-using namespace bh;
+using namespace bxx;
 
 
 #include <stdexcept>
