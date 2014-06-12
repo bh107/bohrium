aa03a2d1c3386fb922156e25ce349745a63e4594
Mads R. B. Kristensen <madsbk@gmail.com>
2014-06-12 18:03:28 +0200

    added email support to the debian builder

--- package/debian/src_build.py
+++ package/debian/src_build.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
 import subprocess
-from subprocess import check_output, check_call
+from subprocess import check_output, check_call, Popen, PIPE
 from datetime import datetime
 import os
 from os import path
 import argparse
 import tempfile
 import re
+import traceback
 
+out = ""#The output of this build and/or testing
+
+def main(args):
+    global out
+
+    #Make and change to a tmp dir
+    tmpdir = tempfile.mkdtemp(prefix="bh_deb_builder_")
+    os.chdir(tmpdir)
+
+    check_call(['git','pull'], cwd=args.cmake_file_dir)
+    check_call(['git','checkout', args.branch], cwd=args.cmake_file_dir)
+    out += check_output(['cmake',args.cmake_file_dir, '-DCPACK_PACKAGE_CONTACT=%s'%args.contact])
+
+    #Lets find the change files generatored by cmake
+    m = re.findall("signfile (.*source\.changes) %s\s*Successfully signed dsc and changes files"%args.contact, out)
+    if len(m) <= 0:
+        raise RuntimeError("cmake didn't generate any deb-src change files!")
+    out += "\ncmake generated the following deb-src change files: %s\n"%str(m)
+
+    #Lets uploade the files
+    for changefile in m:
+        out += "Uploading %s\n"%changefile
+        check_call(['dput', 'bo2hrium-nightly', 'Debian/%s'%changefile])
+
+    #Lets cleanup
+    check_call(['rm','-Rf',tmpdir])
 
 if __name__ == "__main__":
 
@@ -33,28 +60,24 @@ if __name__ == "__main__":
         type=str,
         help='The package contact info which is also used for signing the package.'
     )
+    parser.add_argument(
+        '--email',
+        type=str,
+        help='The result of the build and/or test will be emailed to the specified address.'
+    )
     args = parser.parse_args()
-
-    #Make and change to a tmp dir
-    tmpdir = tempfile.mkdtemp(prefix="bh_deb_builder_")
-    os.chdir(tmpdir)
-
-    check_call(['git','pull'], cwd=args.cmake_file_dir)
-    check_call(['git','checkout', args.branch], cwd=args.cmake_file_dir)
-    out = check_output(['cmake',args.cmake_file_dir, '-DCPACK_PACKAGE_CONTACT=%s'%args.contact])
-
+    status = "SUCCESS"
+    try:
+        main(args)
+    except Exception, e:
+        out += "*"*70
+        out += "\nERROR: %s"%traceback.format_exc()
+        out += "*"*70
+        out += "\n"
+        status = "FAILURE"
     print out
+    if args.email:
+        print "send status email to '%s'"%args.email
+        p = Popen(['mail','-s','"[Bohrium PPA Build] The result of build was a %s"'%status, args.email], stdin=PIPE)
+        p.communicate(input=out)
 
-    #Lets find the change files generatored by cmake
-    m = re.findall("signfile (.*source\.changes) %s\s*Successfully signed dsc and changes files"%args.contact, out)
-    if len(m) <= 0:
-        raise RuntimeError("cmake didn't generate any deb-src change files")
-    print "cmake generated the following two deb-src change files: ", m
-
-    #Lets uploade the files
-    for changefile in m:
-        print "Uploading %s"%changefile
-        check_call(['dput', 'bohrium-nightly', 'Debian/%s'%changefile])
-
-    #Lets cleanup
-    check_call(['rm','-Rf',tmpdir])
