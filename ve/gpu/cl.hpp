9c2ba1607c267da2f3777caceaf9de38cad4fec6
Kenneth Skovhede <kenneth@hexad.dk>
2014-06-20 09:59:04 +0200

    Update issue #79 Status: Fixed
    Fixed include for OpenCL in CMake scripts.
    Updated paths and preprocessor macros for including OpenCL.
    Added some deprecation macros for OpenCL hpp on OSX.

--- ve/gpu/cl.hpp
+++ ve/gpu/cl.hpp
@@ -1,5 +1,11 @@
 #define CL_USE_DEPRECATED_OPENCL_1_1_APIS
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/opencl.h>
+#define CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+#define CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+#else
 #include <CL/cl.h>
+#endif
 #undef CL_VERSION_1_2
 #define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>
