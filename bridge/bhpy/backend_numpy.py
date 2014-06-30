dd9477b5459b656a4bfe60564904e8ff19ee21fd
Mads R. B. Kristensen <madsbk@gmail.com>
2014-06-30 17:04:01 +0200

    bhpy: implemented victim cache for the NumPy backend

--- bridge/bhpy/backend_numpy.py
+++ bridge/bhpy/backend_numpy.py
@@ -6,15 +6,30 @@ import bhc
 import numpy as np
 from _util import dtype_name
 import mmap
+import time
+
+VCACHE_SIZE = 10
+vcache = []
 
 class base(object):
     """base array handle"""
     def __init__(self, size, dtype):
         self.size = size
         self.dtype = dtype
+        for i, (s,m) in enumerate(vcache):
+            if s == size:
+                self.mmap = m
+                vcache.pop(i)
+#                print "create (hit)", self
+                return
         self.mmap = mmap.mmap(-1, size*dtype.itemsize)
+#        print "create (miss)", self
     def __str__(self):
-        return "<base memory at %s"%self.mmap
+        return "<base memory at %s>"%self.mmap
+    def __del__(self):
+#        print "del", self
+        if len(vcache) < VCACHE_SIZE:
+            vcache.append((self.size, self.mmap))
 
 class view(object):
     """array view handle"""
@@ -50,16 +65,21 @@ def get_data_pointer(ary, allocate=False, nullify=False):
 #    print "get_data_pointer", type(ary.ndarray.base),
     return ary.ndarray.ctypes.data
 
+#t_ufunc = 0
+
 def ufunc(op, *args):
     """Apply the 'op' on args, which is the output followed by one or two inputs"""
-
+#    global t_ufunc
 #    print "ufunc: %s "%op.info['name'], [(x.base if isinstance(x, base) else x)  for x in args]
     args = views2numpy(args)
+#    t1 = time.time()
     if op.info['name'] == "identity":
         exec "args[0][...] = args[1][...]"
     else:
         f = eval("np.%s"%op.info['name'])
         f(*args[1:], out=args[0])
+#    t2 = time.time()
+#    t_ufunc += t2-t1
 
 def reduce(op, out, a, axis):
     """reduce 'axis' dimension of 'a' and write the result to out"""
@@ -95,12 +115,19 @@ def extmethod(name, out, in1, in2):
 
 def range(size, dtype):
     """create a new array containing the values [0:size["""
-
+    raise NotImplementedError()
     return np.arange((size,), dtype=dtype)
 
 def random123(size, start_index, key):
     """Create a new random array using the random123 algorithm.
     The dtype is uint64 always."""
-
+    raise NotImplementedError()
     return np.random.random(size)
 
+
+import atexit
+@atexit.register
+def shutdown():
+#    print "ufunc:", t_ufunc
+#    print "vcache size at exit: %d"%len(vcache)
+    pass
