093ed1721354ba89cfdfbaeed621ef10c85a16c4
Troels Blum <troels@blum.dk>
2014-07-03 17:08:05 +0200

    bhpy: Random made more NumPy-like, implementer get_state, set_state, tomaxint

--- bridge/bhpy/random123.pyx
+++ bridge/bhpy/random123.pyx
@@ -10,6 +10,7 @@ import numpy
 import operator
 import datetime
 import os
+import sys
 import backend
 
 from libc.stdint cimport uint64_t, uint32_t
@@ -33,72 +34,231 @@ cdef union key_t:
 cdef extern from "Python.h": 
     void* PyLong_AsVoidPtr(object)
 
-def random123(shape, uint64_t start_index, uint32_t key, bohrium=True):
+cdef class RandomState:
     """
-    New array of uniform pseudo numbers based on the random123 philox2x32 algorithm.
-    NB: dtype is np.uint64 always
+    RandomState(seed=None)
+
+    Container for the Random123 pseudo-random number generator.
+
+    `RandomState` exposes a number of methods for generating random numbers
+    drawn from a variety of probability distributions. In addition to the
+    distribution-specific arguments, each method takes a keyword argument
+    `size` that defaults to ``None``. If `size` is ``None``, then a single
+    value is generated and returned. If `size` is an integer, then a 1-D
+    array filled with generated values is returned. If `size` is a tuple,
+    then an array with that shape is filled and returned.
 
     Parameters
     ----------
-    shape       : tuple of ints
-                  Defines the shape of the returned array of random floats.
-    start_index : The start index (must be positive)
-    key         : The key or seed for the random123 algorithm
+    seed : int, optional
+        Random seed initializing the pseudo-random number generator.
+        Can be an integer or ``None`` (the default).
+        If `seed` is ``None``, then `RandomState` will try to read data from
+        ``/dev/urandom`` (or the Windows analogue) if available or seed from
+        the clock otherwise.
 
-    Returns
-    -------
-    out : Array of uniform pseudo numbers
     """
-    cdef ctr_t ctr 
-    cdef ctr_t rnd
-    cdef key_t k
-    cdef uint64_t* array_data
-    cdef long length
-    cdef long i
+    cdef uint32_t key
+    cdef uint64_t index
 
-    ctr.ul = start_index
-    k.ui = key
-
-    if shape is None:
-        rnd.c = philox2x32(ctr.c,k.k)
-        return rnd.ul
-    elif bohrium is False:
-        array = np.empty(shape,dtype=np.uint64,bohrium=False)
-        length = array.size
-        array_data = <uint64_t *> PyLong_AsVoidPtr(array.ctypes.data)
-        for i from 0 <= i < length:
-            rnd.c = philox2x32(ctr.c,k.k)
-            array_data[i] = rnd.ul
-            ctr.ul += 1
-        return array
-    else:
-        totalsize = numpy.multiply.reduce(numpy.asarray(shape))
-        bhc_obj = backend.random123(totalsize, start_index, key)
-        ret = np.ndarray.new((totalsize,), np.uint64, bhc_obj)
-        return ret
-
-class Random:
     def __init__(self, seed=None):
         self.seed(seed)
 
-    def seed(self, x=None):
+    def seed(self, seed=None):
         """
-        Initialize the random number generator object. Optional argument x
-        can be any hashable object. If x is omitted or None, current
-        system time is used; current system time is also used to initialize
-        the generator when the module is first imported. If randomness
-        sources are provided by the operating system, they are used instead
-        of the system time (see the os.urandom() function for details on
-        availability).
+        seed(seed=None)
+
+        Seed the generator.
+
+        This method is called when `RandomState` is initialized. It can be
+        called again to re-seed the generator. For details, see `RandomState`.
+
+        Parameters
+        ----------
+        seed : int or array_like, optional
+            Seed for `RandomState`.
+
+        See Also
+        --------
+        RandomState
+
         """
-        if x is None:
+        if seed is None:
             try:
                 self.key = numpy.uint32(hash(os.urandom(8)))
             except NotImplementedError:
                 self.key = numpy.uint32(hash(datetime.datetime.now()))
         else:
-            self.key = numpy.uint32(x)
+            self.key = numpy.uint32(seed)
         self.index = 0;
+
+    def get_state(self):
+        """
+        get_state()
+
+        Return a tuple representing the internal state of the generator.
+
+        For more details, see `set_state`.
+
+        Returns
+        -------
+        out : tuple(str, np.uint64, np.uint32)
+            The returned tuple has the following items:
+
+            1. the string 'Random123'.
+            2. an integer ``index``.
+            3. an integer ``key``.
+
+
+        See Also
+        --------
+        set_state
+
+        Notes
+        -----
+        `set_state` and `get_state` are not needed to work with any of the
+        random distributions in Bohrium. If the internal state is manually altered,
+        the user should know exactly what he/she is doing.
+
+        """
+        return ('Random123', self.index, self.key)
+
+    def set_state(self, state):
+        """
+        set_state(state)
+
+        Set the internal state of the generator from a tuple.
+
+        For use if one has reason to manually (re-)set the internal state of the
+        "Mersenne Twister"[1]_ pseudo-random number generating algorithm.
+
+        Parameters
+        ----------
+        state : tuple(str, np.uint64, np.uint32)
+            The returned tuple has the following items:
+
+            1. the string 'Random123'.
+            2. an integer ``index``.
+            3. an integer ``key``.
+
+        Returns
+        -------
+        out : None
+            Returns 'None' on success.
+
+        See Also
+        --------
+        get_state
+
+        Notes
+        -----
+        `set_state` and `get_state` are not needed to work with any of the
+        random distributions in Bohrium. If the internal state is manually altered,
+        the user should know exactly what he/she is doing.
+        """
+        if len(state) != 3:
+            raise ValueError("state must contain 3 elements")
+        if state[0] != 'Random123':
+            raise ValueError("algorithm must be 'Random123'")
+        try:
+            self.index = state[1]
+            self.key = state[2]
+        except TypeError:
+            raise ValueError("state is not a valid Random123 state")
+            
+
+    def random123(self, size=None, bohrium=True):
+        """
+        New array of uniform pseudo numbers based on the random123 philox2x32 algorithm.
+        NB: dtype is np.uint64 always
+        
+        Parameters
+        ----------
+        shape       : tuple of ints
+        Defines the shape of the returned array of random floats.
+        
+        Returns
+        -------
+        out : Array of uniform pseudo numbers
+        """
+        cdef ctr_t ctr 
+        cdef ctr_t rnd
+        cdef key_t key
+        cdef uint64_t* array_data
+        cdef long length
+        cdef long i
+        
+        ctr.ul = self.index
+        key.ui = self.key
+        
+        if size is None:
+            length = 1
+            rnd.c = philox2x32(ctr.c,key.k)
+            ret = rnd.ul
+        elif bohrium is False:
+            ret = np.empty(size,dtype=np.uint64,bohrium=False)
+            length = ret.size
+            array_data = <uint64_t *> PyLong_AsVoidPtr(ret.ctypes.data)
+            for i from 0 <= i < length:
+                rnd.c = philox2x32(ctr.c,key.k)
+                array_data[i] = rnd.ul
+                ctr.ul += 1
+        else:
+            length = numpy.multiply.reduce(numpy.asarray(size))
+            bhc_obj = backend.random123(length, self.index, self.key)
+            ret = np.ndarray.new((length,), np.uint64, bhc_obj).reshape(size)
+        self.index += length
+        return ret
+
+    def tomaxint(self, size=None, bohrium=True):
+        """
+        tomaxint(size=None, bohrium=True)
+
+        Random integers between 0 and ``sys.maxint``, inclusive.
+
+        Return a sample of uniformly distributed random integers in the interval
+        [0, ``sys.maxint``].
+
+        Parameters
+        ----------
+        size : tuple of ints, int, optional
+            Shape of output.  If this is, for example, (m,n,k), m*n*k samples
+            are generated.  If no shape is specified, a single sample is
+            returned.
+
+        Returns
+        -------
+        out : ndarray
+            Drawn samples, with shape `size`.
+
+        See Also
+        --------
+        randint : Uniform sampling over a given half-open interval of integers.
+        random_integers : Uniform sampling over a given closed interval of
+            integers.
+
+        Examples
+        --------
+        >>> RS = np.random.mtrand.RandomState() # need a RandomState object
+        >>> RS.tomaxint((2,2,2))
+        array([[[1170048599, 1600360186],
+                [ 739731006, 1947757578]],
+               [[1871712945,  752307660],
+                [1601631370, 1479324245]]])
+        >>> import sys
+        >>> sys.maxint
+        2147483647
+        >>> RS.tomaxint((2,2,2)) < sys.maxint
+        array([[[ True,  True],
+                [ True,  True]],
+               [[ True,  True],
+                [ True,  True]]], dtype=bool)
+
+        """
+        r_uint = self.random123(size,bohrium) >> (64 - sys.maxint.bit_length())
+        res = np.empty_like(r_uint, dtype=int)
+        res[...] = r_uint
+        return res 
 
     def random_sample(self, shape=None, dtype=np.float64, bohrium=True):
         """
@@ -141,21 +301,22 @@ class Random:
         if not (np.dtype(dtype).type is np.float64 or np.dtype(dtype).type is np.float32):
             raise ValueError("dtype not supported for random_sample")
         #Generate random numbers as uint
-        r_int = random123(shape, self.index, self.key, bohrium=bohrium)
+        r_uint = self.random123(shape, bohrium=bohrium)
         #Convert random numbers to float in the interval [0.0, 1.0).
         max_value = numpy.dtype(dtype).type(numpy.iinfo(numpy.uint64).max)
         if shape is None:
-            self.index += 1
-            return numpy.dtype(dtype).type(r_int) / max_value 
+            return numpy.dtype(dtype).type(r_uint) / max_value 
         else:
-            self.index += numpy.multiply.reduce(numpy.asarray(shape))
-            r = np.empty_like(r_int, dtype=dtype)
-            r[...] = r_int
+            r = np.empty_like(r_uint, dtype=dtype)
+            r[...] = r_uint
             r /= max_value
             return r
 
 #The default random object
-_inst = Random()
+_inst = RandomState()
 seed = _inst.seed
+get_state = _inst.get_state
+set_state = _inst.set_state
 random_sample = _inst.random_sample
 ranf = random = sample = random_sample
+
