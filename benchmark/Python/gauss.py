c69f35c1fd59d271c6cf6cb44ba43ba7aee56417
Simon A. F. Lund <safl@safl.dk>
2014-07-16 18:20:24 -0700

    benchmarks-python:  Fixed gauss...

--- benchmark/Python/gauss.py
+++ benchmark/Python/gauss.py
@@ -1,17 +1,17 @@
 import util
 if util.Benchmark().bohrium:
     import bohrium as np
-    import bohrium.linalg as la
 else:
     import numpy as np
-    import numpy.linalg as la
+
+import bohrium.linalg as la
 
 if __name__ == "__main__":
 
     B = util.Benchmark()
     N = B.size[0]
 
-    a = np.random.random((N, N), dtype=B.dtype)
+    a = np.array(np.random.random((N, N)), dtype=B.dtype)
 
     B.start()
     la.gauss(a)
