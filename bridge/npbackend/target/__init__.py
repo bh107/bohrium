c43639cfc3291a30b24d5df051e88088682cf605
Simon A. F. Lund <safl@safl.dk>
2014-11-04 20:58:04 -0800

    npbackend: Documented npbackend target initialization (formerly known as npbackend backend initialization) And added environment var NPBE_TARGET to conceptually match naming.

--- bridge/npbackend/target/__init__.py
+++ bridge/npbackend/target/__init__.py
@@ -1,15 +1,48 @@
+"""
+Initialization of npbackend target.
+
+The target chosen is controlled by setting one of the following 
+environment vars:
+
+    * BHPY_BACKEND
+    * BHPY_TARGET
+    * NPBE_TARGET
+
+The envvars are searched in the order above, first match wins.
+
+The envvar can be one of the following:
+
+    * bhc
+    * numpy
+    * numexpr
+    * pygpu
+    * chapel
+
+If no envvar is set, the default target "bhc" is used.
+"""
 import os
 
-mth = ['Base', 'View', 'get_data_pointer', 'set_bhc_data_from_ary', \
-       'ufunc', 'reduce', 'accumulate', 'extmethod', 'range', 'random123']
+DEFAULT_TARGET = "bhc"
+TARGETS = ["bhc", "numpy", "numexpr", "pygpu", "chapel"]
+METHODS = [
+    'Base', 'View', 'get_data_pointer', 'set_bhc_data_from_ary',
+    'ufunc', 'reduce', 'accumulate', 'extmethod', 'range', 'random123'
+]
 
-b = os.getenv('BHPY_BACKEND')
-if not b:
-    b = os.getenv('BHPY_TARGET', "bhc")
+TARGET = None               # Check for environment override of default target
+for env_var in ['BHPY_BACKEND', 'BHPY_TARGET', 'NPBE_TARGET']:
+    TARGET = os.getenv(env_var)
+    if TARGET:
+        break
+TARGET = TARGET if TARGET else DEFAULT_TARGET
 
-cmd = "from .target_%s import %s"%(b, mth[0])
-for m in mth[1:]:
-    cmd += ",%s"%m
+if TARGET not in TARGETS:   # Validate the target
+    msg = "Unsupported npbackend target: %s. Use one of: %s." % (
+        TARGET,
+        ", ".join(TARGETS)
+    )
+    raise RuntimeError(msg)
 
-exec(cmd)
+# Do the actual import of methods from the chosen target
+exec("from .target_%s import %s" % (TARGET, ", ".join(METHODS)))
 
