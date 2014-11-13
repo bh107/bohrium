d26551d2e51dde100d14a1ccc71ce97b0a04dbb5
Mads R. B. Kristensen <madsbk@gmail.com>
2014-11-13 10:55:58 +0100

    npbacked: changed the "-m bohrium.as_numpy" command line argument to "-m bohrium"

--- bridge/npbackend/__main__.py
+++ bridge/npbackend/__main__.py
@@ -19,5 +19,5 @@ else:
 if len(sys.argv) > 0:
     execfile(sys.argv[0])
 else:
-    print('ERR: the "-m bohrium.as_numpy" does not support interactive mode');
+    print('ERR: the "-m bohrium" does not support interactive mode');
 
