6e99d6cba512c56ffa7da6ddf1a6f2709ead6b4c
Simon A. F. Lund <safl@safl.dk>
2015-02-02 14:10:17 +0100

    cpu: Changed scheduling.
    It seemed like the default scheduling was not behaving as expected.
    Seems loop-count / nthreads is divided regardless of the increment.
    Trying to fix this by changing the chunk-size to match the inner-most shape.

--- ve/cpu/tools/bug_hunting.sh
+++ ve/cpu/tools/bug_hunting.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 make purge_krn install
-BH_VE_CPU_JIT_DUMPSRC=1 BH_VE_CPU_JIT_FUSION=1 python ~/bohrium/test/numpy/numpytest.py
-BH_VE_CPU_JIT_DUMPSRC=1 BH_VE_CPU_JIT_FUSION=0 python ~/bohrium/test/numpy/numpytest.py
+BH_VE_CPU_JIT_DUMPSRC=1 BH_VE_CPU_JIT_FUSION=1 BH_VE_CPU_BIND=0 python ~/bohrium/test/numpy/numpytest.py
+BH_VE_CPU_JIT_DUMPSRC=1 BH_VE_CPU_JIT_FUSION=0 BH_VE_CPU_BIND=0 python ~/bohrium/test/numpy/numpytest.py
 
 # This fails when contracting as hinted by bh_ir_kernel temps
 #python ~/bohrium/test/numpy/numpytest.py --test=test_largedim --test=test_shallow_water --test=test_different_inputs
