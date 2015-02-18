f1817a6551e25dee1ffaf690b279c09ae8f86e7b
Simon A. F. Lund <safl@safl.dk>
2015-02-18 15:47:55 +0100

    cpu: Tools...

--- ve/cpu/tools/gentest.py
+++ ve/cpu/tools/gentest.py
@@ -2,11 +2,26 @@
 import bohrium as np
 
 #a = np.arange(0, n)
-def stuff(n):
+def test_range(n):
+    a = np.ones(n)
+    b = a
+    c = a
+    
+    return ((b+c)*b)/c
+
+def test_random(n):
+    a = np.random(n)
+    b = a[::2]
+    c = a[::2]
+    
+    return ((b+c)*b)/c
+
+def test_ones(n):
     a = np.ones(n)
     b = a[::2]
     c = a[::2]
     
     return ((b+c)*b)/c
 
-print(stuff(20))
+if __name__ == "__main__":
+    print(test_range(20))
