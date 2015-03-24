c746ebd2e796a42e1c789a322d48db4c5ed3b5eb
Simon A. F. Lund <safl@safl.dk>
2015-03-24 16:37:36 +0100

    filter-complete-reduction: First draft of the filter.

--- filter/complete_reduction/filter.cpp
+++ filter/complete_reduction/filter.cpp
@@ -25,37 +25,123 @@ using namespace std;
 typedef vector<bh_instruction> ilist;
 typedef ilist::iterator ilist_iter;
 
+void rewrite_chain(vector<bh_instruction*>& links, bh_instruction* first, bh_instruction* last)
+{
+    // Rewrite the first reduction as a "COMPLETE" REDUCE.
+    // Copy the meta-data of the SCALAR output from the last REDUCE
+    first->operand[0] = last->operand[0];
+
+    // Set the last reduction to NONE, it no longer needs execution.
+    last->opcode = BH_NONE;
+
+    // Set all the instructions "links" in the chain as BH_NONE
+    // they no longer need execution.
+    for(vector<bh_instruction*>::iterator rit=links.begin();
+        rit!=links.end();
+        ++rit) {
+        bh_instruction& rinstr = **rit;
+        rinstr.opcode = BH_NONE;
+    }
+}
+
 void filter(bh_ir &bhir)
 {
+    bh_base* reduce_output = NULL;
+    bh_opcode reduce_opcode = BH_NONE;
+    bh_instruction* first;
+    bh_instruction* last;
+
+    vector<bh_instruction*> links;  // Instructions in the chain that are not,
+                                    // the first and the last reduction.
+
     for(ilist_iter it = bhir.instr_list.begin();
         it!=bhir.instr_list.end();
         ++it) {
         bh_instruction& instr = *it;
-        switch(instr.opcode) {
-            case BH_ADD_REDUCE:
-            case BH_MULTIPLY_REDUCE:
-            case BH_MINIMUM_REDUCE:
-            case BH_MAXIMUM_REDUCE:
-            case BH_LOGICAL_AND_REDUCE:
-            case BH_BITWISE_AND_REDUCE:
-            case BH_LOGICAL_OR_REDUCE:
-            case BH_BITWISE_OR_REDUCE:
-            case BH_LOGICAL_XOR_REDUCE:
-            case BH_BITWISE_XOR_REDUCE:
-                printf("Reduction...");
-                break;
 
-            case BH_FREE:
-                printf("Free...");
-                break;
+        // Find the "first" reduction in a reduction chain.
+        if ((reduce_output == NULL) and \
+            (bh_opcode_is_reduction(instr.opcode))) {
 
-            case BH_DISCARD:
-                printf("Discard...");
-                break;
+            reduce_output = instr.operand[0].base;
+            reduce_opcode = instr.opcode;
 
-            default:
-                printf("Something else.");
-                break;
+            first = &instr;
+
+            printf("Beginning the chain...\n");
+
+        // A potential continuation of the chain
+        } else if ( (reduce_output != NULL) and \
+                    (reduce_opcode == instr.opcode) and \
+                    (reduce_output == instr.operand[1].base)) {
+
+            bool other_use=false, gets_freed=false, gets_discarded=false;
+            for(ilist_iter rit(it+1); rit!=bhir.instr_list.end(); ++rit) {
+                bh_instruction& other_instr = *rit;
+                switch(other_instr.opcode) {
+                    case BH_FREE:
+                        if (other_instr.operand[0].base == reduce_output) {
+                            gets_freed = true;
+                            links.push_back(&other_instr);
+                        }
+                        break;
+                    case BH_DISCARD:
+                        if (other_instr.operand[0].base == reduce_output) {
+                            gets_discarded = true;
+
+                            links.push_back(&other_instr);
+                        }
+                        break;
+                    default:
+                        for(int oidx=0; oidx<bh_operands(other_instr.opcode); ++oidx) {
+                            if (bh_is_constant(&other_instr.operand[oidx])) {
+                                continue;
+                            }
+                            if (other_instr.operand[oidx].base == reduce_output) {
+                                other_use = true;
+                            }
+                        }
+                        break;
+                }
+                // Can stop looking further if it gets used by something else
+                // or if it gets freed and discarded.
+                if (other_use or (gets_freed and gets_discarded)) {
+                    break;
+                }
+            }
+            
+            bool is_continuation = gets_freed and gets_discarded and not other_use;
+            bool is_scalar = (instr.operand[0].ndim == 1) and (instr.operand[0].shape[0] == 1);
+
+            reduce_output = instr.operand[0].base;
+
+            if (is_continuation and is_scalar) {            // End of the chain
+
+                printf("Ending the chain and REWRITE as COMPLETE REDUCE\n");
+                last = &instr;
+
+                rewrite_chain(links, first, last);
+
+            } else if (is_continuation and not is_scalar) { // Continuation
+                printf("Continuing the chain...\n");
+                links.push_back(&instr);
+            } else {                                        // Break the chain.
+                printf("Break the chain.\n");
+            }
+
+            if (not is_continuation or is_scalar) {         // Reset the search
+                printf("Resetting search.\n");
+                reduce_output = NULL;
+                reduce_opcode = BH_NONE;
+                links.clear();
+            }
+
+        // A break
+        } else if (reduce_output != NULL) {
+            reduce_output = NULL;
+            reduce_opcode = BH_NONE;
+            links.clear();
+            printf("Breaking the chain...\n");
         }
     }
 }
