48c1b324ca993f8886677df21ccd70fa217de9d9
Simon A. F. Lund <safl@safl.dk>
2015-03-24 17:15:01 +0100

    cpu: Tool-stuff.

--- ve/cpu/tools/gentest.py
+++ ve/cpu/tools/gentest.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
 import sys
 
-def test_gauss(np,bohrium, shape):
-    a = np.arange(5, shape[0]*shape[1]+5,dtype=np.float32).reshape(shape)
-    for c in xrange(1, a.shape[0]):
-        a[c:,c-1:] = a[c:,c-1:] - (a[c:,c-1]/a[c-1,c-1:c])[:,None] * a[c-1,c-1:]
-        if bohrium:
-            np.flush(a)
-    a /= np.diagonal(a)[:,None]
-
-    return a
-
 def test_reduce(np, bohrium, shape):
 
-    return np.add.reduce(np.arange(0, shape[0]*shape[1]).reshape(shape),1)
+    return np.sum(np.ones(shape))
 
 if __name__ == "__main__":
 
+    """
     print("NUMPY")
     import numpy as np
-    print test_reduce(np, False, (10, 10))
+    print test_reduce(np, False, (10, 10, 10))
+    print test_reduce(np, False, (10, 10, 10))
+    """
 
     print("BOHRIUM")
     import bohrium as np
-    print test_reduce(np, True, (10, 10))
+    shape = (10,10,10)
+    a = np.sum(np.ones(shape))
+    b = np.sum(np.ones(shape))
+    c = np.add.reduce(np.add.reduce(np.add.reduce(np.ones(shape))))
+    d = np.sum(np.ones(shape))
+    print a,b,c,d
 
+    #print np.add.reduce(np.add.reduce(np.add.reduce(np.ones(shape))))
+
+    #t1 = np.add.reduce(np.ones(shape))
+    #t2 = np.add.reduce(t1)
+    #t3 = np.add.reduce(t2)
+    #del t1, t2
+    #print t3
