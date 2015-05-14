9bc85027a7a9599c5ffa7d5e726af74cec4fdf05
Simon A. F. Lund <safl@safl.dk>
2015-05-14 05:42:34 +0200

    ...

--- ve/cpu/tools/jazzz.py
+++ ve/cpu/tools/jazzz.py
@@ -5,9 +5,9 @@ x = np.asarray([1,1,0,-1,-1], dtype=np.complex64)
 print(x)
 print(np.sign(x))
 
-#x = np.asarray([1,1,0,-1,-1])
-#print(x)
-#print(np.sign(x))
+x = np.asarray([1,1,0,-1,-1])
+print(x)
+print(np.sign(x))
 
 #sign = np.asarray(x<np.complex64(0.0), dtype=np.complex64)-np.asarray(x>np.complex64(0.0), dtype=np.complex64)
 #print(sign)
