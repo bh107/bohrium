4fa72eff5fbbc2c8577f9385ddc9d8e7cb327953
Simon A. F. Lund <safl@safl.dk>
2015-05-20 13:57:07 +0200

    Minor fixes...

--- ve/cpu/tools/sign.py
+++ ve/cpu/tools/sign.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python
 import numpy as np
+import bohrium as bh
 
 def thesign(z):
+    bh.flush()
     x = np.real(z)
-    x_prod = x * x
-
+    xp = x*x
+    del x
     y = np.imag(z)
-    y_prod = y * y
-    xy = x_prod + y_prod
-    xy_sqrt = np.sqrt(xy)
-    
-    z_zero = z == 0
-    divisor = xy_sqrt + z_zero
-    print("z", z)
-    print("x", x)
-    print("y", y)
-    print("x*x", x_prod)
-    print("y*y", y_prod)
-    print("x+y", xy)
-    print("sqrt", xy_sqrt)
-    print("z_zero", z_zero)
-    print("divisor", divisor)
+    yp = y*y
+    del y
+    xpy = xp+yp
+    del xp
+    del yp
+    xpy_sqrt = np.sqrt(xpy)
+    del xpy
+    z0 = z == 0
+    sqrt_z0 = xpy_sqrt + z0
+    del z0
+    del xpy_sqrt
+    out = z / sqrt_z0
+    del sqrt_z0
+    bh.flush()
 
-    return z/divisor
+    return out 
+
+def sign_is(z):
+    bh.flush()
+    x = np.real(z)
+    y = np.imag(z)
+    out =  z / (np.sqrt(x*x+y*y)+(z==0))
+    bh.flush()
+    return out
 
 z = np.asarray([0+1j,0-1j,0+0j,1+0j,1+1j,1-1j,0,1+0j,-1+1j,-1-1j,-1+0j], dtype=np.complex64)
-w = np.arange(-10, 10, 1, dtype=np.float64)
-x = np.arange(-10, 10, 1, dtype=np.int64)
-#print(thesign(z))
+print(thesign(z))
+#print(sign_is(z))
 print(np.sign(z))
-print(np.sign(w))
-print(np.sign(x))
+#print(np.sign(w))
+#print(np.sign(x))
 
