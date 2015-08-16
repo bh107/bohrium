15bfaa4c621fd8c1e1d6381f55242c5991175f2c
Simon A. F. Lund <safl@safl.dk>
2015-08-16 21:42:00 +0200

    cpu/cape: Couple of accelerator fixes for mem. management.

--- ve/cpu/kp_acc_openacc.c
+++ ve/cpu/kp_acc_openacc.c
@@ -1,3 +1,11 @@
+#include <openacc.h>
+
+int kp_acc_init(kp_acc* acc) {
+    // TODO: write an actual routine
+    acc_init(acc_device_nvidia);
+    return 1;
+}
+
 void kp_acc_alloc(kp_acc* acc, kp_buffer* buffer)
 {
     if (kp_acc_allocated(acc, buffer)) {    // Buffer is already allocated on device
@@ -7,7 +15,11 @@ void kp_acc_alloc(kp_acc* acc, kp_buffer* buffer)
         fprintf(stderr, "kp_acc_alloc: Buffer is not allocated on host.\n");
         return;
     }
+
     // TODO: Call OpenACC lib
+    
+    acc->nbytes_allocated += buffer->nelem * kp_etype_nbytes(buffer->type); // House-keeping
+    kp_set_insert(acc->buffers_allocated, buffer);   
 }
 
 void kp_acc_free(kp_acc* acc, kp_buffer* buffer)
@@ -19,7 +31,12 @@ void kp_acc_free(kp_acc* acc, kp_buffer* buffer)
         fprintf(stderr, "kp_acc_free: Buffer is not allocated on host.\n");
         return;
     }
+
     // TODO: Call OpenACC lib
+
+    acc->nbytes_allocated -= buffer->nelem*kp_etype_nbytes(buffer->type); // Housekeeping
+    kp_set_erase(acc->buffers_allocated, buffer);
+    kp_set_erase(acc->buffers_pushed, buffer);
 }
 
 void kp_acc_push(kp_acc* acc, kp_buffer* buffer)
@@ -31,7 +48,10 @@ void kp_acc_push(kp_acc* acc, kp_buffer* buffer)
         fprintf(stderr, "kp_acc_push: Buffer is not allocated on device.\n");
         return;
     }
+
     // TODO: Call OpenACC lib
+
+    kp_set_insert(acc->buffers_pushed, buffer); // House-keeping
 }
 
 void kp_acc_pull(kp_acc* acc, kp_buffer* buffer)
@@ -45,6 +65,5 @@ void kp_acc_pull(kp_acc* acc, kp_buffer* buffer)
     }
 
     // TODO: Call OpenACC lib
-    
 }
 
