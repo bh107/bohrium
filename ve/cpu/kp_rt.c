781959dc611e737cd463a01955e548e6242ad6fc
Kenneth Skovhede <kenneth@hexad.dk>
2015-08-31 15:12:15 +0200

    Fixed CPU OMP mock functions to provide the inline implementations under C99 sematics (CLang)

--- ve/cpu/kp_rt.c
+++ ve/cpu/kp_rt.c
@@ -7,9 +7,9 @@
 #if defined(CAPE_WITH_THREADBINDING)
 #include <omp.h>
 #else
-inline int omp_get_max_threads() { return 1; }
-inline int omp_get_thread_num()  { return 0; }
-inline int omp_get_num_threads() { return 1; }
+static inline int omp_get_max_threads() { return 1; }
+static inline int omp_get_thread_num()  { return 0; }
+static inline int omp_get_num_threads() { return 1; }
 #endif
 
 #include "kp_rt.h"
