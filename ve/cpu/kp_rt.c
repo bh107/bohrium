df8771138731979f74e5e99bcd9452a6e2d9667b
Kenneth Skovhede <kenneth@hexad.dk>
2015-08-31 15:14:43 +0200

    Fixed a compile warning

--- ve/cpu/kp_rt.c
+++ ve/cpu/kp_rt.c
@@ -5,11 +5,13 @@
 #include <hwloc.h>
 #endif
 #if defined(CAPE_WITH_THREADBINDING)
+#if defined(_OPENMP)
 #include <omp.h>
 #else
 static inline int omp_get_max_threads() { return 1; }
 static inline int omp_get_thread_num()  { return 0; }
 static inline int omp_get_num_threads() { return 1; }
+#endif
 #endif
 
 #include "kp_rt.h"
