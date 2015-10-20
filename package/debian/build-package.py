0a0dc9b9e6fab698ccc9c7aa6e4d7769504aebc2
Mads R. B. Kristensen <madsbk@gmail.com>
2015-10-20 13:14:16 +0200

    deb: work on maintenance scripts

--- package/debian/build-package.py
+++ package/debian/build-package.py
@@ -59,8 +59,8 @@ binary-core: build
 	dpkg-gensymbols -q -pbohrium -Pdebian/core
 	dpkg-gencontrol -pbohrium -Pdebian/core
 	dpkg --build debian/core ..
-        cp -p preinst debian/core/DEBIAN/
-        cp -p prerm debian/core/DEBIAN/
+	cp -p preinst debian/core/DEBIAN/
+	cp -p prerm debian/core/DEBIAN/
 
 binary-numcil: build
 	cd b; cmake -DCOMPONENT=bohrium-numcil -DCMAKE_INSTALL_PREFIX=../debian/numcil/usr -P cmake_install.cmake
