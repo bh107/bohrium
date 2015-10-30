2557201da32bdea9148d224f322347b869780655
Mads R. B. Kristensen <madsbk@gmail.com>
2015-10-30 13:26:05 +0100

    bhc: started implementing the new clean c bridge

---
+++ bridge/c/gen_array_operations.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import json
+import os
+from os.path import join, exists
+import argparse
+
+def main(args):
+
+    prefix = os.path.abspath(os.path.dirname(__file__))
+
+    # Let's read the opcode and type files
+    with open(join(prefix,'..','..','core','codegen','opcodes.json')) as f:
+        opcodes = json.loads(f.read())
+    with open(join(prefix,'..','cpp','codegen','element_types.json')) as f:
+        types   = json.loads(f.read())
+        type_map = {}
+        for t in types:
+            type_map[t[-1]] = {'cpp'     : t[0],
+                               'bhc'     : t[1],
+                               'name'    : t[2],
+                               'bhc_ary' : "bhc_ndarray_%s_p"%t[2]}
+
+    # Let's generate the header and implementation of all array operations
+    head = ""; impl = ""
+    for op in opcodes:
+        if op['opcode'] == "BH_RANDOM":
+            continue
+        doc = "// %s: %s\n"%(op['opcode'][3:], op['doc'])
+        doc += "// E.g. %s:\n"%(op['code'])
+        impl += doc; head += doc
+        for type_sig in op['types']:
+            for layout in op['layout']:
+                decl = "void %s bhc_%s"%(type_map[type_sig[0]]['bhc_ary'], op['opcode'][3:].lower())
+                assert len(layout) == len(type_sig)
+                for symbol, t in zip(layout,type_sig):
+                    decl += "_%s%s"%(symbol, type_map[t]['name'])
+                decl += "(%s out"%type_map[type_sig[0]]['bhc_ary']
+                for i, (symbol, t) in enumerate(zip(layout[1:], type_sig[1:])):
+                    decl += ", "
+                    if symbol in ["A", "D1"]:
+                        decl += "const %s in%d"%(type_map[t]['bhc_ary'],i+1)
+                    else:
+                        decl += "%s in%d"%(type_map[t]['bhc'],i+1)
+                decl += ")"
+                head += "DLLEXPORT %s;\n"%decl
+                impl += decl;
+                impl += "{\n"
+                impl += "\tmulti_array<%(t)s> *o = (multi_array<%(t)s> *) out;\n"%{'t':type_map[type_sig[0]]['cpp']}
+                bxx_args = "*o";
+                for i, (symbol, t) in enumerate(zip(layout[1:], type_sig[1:])):
+                    if symbol in ["A", "D1"]:
+                        impl += "\tmulti_array<%(t)s> *i%(i)d = (multi_array<%(t)s> *) in%(i)d;\n"%{'t':type_map[t]['cpp'], 'i':i+1}
+                    else:
+                        impl += "\t%(t)s i%(i)d = in%(i)d;\n"%{'t':type_map[t]['cpp'], 'i':i+1}
+                    bxx_args += ", *i%d"%(i+1);
+                impl += "\t%s(%s);\n"%(op['opcode'].lower(), bxx_args)
+
+                impl += "}\n"
+        impl += "\n\n"; head += "\n\n"
+
+    #Let's add header and footer
+    head = """/* Bohrium C Bridge: array operation functions. Auto generated! */
+
+#ifndef __BHC_ARRAY_OPERATIONS_H
+#define __BHC_ARRAY_OPERATIONS_H
+
+#include <stdint.h>
+#include "bh_type.h"
+
+#ifdef _WIN32
+#define DLLEXPORT __declspec( dllexport )
+#else
+#define DLLEXPORT
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+%s
+#ifdef __cplusplus
+}
+#endif
+#endif
+"""%head
+    impl = """/* Bohrium C Bridge: array operation functions. Auto generated! */
+
+#include <bohrium.hpp>
+#include "bhc_array_operations.h"
+
+using namespace bxx;
+
+%s
+"""%impl
+
+    #Finally, let's write the files
+    with open(join(args.output,'bhc_array_operations.h'), 'w') as f:
+        f.write(head)
+    with open(join(args.output,'bhc_array_operations.cpp'), 'w') as f:
+        f.write(impl)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description = 'Generates the array operation source files for the Bohrium C bridge.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'output',
+        help='Path to the output directory.'
+    )
+    args = parser.parse_args()
+    main(args)
---
+++ bridge/c/gen_types.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import json
+import os
+from os.path import join, exists
+import argparse
+
+def main(args):
+
+    prefix = os.path.abspath(os.path.dirname(__file__))
+
+    # Let's read the opcode and type files
+    with open(join(prefix,'..','cpp','codegen','element_types.json')) as f:
+        types   = json.loads(f.read())
+        type_map = {}
+        for t in types:
+            type_map[t[-1]] = {'cpp'     : t[0],
+                               'bhc'     : t[1],
+                               'name'    : t[2],
+                               'bhc_ary' : "bhc_ndarray_%s_p"%t[2]}
+
+    # Let's generate the header and implementation of all data types
+    head = ""; impl = ""
+    head += "//Forward declaration of array types:\n"
+    for key, val in type_map.iteritems():
+        head += "struct bhc_ndarray_%s;\n"%val['name']
+    head += "\n//Pointer shorthands:\n"
+    for key, val in type_map.iteritems():
+        head += "typedef struct bhc_ndarray_%s* %s;\n"%(val['name'], val['bhc_ary'])
+
+    impl += "//Array types:\n"
+    for key, val in type_map.iteritems():
+        impl += "struct bhc_ndarray_%s {multi_array<%s> me;};\n"%(val['name'], val['bhc'])
+
+    #Let's add header and footer
+    head = """/* Bohrium C Bridge: data types. Auto generated! */
+
+#ifndef __BHC_TYPES_H
+#define __BHC_TYPES_H
+
+#include <stdint.h>
+#include <bh_type.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+%s
+#ifdef __cplusplus
+}
+#endif
+#endif
+"""%head
+    impl = """/* Bohrium C Bridge: data types. Auto generated! */
+
+#include <bohrium.hpp>
+#include "bhc_types.h"
+
+using namespace bxx;
+
+%s
+"""%impl
+
+    #Finally, let's write the files
+    with open(join(args.output,'bhc_types.h'), 'w') as f:
+        f.write(head)
+    with open(join(args.output,'bhc_types.cpp'), 'w') as f:
+        f.write(impl)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description = 'Generates the type source files for the Bohrium C bridge.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'output',
+        help='Path to the output directory.'
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+    main(args)
