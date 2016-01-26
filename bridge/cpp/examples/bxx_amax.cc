a8850ad2f382729a2a3c8b584cc9f2313fd7e0b0
Simon A. F. Lund <safl@safl.dk>
2016-01-26 16:17:02 +0100

    bxx: Added amax(in, axis)/amin(in, axis) sugar functions and example.
    These are supported by the BH_ARG_MAXIMUM_REDUCE/BH_ARG_MINIMUM_REDUCE
    bytecodes and calls the bh_arg_maximum_reduce/bh_arg_minimum_reduce runtime functions.

---
+++ bridge/cpp/examples/bxx_amax.cc
@@ -0,0 +1,44 @@
+/*
+This file is part of Bohrium and Copyright (c) 2012 the Bohrium team:
+http://bohrium.bitbucket.org
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the
+GNU Lesser General Public License along with bohrium.
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <bxx/bohrium.hpp>
+
+#include <iostream>
+#include <complex>
+
+using namespace bxx;
+
+void compute()
+{
+    std::cout << "Hello Arg Min/Max." << std::endl;
+
+    multi_array<double> x;
+
+    x = range<double>(10);
+
+    std::cout << amin(x, 0) << std::endl;
+    std::cout << amax(x, 0) << std::endl;
+}
+
+int main()
+{
+    compute();
+    return 0;
+}
+
