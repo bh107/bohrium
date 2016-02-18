970c02f279d6f64a0972a19665b39fe71ad8f614
Mads Ohm Larsen <mads.ohm@gmail.com>
2016-02-18 13:32:23 +0100

    Test diagonal offset and axis

--- test/python/test_views.py
+++ test/python/test_views.py
@@ -35,6 +35,51 @@ class test_diagonal(numpytest):
         exec(cmd)
         return (res,cmd)
 
+class test_diagonal_offset(numpytest):
+    def init(self):
+        for v in gen_views(4,16,6,min_ndim=2):
+            a = {}
+            self.offset = 0
+            exec(v)
+            yield (a,v)
+
+            for offset in xrange(1, a[0].shape[0]):
+                exec(v)
+                self.offset = offset
+                yield (a,v)
+
+                exec(v)
+                self.offset = -offset
+                yield (a,v)
+
+    def test_diagonal_offset(self,a):
+        cmd = "res = np.diagonal(a[0], offset=%d)" % self.offset
+        exec(cmd)
+        return (res,cmd)
+
+class test_diagonal_axis(numpytest):
+    def init(self):
+        for v in gen_views(4,16,6,min_ndim=2):
+            a = {}
+            self.axis1 = 0
+            self.axis2 = 1
+            exec(v)
+            yield (a,v)
+
+            for axis1 in xrange(a[0].ndim):
+                for axis2 in xrange(a[0].ndim):
+                    if axis1 == axis2:
+                        continue
+                    exec(v)
+                    self.axis1 = axis1
+                    self.axis2 = axis2
+                    yield (a,v)
+
+    def test_diagonal_axis(self,a):
+        cmd = "res = np.diagonal(a[0], axis1=%d, axis2=%d)" % (self.axis1, self.axis2)
+        exec(cmd)
+        return (res,cmd)
+
 class test_transpose(numpytest):
     def init(self):
         for v in gen_views(4,16,6,min_ndim=2):
