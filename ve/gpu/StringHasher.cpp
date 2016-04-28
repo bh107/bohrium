d67c67182a7298f14efe4732c339bde1b9d000ee
Mads Ohm Larsen <mads.ohm@gmail.com>
2016-04-28 15:36:17 +0200

    [gpu] Add missing include for stringhasher

--- ve/gpu/StringHasher.cpp
+++ ve/gpu/StringHasher.cpp
@@ -3,8 +3,8 @@ This file is part of Bohrium and copyright (c) 2012 the Bohrium
 team <http://www.bh107.org>.
 
 Bohrium is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3
 of the License, or (at your option) any later version.
 
 Bohrium is distributed in the hope that it will be useful,
@@ -12,13 +12,14 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the 
-GNU Lesser General Public License along with Bohrium. 
+You should have received a copy of the
+GNU Lesser General Public License along with Bohrium.
 
 If not, see <http://www.gnu.org/licenses/>.
 */
- 
+
 #include "StringHasher.hpp"
+#include <bh_type.h>
 
 #ifdef _WIN64
 #define HAS_CPP_HASH 0
@@ -34,7 +35,7 @@ If not, see <http://www.gnu.org/licenses/>.
 
 #if HAS_CPP_HASH > 0
 #include <functional>
-std::hash<std::string> hash_fn; 
+std::hash<std::string> hash_fn;
 size_t string_hasher(std::string str)
 {
     return hash_fn(str);
