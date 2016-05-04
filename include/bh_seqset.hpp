a4e43a592add3e984fec0a817a48660d3c42c722
Mads R. B. Kristensen <madsbk@gmail.com>
2016-05-04 19:34:37 +0200

    added Header include lock to seqset

--- include/bh_seqset.hpp
+++ include/bh_seqset.hpp
@@ -1,8 +1,31 @@
+/*
+This file is part of Bohrium and copyright (c) 2012 the Bohrium
+team <http://www.bh107.org>.
+
+Bohrium is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3
+of the License, or (at your option) any later version.
+
+Bohrium is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the
+GNU Lesser General Public License along with Bohrium.
+
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __BH_SEQSET_HPP
+#define __BH_SEQSET_HPP
+
 #include <map>
-#include <stdexcept> 
+#include <stdexcept>
 
 /* Container class that works like a set (i.e. all objects are unique)
- * while also maintaining the insertion order. 
+ * while also maintaining the insertion order.
  */
 template <typename T>
 class seqset
@@ -14,7 +37,7 @@ private:
 public:
     seqset() : maxid(0) {}
 
-    // Clear the containor 
+    // Clear the containor
     void clear()
     {
         maxid = 0;
@@ -22,7 +45,7 @@ public:
         _rmap.clear();
     }
 
-    // Insert an object 
+    // Insert an object
     std::pair<size_t,bool> insert(const T &v)
     {
         auto it = _map.find(v);
@@ -32,8 +55,8 @@ public:
             _map.insert(std::make_pair(v,id));
             _rmap.insert(std::make_pair(id,v));
             return std::make_pair(id,true);
-            
-        } else {  
+
+        } else {
             return  std::make_pair(it->second,false);
         }
     }
@@ -51,7 +74,7 @@ public:
     }
 
     // Get the id of an object
-    size_t operator[] (const T &v) const 
+    size_t operator[] (const T &v) const
     {
         auto it = _map.find(v);
         if (it == _map.end())
@@ -60,7 +83,7 @@ public:
     }
 
     // Get an object given the id
-    const T& operator[] (size_t id) const 
+    const T& operator[] (size_t id) const
     {
         auto it = _rmap.find(id);
         if (it == _rmap.end())
@@ -72,8 +95,8 @@ public:
     {
         return _rmap.cbegin();
     }
-    
-    typename std::map<size_t, T>::const_iterator end() const 
+
+    typename std::map<size_t, T>::const_iterator end() const
     {
         return _rmap.cend();
     }
@@ -82,7 +105,7 @@ public:
     {
         return _map.size();
     }
-    
+
     std::set<T> set() const
     {
         std::set<T> res;
@@ -92,4 +115,4 @@ public:
     }
 };
 
-
+#endif
