ac1fae79873276317f559f1f8342d0aba0a0db9d
Mads R. B. Kristensen <madsbk@gmail.com>
2016-05-23 10:29:42 +0200

    Added xenial deb package

--- package/debian/build-package.py
+++ package/debian/build-package.py
@@ -134,7 +134,7 @@ gacutil -u NumCIL.Unsafe
 exit 0
 """
 
-UBUNTU_RELEASES = ['trusty', 'wily']
+UBUNTU_RELEASES = ['trusty', 'wily', 'xenial']
 
 
 SRC = path.join(path.dirname(os.path.realpath(__file__)),"..","..")
