965d9991d84e31e389ff2ac9ebfdd3cbb3c1e240
Mads R. B. Kristensen <madsbk@gmail.com>
2016-06-24 10:35:42 +0200

    test: now using the sign function as in NumPy

--- test/python/test_sign.py
+++ test/python/test_sign.py
@@ -1,16 +1,6 @@
 import bohrium as np
 from numpytest import numpytest, TYPES
 
-sign_str="""
-def sign(a):
-    if np.bhary.check(a):
-        return np.sign(a)
-    else:
-        s = np.absolute(a)
-        s += s == 0
-        return a/s
-"""
-
 class test_sign(numpytest):
 
     def init(self):
@@ -92,6 +82,6 @@ class test_csign_mixed(numpytest):
             yield (a, cmd)
 
     def test_sign(self,a):
-        cmd = sign_str+"res = sign(a[0])"
+        cmd = "res = np.sign(a[0])"
         exec(cmd)
         return (res, cmd)
