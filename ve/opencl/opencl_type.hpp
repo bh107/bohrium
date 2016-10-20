69cae9db816eaa6f7ef1bacd5ff2d1ea9970f86e
Mads R. B. Kristensen <madsbk@gmail.com>
2016-10-20 14:41:25 +0200

    opencl: added boolean

--- ve/opencl/opencl_type.hpp
+++ ve/opencl/opencl_type.hpp
@@ -21,35 +21,16 @@ If not, see <http://www.gnu.org/licenses/>.
 #ifndef __OPENCL_DTYPE_HPP
 #define __OPENCL_DTYPE_HPP
 
-#include <bh_type.h>
+#include <cassert>
 
-// Return OpenCL API types, which are used at the host (not inside the JIT kernels)
-const char* write_opencl_api_type(bh_type dtype)
-{
-    switch (dtype)
-    {
-        case BH_INT8: return "cl_char";
-        case BH_INT16: return "cl_short";
-        case BH_INT32: return "cl_int";
-        case BH_INT64: return"cl_long";
-        case BH_UINT8: return "cl_uchar";
-        case BH_UINT16: return "cl_ushort";
-        case BH_UINT32: return "cl_uint";
-        case BH_UINT64: return "cl_ulong";
-        case BH_FLOAT32: return "cl_float";
-        case BH_FLOAT64: return "cl_double";
-        case BH_COMPLEX64: return "cl_float2";
-        case BH_COMPLEX128: return "cl_double2";
-        case BH_R123: return "cl_ulong2";
-        default: throw std::runtime_error("Unknown OCLtype");
-    }
-}
+#include <bh_type.h>
 
 // Return OpenCL API types, which are used inside the JIT kernels
 const char* write_opencl_type(bh_type dtype)
 {
     switch (dtype)
     {
+        case BH_BOOL: return "bool";
         case BH_INT8: return "char";
         case BH_INT16: return "short";
         case BH_INT32: return "int";
@@ -63,7 +44,9 @@ const char* write_opencl_type(bh_type dtype)
         case BH_COMPLEX64: return "float2";
         case BH_COMPLEX128: return "double2";
         case BH_R123: return "ulong2";
-        default: throw std::runtime_error("Unknown OCLtype");
+        default:
+            std::cerr << "Unknown OpenCL type: " << bh_type_text(dtype) << std::endl;
+            throw std::runtime_error("Unknown OpenCL type");
     }
 }
 
