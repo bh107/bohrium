f76f73e94d058c46501970343bda71dd5f296c76
Mads R. B. Kristensen <madsbk@gmail.com>
2016-10-31 10:52:47 +0100

    jitk: implemented merge_block()

--- core/jitk/fuser.cpp
+++ core/jitk/fuser.cpp
@@ -111,6 +111,33 @@ vector<Block> fuser_singleton(vector<bh_instruction> &instr_list, const set<bh_i
     return block_list;
 }
 
+// Merges the two blocks 'a' and 'a' (in that order)
+pair<Block, bool> block_merge(const Block &a, const Block &b, const set<bh_instruction*> &news) {
+
+    // First we check for data incompatibility
+    if (b.isInstr() or not data_parallel_compatible(a, b) or sweeps_accessed_by_block(a._sweeps, b)) {
+        return make_pair(Block(), false);
+    }
+
+    // Check for perfect match, which is directly mergeable
+    if (a.size == b.size) {
+        return make_pair(merge(a, b), true);
+    }
+    // Check fusibility of reshapable blocks
+    if (b._reshapable && b.size % a.size == 0) {
+        vector<bh_instruction *> cur_instr = a.getAllInstr();
+        vector<bh_instruction *> it_instr = b.getAllInstr();
+        cur_instr.insert(cur_instr.end(), it_instr.begin(), it_instr.end());
+        return make_pair(create_nested_block(cur_instr, b.rank, a.size, news), true);
+    }
+    if (a._reshapable && a.size % b.size == 0) {
+        vector<bh_instruction *> cur_instr = a.getAllInstr();
+        vector<bh_instruction *> it_instr = b.getAllInstr();
+        cur_instr.insert(cur_instr.end(), it_instr.begin(), it_instr.end());
+        return make_pair(create_nested_block(cur_instr, a.rank, b.size, news), true);
+    }
+    return make_pair(Block(), false);
+}
 
 vector<Block> fuser_serial(vector<Block> &block_list, const set<bh_instruction*> &news) {
     vector<Block> ret;
@@ -123,41 +150,12 @@ vector<Block> fuser_serial(vector<Block> &block_list, const set<bh_instruction*>
         }
         // Let's search for fusible blocks
         for (; it != block_list.end(); ++it) {
-            if (it->isInstr())
-                break;
-            if (not data_parallel_compatible(cur, *it))
-                break;
-            if (sweeps_accessed_by_block(cur._sweeps, *it))
-                break;
-            assert(cur.rank == it->rank);
-
-            // Check for perfect match, which is directly mergeable
-            if (cur.size == it->size) {
-                cur = merge(cur, *it);
-                continue;
+            const pair<Block, bool> res = block_merge(cur, *it, news);
+            if (res.second) {
+                cur = res.first;
+            } else {
+                break; // We couldn't find any shape match
             }
-            // Check fusibility of reshapable blocks
-            if (it->_reshapable && it->size % cur.size == 0) {
-                vector<bh_instruction *> cur_instr = cur.getAllInstr();
-                vector<bh_instruction *> it_instr = it->getAllInstr();
-                cur_instr.insert(cur_instr.end(), it_instr.begin(), it_instr.end());
-                Block b = create_nested_block(cur_instr, it->rank, cur.size, news);
-                assert(b.size == cur.size);
-                cur = b;
-                continue;
-            }
-            if (cur._reshapable && cur.size % it->size == 0) {
-                vector<bh_instruction *> cur_instr = cur.getAllInstr();
-                vector<bh_instruction *> it_instr = it->getAllInstr();
-                cur_instr.insert(cur_instr.end(), it_instr.begin(), it_instr.end());
-                Block b = create_nested_block(cur_instr, cur.rank, it->size, news);
-                assert(b.size == it->size);
-                cur = b;
-                continue;
-            }
-
-            // We couldn't find any shape match
-            break;
         }
         // Let's fuse at the next rank level
         cur._block_list = fuser_serial(cur._block_list, news);
