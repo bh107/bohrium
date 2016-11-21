54888cbe9b3fb47a3650a0d99983030c705bec08
Mads R. B. Kristensen <madsbk@gmail.com>
2016-11-21 14:31:47 +0100

    opencl: added a const

--- ve/opencl/main.cpp
+++ ve/opencl/main.cpp
@@ -316,7 +316,7 @@ void Impl::write_kernel(const Kernel &kernel, BaseDB &base_ids, const vector<con
         for (unsigned int i=0; i < threaded_blocks.size(); ++i) {
             const LoopB *b = threaded_blocks[i];
             spaces(ss, 4);
-            ss << write_opencl_type(BH_UINT64) << " i" << b->rank << " = get_global_id(" << i << "); " \
+            ss << "const " << write_opencl_type(BH_UINT64) << " i" << b->rank << " = get_global_id(" << i << "); " \
                << "if (i" << b->rank << " >= " << b->size << ") {return;} // Prevent overflow" << endl;
         }
         ss << endl;
