3f21b3b10eb57b9c28088a59f3b71ee67f30417d
Mads R. B. Kristensen <madsbk@gmail.com>
2016-12-14 16:46:32 +0100

    npbackend: execfile() now in python3

--- bridge/npbackend/__main__.py
+++ bridge/npbackend/__main__.py
@@ -7,16 +7,29 @@ import sys
 import os
 import numpy
 import bohrium
+
+def get_execfile():
+    """execfile() does not exist in Python 3"""
+    try:
+        import runpy
+        return runpy.run_path
+    except ImportError:
+        return execfile
+
+# numpy becomes bohrium
 sys.modules['numpy_force'] = numpy
 sys.modules['numpy'] = bohrium
 
+# Set the module search path to the dir of the script
 sys.argv.pop(0)
 if len(sys.argv) > 0:
     sys.path[0] = os.path.dirname(os.path.abspath(sys.argv[0]))
 else:
     sys.path[0] = ""
+
+# Let's run the script
 if len(sys.argv) > 0:
-    execfile(sys.argv[0])
+    get_execfile()(sys.argv[0])
 else:
     print ('ERR: the "-m bohrium" does not support interactive mode')
 
