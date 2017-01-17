f95891dbd3adcdcfdca75dead472216b73c2933a
Mads R. B. Kristensen <madsbk@gmail.com>
2017-01-17 10:18:38 +0100

    test: added test of a.sum() with axis

--- test/python/tests/test_reduce.py
+++ test/python/tests/test_reduce.py
@@ -23,16 +23,17 @@ class test_reduce_sum:
         for cmd, shape in util.gen_random_arrays("R", 3, dtype="np.float32"):
             cmd = "R = bh.random.RandomState(42); a = %s; " % cmd
             for op in ["sum", "prod"]:
-                yield (cmd, op)
+                for axis in range(len(shape)):
+                    yield (cmd, op, axis)
 
     def test_func(self, arg):
-        (cmd, op) = arg
-        cmd += "res = M.%s(a)" % op
+        (cmd, op, axis) = arg
+        cmd += "res = M.%s(a, axis=%d)" % (op, axis)
         return cmd
 
     def test_method(self, arg):
-        (cmd, op) = arg
-        cmd += "res = a.%s()" % op
+        (cmd, op, axis) = arg
+        cmd += "res = a.%s(axis=%d)" % (op, axis)
         return cmd
 
 
