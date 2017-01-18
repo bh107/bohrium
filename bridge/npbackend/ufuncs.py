f62566cbac81ee3032714677bead244b372a7ac3
Mads R. B. Kristensen <madsbk@gmail.com>
2017-01-18 13:33:59 +0100

    Fixed #166

--- bridge/npbackend/ufuncs.py
+++ bridge/npbackend/ufuncs.py
@@ -63,6 +63,11 @@ def setitem(ary, loc, value):
     if not isinstance(loc, tuple):
         loc = (loc,)
 
+    # Let's try to convert non-arrays and npn-scalars to an array
+    # e.g. converting a python list to an array
+    if not (bhary.check(value) or np.isscalar(value)):
+        value = array_create.array(value)
+
     #Lets make sure that not all dimensions are indexed by integers
     loc = list(loc)
     if len(loc) == ary.ndim and all((np.isscalar(s) for s in loc)):
---
+++ test/python/tests/test_assignment.py
@@ -0,0 +1,20 @@
+import util
+
+
+class test_assign_vector:
+    def init(self):
+        yield "res = M.arange(100).reshape(10,10); "
+
+    def test_row(self, cmd):
+        return cmd + "res[2, :] = M.ones(10)"
+
+    def test_column(self, cmd):
+        return cmd + "res[:, 2] = M.ones(10)"
+
+    def test_row_list(self, cmd):
+        return cmd + "res[2, :] = [42]*10"
+
+    def test_column_list(self, cmd):
+        return cmd + "res[:, 2] = [42]*10"
+
+
