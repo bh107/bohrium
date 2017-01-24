9bb2e8720395c18981a15eaf1cf2e8c188e9cf20
Mads Ohm Larsen <mads.ohm@gmail.com>
2017-01-24 14:08:53 +0100

    Workaround for redefining macros on Mac

--- ve/opencl/cl.hpp
+++ ve/opencl/cl.hpp
@@ -25,12 +25,21 @@ If not, see <http://www.gnu.org/licenses/>.
 
 #define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 #if defined(__APPLE__) || defined(__MACOSX)
-#include <OpenCL/opencl.h>
-#define CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
-#define CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+    #include <OpenCL/opencl.h>
+
+    // To avoid warnings from compiler about redefining needed macros
+    #ifdef CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+        #undef CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+        #define CL_EXT_PREFIX__VERSION_1_1_DEPRECATED
+    #endif
+    #ifdef CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+        #undef CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+        #define CL_EXT_SUFFIX__VERSION_1_1_DEPRECATED
+    #endif
 #else
-#include <CL/cl.h>
+    #include <CL/cl.h>
 #endif
+
 #undef CL_VERSION_1_2
 #define __CL_ENABLE_EXCEPTIONS
 
