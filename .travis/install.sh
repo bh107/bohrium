320e917a78fadd975f915e7e9ad70d633e24b83c
Mads Ohm Larsen <mads.ohm@gmail.com>
2017-01-24 11:22:15 +0100

    Test for OSX on Travis

---
+++ .travis/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -x
+set -e
+
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+  ################
+  #   MAC OS X   #
+  ################
+  # Fetch 'benchpress'
+  mkdir -p $HOME/benchpress
+  wget -nv https://github.com/bh107/benchpress/archive/master.zip -O $HOME/benchpress/master.zip
+  unzip -q $HOME/benchpress/master.zip
+
+  export PATH="$HOME/benchpress/benchpress-master/bin:$PATH"
+  export DYLD_LIBRARY_PATH="/usr/lib:$HOME/.local/lib:$DYLD_LIBRARY_PATH"
+  export PYTHONPATH="$HOME/benchpress/benchpress-master/module:$PYTHONPATH"
+  export PYTHONPATH="$HOME/.local/lib/$PYTHON_EXEC/site-packages:$PYTHONPATH"
+
+  # Build 'bohrium'
+  cmake . -DCMAKE_BUILD_TYPE=Debug -DUSE_WERROR=ON -DEXT_VISUALIZER=OFF
+  make install
+else
+  #############
+  #   LINUX   #
+  #############
+  echo "Nothing special for 'linux'."
+  echo "We just run everything in docker."
+fi
