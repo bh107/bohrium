f5e7873a2059aa3b81b545656948db97c957b792
Mads Ohm Larsen <mads.ohm@gmail.com>
2017-01-27 11:06:22 +0100

    Copy if not contiguous

--- bridge/npbackend/blas.py
+++ bridge/npbackend/blas.py
@@ -10,12 +10,23 @@ from sys import stderr
 from . import ufuncs, bhary, array_create
 
 def gemm(a, b, c=None, alpha=1.0, beta=0.0):
+    if not (a.ndim == 2 and b.ndim == 2):
+        stderr.write("[ext] Matrices need to be two-dimensional.\n")
+        return None
+
     if a.shape[1] != b.shape[0]:
         stderr.write("[ext] Wrong shape of matrices: first argument has shape {} and second has shape {}.\n".format(a.shape, b.shape))
         return None
 
+    if not a.flags['C_CONTIGUOUS']:
+        a = a.copy()
+    if not b.flags['C_CONTIGUOUS']:
+        b = b.copy()
+
     if c is None:
         c = np.empty(shape=(a.shape[0], b.shape[1]), dtype=a.dtype)
+    elif not c.flags['C_CONTIGUOUS']:
+        c = c.copy()
 
     if alpha != 1.0:
         a = a * alpha
