f2366798e5d89122919ff97d7bae43b7a9a47d15
Mads Ohm Larsen <mads.ohm@gmail.com>
2017-02-08 15:04:28 +0100

    :lipstick: Clean up

--- bridge/npbackend/blas.py
+++ bridge/npbackend/blas.py
@@ -1,27 +1,30 @@
 """
 Basic Linear Algebra Subprograms (BLAS)
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 Utilize BLAS directly from Python
 """
 
 import bohrium as np
 from sys import stderr
-from . import ufuncs, bhary, array_create
+from . import ufuncs
 
-def gemm(a, b, c=None, alpha=1.0, beta=0.0):
-    if not (a.ndim == 2 and b.ndim == 2):
-        stderr.write("[ext] Matrices need to be two-dimensional.\n")
-        return None
+def __blas(name, a, b, alpha=1.0, c=None, beta=0.0):
+    if not b is None:
+        if not (a.ndim == 2 and b.ndim == 2):
+            stderr.write("[ext] Matrices need to be two-dimensional.\n")
+            return None
 
-    if a.shape[1] != b.shape[0]:
-        stderr.write("[ext] Wrong shape of matrices: first argument has shape {} and second has shape {}.\n".format(a.shape, b.shape))
-        return None
+        if a.shape[1] != b.shape[0]:
+            stderr.write("[ext] Wrong shape of matrices: first argument has shape {} and second has shape {}.\n".format(a.shape, b.shape))
+            return None
+
+        if not b.flags['C_CONTIGUOUS']:
+            b = b.copy()
+    else:
+        b = np.empty(shape=(a.shape[0], a.shape[1]), dtype=a.dtype)
 
     if not a.flags['C_CONTIGUOUS']:
         a = a.copy()
-    if not b.flags['C_CONTIGUOUS']:
-        b = b.copy()
 
     if c is None:
         c = np.empty(shape=(a.shape[0], b.shape[1]), dtype=a.dtype)
@@ -34,5 +37,10 @@ def gemm(a, b, c=None, alpha=1.0, beta=0.0):
     if beta != 0.0:
         c = c * beta
 
-    ufuncs.extmethod("blas_gemm", c, a, b)
+    ufuncs.extmethod(name, c, a, b) # modifies 'c'
     return c
+
+# All of A, B, and C are used
+def gemm(a, b, alpha=1.0, c=None, beta=0.0):
+    """ C := alpha * A * B + beta * C """
+    return __blas("blas_gemm", a, b, alpha, c, beta)
--- test/python/tests/test_ext_blas.py
+++ test/python/tests/test_ext_blas.py
@@ -2,27 +2,30 @@ import util
 import bohrium as bh
 import bohrium.blas
 
-class test_ext_blas:
+def has_ext():
+    try:
+        a = bh.arange(4).astype(bh.float64).reshape(2, 2)
+        bh.blas.gemm(a, a)
+        return True
+    except Exception as e:
+        print("\n[ext] Cannot test BLAS extension methods.")
+        print(e)
+        return False
+
+
+class test_ext_blas_identical:
     def init(self):
-        try:
-            a = bh.arange(4).astype(bh.float64).reshape(2, 2)
-            bh.blas.gemm(a, a)
-        except Exception as e:
-            print("\n[ext] Cannot test BLAS extension methods.")
-            print(e)
+        if not has_ext():
             return
 
         for t in util.TYPES.FLOAT + util.TYPES.COMPLEX:
-            rows = 5
-            cols = 5
-            for r in range(1, rows):
-                for c in range(1, cols):
-                    cmd  = "a = M.arange(%d, dtype=%s).reshape(%s); " % (r*c, t, (r, c))
-                    cmd += "b = M.arange(%d, dtype=%s).reshape(%s); " % (c*r, t, (c, r))
-                    yield cmd
+            for r in range(1, 20):
+                cmd  = "a = M.arange(%d, dtype=%s).reshape(%s); " % (r*r, t, (r, r))
+                cmd += "b = M.arange(%d, dtype=%s).reshape(%s); " % (r*r, t, (r, r))
+                yield cmd, t
 
-    def test_gemm(self, cmd):
-        # Tests that 'bh.blas.gemm(a, b)' calculates the same as 'np.dot(a, b)'
+    def test_gemm(self, args):
+        cmd, _ = args
         cmd_np = cmd + "res = np.dot(a, b);"
         cmd_bh = cmd + "res = bh.blas.gemm(a, b);"
         return cmd_np, cmd_bh
