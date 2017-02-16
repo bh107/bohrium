f8539739085a8e1f55391cf14a410385fae88f2c
Mads R. B. Kristensen <madsbk@gmail.com>
2017-02-16 13:45:12 +0100

    conda: written the recipe for Bohrium

--- package/conda/bohrium/build.sh
+++ package/conda/bohrium/build.sh
@@ -2,14 +2,41 @@
 
 mkdir build
 cd build
-pwd
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBRIDGE_CIL=OFF -DVEM_PROXY=OFF -DEXT_BLAS=OFF -DEXT_CLBLAS=OFF -DVE_OPENCL=OFF -DEXT_VISUALIZER=OFF -DBoost_USE_STATIC_LIBS=ON -DCORE_LINK_FLAGS="-static-libgcc -static-libstdc++" -DCMAKE_INSTALL_PREFIX=$PREFIX
+export 
+ls $CONDA_PREFIX
+ls $CONDA_PREFIX/lib
+ls $PREFIX
+ls $PREFIX/lib
+ls $CONDA_PREFIX
+ls
+echo "Build Boost"
+wget https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz
+tar -xzf boost_1_63_0.tar.gz
+cd boost_1_63_0
+bash $RECIPE_DIR/build_boost.sh $PWD/../boost
+cd ..
+ls $PWD/boost
 
-make VERBOSE=0
+cmake --version
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+	 -DBRIDGE_CIL=OFF \
+	 -DVEM_PROXY=OFF \
+	 -DEXT_BLAS=OFF \
+ 	 -DEXT_CLBLAS=OFF \
+	 -DVE_OPENCL=OFF \
+	 -DEXT_VISUALIZER=OFF \
+         -DBoost_NO_SYSTEM_PATHS=ON \
+	 -DBOOST_ROOT=$PWD/boost \
+	 -DBoost_USE_STATIC_LIBS=ON \
+	 -DCORE_LINK_FLAGS="-static-libgcc -static-libstdc++" \
+	 -DFORCE_CONFIG_PATH=$PREFIX/etc/bohrium \
+	 -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+make VERBOSE=1
 make install
 
 # Set the OpenMP compiler command to "cc"
-sed -ie 's/\/usr\/local\/bin\/cc/cc/g' $PREFIX/etc/bohrium/config.ini
+#sed -ie 's/compiler_cmd =.*$/compiler_cmd = cc/g' $PREFIX/etc/bohrium/config.ini
 
 # Turn off the OpenMP SIMD flag
 sed -ie 's/-fopenmp-simd//g' $PREFIX/etc/bohrium/config.ini
