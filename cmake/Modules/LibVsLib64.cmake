ea80d2dbc2245abee08890f0dc0884999f55c2e6
Mads R. B. Kristensen <madsbk@gmail.com>
2017-02-27 14:34:39 +0100

    cmake: LIBDIR in a string

--- cmake/Modules/LibVsLib64.cmake
+++ cmake/Modules/LibVsLib64.cmake
@@ -11,5 +11,4 @@ else ()
   set (_libdir_def "lib")
 endif()
 
-set (LIBDIR "${_libdir_def}" CACHE PATH "Library Directory")
-mark_as_advanced (LIBDIR)
+set (LIBDIR "${_libdir_def}" CACHE STRING "Name of library directory (not the path)")
