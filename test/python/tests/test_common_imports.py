3288495182fc35fe4401181f83891de5324c9626
Mads R. B. Kristensen <madsbk@gmail.com>
2017-03-03 16:31:14 +0100

    test: now testing import and use of common libraries

---
+++ test/python/tests/test_common_imports.py
@@ -0,0 +1,37 @@
+
+class _test_scipy:
+    def init(self):
+        yield ("")
+
+    def test_scpy(self, arg):
+        cmd = "import scipy; res = M.ones(10)"
+        return cmd
+
+    def test_sparse(self, arg):
+        cmd = "import scipy.sparse; res = M.ones(10)"
+        return cmd
+
+try:
+    import scipy
+    test_scipy = _test_scipy
+except ImportError:
+    print("SciPy not found, ignoring some tests")
+
+
+class _test_matplotlib:
+    def init(self):
+        yield ("")
+
+    def test_matplotlib(self, arg):
+        cmd = "import matplotlib; res = M.ones(10)"
+        return cmd
+
+    def test_pyplot(self, arg):
+        cmd = "import matplotlib.pyplot as plt; res = M.ones(10).reshape((2,5)); plt.imshow(res)"
+        return cmd
+
+try:
+    import matplotlib
+    test_matplotlib = _test_matplotlib
+except ImportError:
+    print("Matplotlib not found, ignoring some tests")
