3649aaf5ea50cd03d2d889dca5c7cce4621c28b1
Dion Häfner <mail@dionhaefner.de>
2017-03-28 19:06:23 +0200

    Fix CG solver
    To fix #310, I made some changes to the CG solver:
    
    * Instead of looping over the first axis of the RHS vector, we check for convergence in every time step (due to roundoff errors, convergence is not guaranteed after one full loop).
    * I didn't understand how BLAS's ``gemmt`` was used here - as far as I get it, ``gemmt`` is used for matrix-matrix multiplications, and it only updates a triangular part of the resulting matrix. Since we are doing matrix-vector and vector-vector products here, I have replaced it with ``np.dot`` (which also seems to be faster in my crude benchmarks). I don't mean to step on anyone's toes here, though - it might very well be that using ``gemmt`` makes perfect sense and I just didn't see the deeper reason behind it. In that case, we should switch it back.
    * I have added the desired solver tolerance as a keyword argument, and set it to the same value as the ``scipy.sparse.cg`` solver.
    
    With this implementation, I achieve the same precision as the SciPy solver, but it is considerably slower than the SciPy (Fortran) version.

--- bridge/npbackend/cg.py
+++ bridge/npbackend/cg.py
@@ -4,14 +4,12 @@ Conjugate Gradient (CG) solver
 """
 
 import bohrium as np
-import bohrium.blas as blas
-import bohrium.linalg as LA
 
 from sys import stderr
 from . import ufuncs
 
 # Implemented as example MATLAB code from https://en.wikipedia.org/wiki/Conjugate_gradient_method
-def cg(A, b, x=None):
+def cg(A, b, x=None, tol=1e-5):
     # If no guess is given, set an empty guess
     if x is None:
         x = np.zeros(shape=b.shape, dtype=b.dtype)
@@ -21,20 +19,20 @@ def cg(A, b, x=None):
     rsold = np.zeros(shape=(1,1), dtype=b.dtype)
     rsnew = np.zeros(shape=(1,1), dtype=b.dtype)
 
-    r = b - np.matmul(A, x)
-    p = r;
-    rsold = blas.gemmt(r, r, c=rsold)
-
-    for _ in range(b.shape[0]):
-        Ap = np.matmul(A, p)
-        alpha = rsold / blas.gemmt(p, Ap, c=alpha)
-
+    r = b - np.dot(A, x)
+    p = r.copy()
+    r_squared = r * r
+    rsold = np.sum(r_squared)
+    
+    tol_squared = tol * tol
+    while np.max(r_squared) > tol_squared:
+        Ap = np.dot(A, p)
+        alpha = rsold / np.dot(p, Ap)
+        
         x = x + alpha * p
         r = r - alpha * Ap
-        rsnew = blas.gemmt(r, r, c=rsnew)
-
-        if LA.norm(rsnew) < 1e-10:
-            return x
+        r_squared = r * r
+        rsnew = np.sum(r_squared)
 
         p = r + (rsnew / rsold) * p
         rsold = rsnew
