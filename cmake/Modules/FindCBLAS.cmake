f9a380ef2142eaa0dfaadc5ca4faab8932a45d35
Dion Häfner <mail@dionhaefner.de>
2017-04-20 02:08:18 +0200

    [cmake] no symbol checking for apple accelerate library

--- cmake/Modules/FindCBLAS.cmake
+++ cmake/Modules/FindCBLAS.cmake
@@ -8,14 +8,24 @@
 include(FindPackageHandleStandardArgs)
 include(CheckLibraryExists)
 
-function(FIND_AND_CHECK_CBLAS _libname _includename _symbol)
+function(FIND_AND_CHECK_CBLAS _libname _includename)
   find_path(_CBLAS_INCLUDES ${_includename})
   find_library(_CBLAS_LIBRARIES NAMES ${_libname})
 
-  # check if cblas symbol is present
+  if(ARGV2) # check for optional argument
+    set(_symbol ${ARGV2})
+  endif(ARGV2)
+
   if(_CBLAS_LIBRARIES AND _CBLAS_INCLUDES)
-    get_filename_component(_LIB_PATH ${_CBLAS_LIBRARIES} DIRECTORY)
-    check_library_exists(${_libname} ${_symbol} ${_LIB_PATH} _HAVE_CBLAS_SYMBOL)
+    if(_symbol)
+      # check if given symbol is present
+      get_filename_component(_LIB_PATH ${_CBLAS_LIBRARIES} DIRECTORY)
+      check_library_exists(${_libname} ${_symbol} ${_LIB_PATH} _HAVE_CBLAS_SYMBOL)
+      unset(_LIB_PATH CACHE)
+    else()
+      set(_HAVE_CBLAS_SYMBOL True)
+    endif()
+
     if(_HAVE_CBLAS_SYMBOL)
       set(CBLAS_LIBRARIES ${_CBLAS_LIBRARIES} CACHE FILEPATH "Path to CBLAS library")
       set(CBLAS_INCLUDES ${_CBLAS_INCLUDES} CACHE PATH "Path to CBLAS include directory")
@@ -23,29 +33,32 @@ function(FIND_AND_CHECK_CBLAS _libname _includename _symbol)
   endif(_CBLAS_LIBRARIES AND _CBLAS_INCLUDES)
 
   # reset variables
+  unset(_HAVE_CBLAS_SYMBOL CACHE)
   unset(_CBLAS_INCLUDES CACHE)
   unset(_CBLAS_LIBRARIES CACHE)
 endfunction(FIND_AND_CHECK_CBLAS)
 
 
 # check Apple CBLAS
-if(NOT CBLAS_LIBRARIES)
-  FIND_AND_CHECK_CBLAS("Accelerate" "Accelerate.h" cblas_sgemm)
-endif(NOT CBLAS_LIBRARIES)
+if(APPLE)
+  if(NOT CBLAS_LIBRARIES)
+    FIND_AND_CHECK_CBLAS("openblas" "cblas.h")
+  endif(NOT CBLAS_LIBRARIES)
+endif(APPLE)
 
 # OpenBLAS
 if(NOT CBLAS_LIBRARIES)
-  FIND_AND_CHECK_CBLAS("openblas" "cblas.h" cblas_sgemm)
+  FIND_AND_CHECK_CBLAS("openblas" "cblas.h" "cblas_sgemm")
 endif(NOT CBLAS_LIBRARIES)
 
 # generic cblas / ATLAS
 if(NOT CBLAS_LIBRARIES)
-  FIND_AND_CHECK_CBLAS("cblas" "cblas.h" cblas_sgemm)
+  FIND_AND_CHECK_CBLAS("cblas" "cblas.h" "cblas_sgemm")
 endif(NOT CBLAS_LIBRARIES)
 
 # generic blas
 if(NOT CBLAS_LIBRARIES)
-  FIND_AND_CHECK_CBLAS("blas" "cblas.h" cblas_sgemm)
+  FIND_AND_CHECK_CBLAS("blas" "cblas.h" "cblas_sgemm")
 endif(NOT CBLAS_LIBRARIES)
 
 
