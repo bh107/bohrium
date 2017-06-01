c6b555b9264edcf19539a55d54fda0a79621ca66
Michael F. Herbst <info@michael-herbst.com>
2017-06-01 14:43:14 +0200

    Correct typo

--- bridge/cpp/bxx/traits.hpp
+++ bridge/cpp/bxx/traits.hpp
@@ -33,7 +33,7 @@ namespace bxx {
 inline
 void assign_const_type(bh_constant* constant, T value) {
     //TODO: The general case should result in a meaningful compile-time error.
-    std::cout << "Unsupported type [%s, " << constant->type << "] " << &value << std::cout;
+    std::cout << "Unsupported type [%s, " << constant->type << "] " << &value << std::endl;
 }
 
     template <>
