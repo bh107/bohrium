1d6ec48c37e2935b6f855d3d5da5805eab082231
Mads R. B. Kristensen <madsbk@gmail.com>
2017-06-23 10:44:14 +0200

    npbackend: implemented argmin() argmax() for axis=None

--- test/python/tests/test_summations.py
+++ test/python/tests/test_summations.py
@@ -1,4 +1,7 @@
 import util
+import functools
+import operator
+
 
 class test_sums:
     """ Test reduction of sum(), prod(), any(), and all()"""
@@ -20,6 +23,24 @@ class test_sums:
 
     def test_method(self, arg):
         (cmd, op, axis) = arg
-        cmd += "res = a.%s(axis=%d)" % (op, axis)
+        if axis is None:
+            cmd += "res = a.%s()" % op
+        else:
+            cmd += "res = a.%s(axis=%d)" % (op, axis)
         return cmd
+
+
+class test_argminmax:
+    def init(self):
+        for cmd, shape in util.gen_random_arrays("R", 3, dtype="np.float32"):
+            cmd = "R = bh.random.RandomState(42); a = %s; " % cmd
+            nelem = functools.reduce(operator.mul, shape)
+            if nelem > 0:
+                yield (cmd)
+
+    def test_min(self, cmd):
+        return cmd + "res = M.argmin(a)"
+
+    def test_max(self, cmd):
+        return cmd + "res = M.argmax(a)"
 
