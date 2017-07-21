c157451654db1a58785c07e8a1f6ccfcbf78aa92
Mads R. B. Kristensen <madsbk@gmail.com>
2017-07-21 16:48:15 +0200

    get_array() now uses _numpy_wrapper()

--- bridge/npbackend/bohrium/interop_numpy.py
+++ bridge/npbackend/bohrium/interop_numpy.py
@@ -7,14 +7,8 @@ from .bhary import get_bhc
 from .target import get_data_pointer
 
 
-def get_buffer(bh_ary):
-    """Return a Buffer object wrapping the memory of the Bohrium array `ary`"""
-    mem_ptr = get_data_pointer(get_bhc(bh_ary), copy2host=True)
-    return numpy.core.multiarray.int_asbuffer(mem_ptr, bh_ary.nbytes)
-
-
 def get_array(bh_ary):
     """Return a NumPy array wrapping the memory of the Bohrium array `ary`
-       NB: Changing or deallocating `bh_ary` invalidates the return NumPy array
+       NB: Changing or deallocating `bh_ary` invalidates the returned NumPy array
     """
-    return numpy.frombuffer(get_buffer(bh_ary), bh_ary.dtype)
+    return bh_ary._numpy_wrapper()
