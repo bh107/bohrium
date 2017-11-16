c3cc919871acbfc6c280395700e4b2e5e6a99d56
Mads R. B. Kristensen <madsbk@gmail.com>
2017-11-16 22:20:54 +0100

    package: implemented a pip wheel setup

---
+++ package/pip/create_wheel.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+from distutils.dir_util import mkpath, copy_tree
+
+# To use a consistent encoding
+from codecs import open
+import os
+import re
+import argparse
+import sys
+import shutil
+import glob
+import subprocess
+
+from os.path import join
+
+# We overload the setup.py with some extra arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--bh-install-prefix',
+    help='Path to the Bohrium install prefix.'
+)
+parser.add_argument(
+    '--npbackend-dir',
+    help='Path to the Python library of Bohrium'
+)
+parser.add_argument(
+    '--config',
+    type=argparse.FileType('r'),
+    help='Path to the Bohrium config file'
+)
+(args_extra, argv) = parser.parse_known_args()
+sys.argv = [sys.argv[0]] + argv  # Write the remaining arguments back to `sys.argv` for distutils to read
+assert(args_extra.bh_install_prefix)
+assert(args_extra.npbackend_dir)
+assert(args_extra.config)
+assert("bdist_wheel" in sys.argv)
+
+args_extra.bh_install_prefix = os.path.abspath(args_extra.bh_install_prefix)
+args_extra.npbackend_dir = os.path.abspath(args_extra.npbackend_dir)
+
+
+def _script_path():
+    """Returns the path to the dir this script is in"""
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def _li1st_files(path, prefix=None, regex_include="\.so|\.ini|\.dylib"):
+    """Returns a list of filenames of the files in `path`"""
+    ret = []
+    for f in os.listdir(path):
+        if re.search(regex_include, f):
+            if prefix is not None:
+                f = prefix + f
+            ret.append(f)
+    return ret
+
+
+def _find_data_files(root_path, regex_exclude="\.pyc|\.py"):
+    """Return a list of paths relative to `root_path` of all files rooted in `root_path`"""
+    root_path = os.path.abspath(root_path)
+    ret = []
+    for root, _, filenames in os.walk(root_path):
+        for fname in filenames:
+            fullname = join(root, fname)
+            if not re.search(regex_exclude, fullname):
+                ret.append(fullname.replace("%s/" % root_path, ""))
+    return ret
+
+
+def _copy_files(glob_str, dst_dir):
+    """Copy files using the `glob_str` and copy to `dst_dir`"""
+    mkpath(dst_dir)
+    for fname in glob.glob(glob_str):
+        if os.path.isfile(fname):
+            shutil.copy(fname, dst_dir)
+            print("copy: %s => %s" % (fname, dst_dir))
+
+
+def _copy_dirs(src_dir, dst_dir):
+    """Copy dir"""
+    mkpath(dst_dir)
+    copy_tree(src_dir, dst_dir)
+    print("copy %s => %s" % (src_dir, dst_dir))
+
+
+def _regex_replace(pattern, repl, src):
+    """Replacing matches in `src` with `repl` using regex `pattern`"""
+    print ("config.ini: replacing: '%s' => '%s'" % (pattern, repl))
+    return re.sub(pattern, repl, src)
+
+
+# Copy the include dir for the JIT compilation into the Python package
+_copy_dirs(join(args_extra.bh_install_prefix, "share", "bohrium", "include"),
+           join(args_extra.npbackend_dir, "include"))
+
+
+# Copy Bohrium's shared libraries into the Python package
+_copy_files(join(args_extra.bh_install_prefix, 'lib', 'lib*'), join(args_extra.npbackend_dir, "lib"))
+
+
+# Update the RPATH of the Python extensions to look in the the `lib` dir
+for filename in glob.glob(join(args_extra.npbackend_dir, '*.so')):
+    cmd = "patchelf --set-rpath '$ORIGIN/lib' %s" % filename
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
+
+# Update the RPATH of Bohrium's shared libraries to look in the current dir
+for filename in glob.glob(join(args_extra.npbackend_dir, 'lib', '*.so')):
+    cmd = "patchelf --set-rpath '$ORIGIN' %s" % filename
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
+
+
+# Write a modified config file to the Python package dir
+_config_path = join(args_extra.npbackend_dir, "config.ini")
+_config_dir = os.path.dirname(_config_path)
+with open(_config_path, "w") as f:
+    config_str = args_extra.config.read()
+
+    # Unset the `cache_dir` option
+    config_str = _regex_replace("cache_dir = .*", "cache_dir = ", config_str)
+
+    # Compile command: replace absolute include path with a path relative to {CONF_PATH}.
+    config_str = _regex_replace("-I%s/share/bohrium/" % args_extra.bh_install_prefix, "-I{CONF_PATH}/", config_str)
+
+    # Compile command: replace absolute library path with a path relative to {CONF_PATH}.
+    config_str = _regex_replace("-L%s/" % args_extra.bh_install_prefix, "-L{CONF_PATH}/", config_str)
+
+    # Replace absolute library paths with a relative path.
+    config_str = _regex_replace("%s/" % args_extra.bh_install_prefix, "./", config_str)
+    f.write(config_str)
+    print("writing config file: %s" % f.name)
+
+
+# The version if written in the VERSION file in the root of Bohrium
+_version = "0.0.0"
+with open(join(_script_path(), "..", "..", "VERSION"), "r") as f:
+    _version = f.read().strip()
+
+# Get the long description from the README file
+#with open(os.path.join(_script_path(), '../../README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+setup(
+    name='bohrium',
+    version=_version,
+    description='Bohrium NumPy',
+ #   long_description=long_description,
+    author='The Bohrium Team',
+    author_email='contact@bh107.org',
+    url='http://www.bh107.org',
+    license='LGPLv3',
+    maintainer='Mads R. B. Kristensen',
+    maintainer_email='madsbk@gmail.com',
+    platforms=['Linux', 'OSX'],
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+
+    # What does your project relate to?
+    keywords='Benchmark, Bohrium, bh107',
+
+    package_dir={'bohrium': args_extra.npbackend_dir},
+    packages=['bohrium', 'bohrium.target', 'bohrium.nobh'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+   # extras_require={
+   #     'dev': ['check-manifest'],
+   #     'test': ['coverage'],
+   # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'bohrium': _find_data_files(args_extra.npbackend_dir)
+    },
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    #entry_points={
+    #    'console_scripts': [
+    #        'bp-run=benchpress.run:main',
+    #    ],
+    #},
+)
