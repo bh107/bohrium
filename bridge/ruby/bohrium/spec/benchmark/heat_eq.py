887f0a37391a7af41d7136860c29620bd05e5cbf
Mads Ohm Larsen <mads.ohm@gmail.com>
2018-01-23 09:40:27 -0800

    Added heat equation benchmarks

--- bridge/ruby/bohrium/spec/benchmark/heat_eq.py
+++ bridge/ruby/bohrium/spec/benchmark/heat_eq.py
@@ -1,43 +1,33 @@
 # Python version of the Heat Equation using Bohrium
-import bohrium as np
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
+import numpy as np
 
-fig = plt.figure()
-plt.xticks([])
-plt.yticks([])
+# np.set_printoptions(linewidth=200)
 
 def heat2d(height, width, epsilon=42):
     grid = np.zeros((height+2, width+2), dtype=np.float64)
-    grid[:,0]  = np.float64(-273.15)
-    grid[:,-1] = np.float64(-273.15)
-    grid[-1,:] = np.float64(-273.15)
-    grid[0,:]  = np.float64(40.0)
+    grid[:,0]  = -273.15
+    grid[:,-1] = -273.15
+    grid[-1,:] = -273.15
+    grid[0,:]  = 40.0
 
     center = grid[1:-1,1:-1]
-    north  = grid[0:-2,1:-1]
+    north  = grid[:-2,1:-1]
     east   = grid[1:-1,2:]
-    west   = grid[1:-1,0:-2]
+    west   = grid[1:-1,:-2]
     south  = grid[2:,1:-1]
 
-    delta  = epsilon+1
-    i = 0
-    # plt.imshow(grid.copy2numpy(), cmap="coolwarm")
-    # plt.draw()
+    delta = epsilon+1
     while delta > epsilon:
         tmp = 0.2 * (center + north + south + east + west)
         delta = np.sum(np.abs(tmp - center))
-        grid[1:-1,1:-1] = tmp
-        if i % 100 == 0:
-            pass
-            # plt.imshow(center.copy2numpy(), cmap="coolwarm")
-            # plt.draw()
-            # plt.pause(0.000000000001)
-        i += 1
+        center[:] = tmp
 
     return center
 
-result = heat2d(100, 100)
-# plt.show()
-# plt.imshow(result.copy2numpy(), cmap="coolwarm")
-# plt.show()
+import sys
+size = int(sys.argv[1])
+result = heat2d(size, size)
+try:
+    np.flush()
+except:
+    pass
--- bridge/ruby/bohrium/spec/benchmark/heat_eq.rb
+++ bridge/ruby/bohrium/spec/benchmark/heat_eq.rb
@@ -7,21 +7,33 @@ def heat2d(height, width, epsilon=42)
   grid[height+1,    true] = -273.15
   grid[       0,    true] = 40.0
 
-  center = grid[1..height-1, 1..width-1]
-  north  = grid[0..height-2, 1..width-1]
-  south  = grid[2..height-1, 1..width-1]
-  east   = grid[1..height-1, 0..width-2]
-  west   = grid[1..height-1, 2..width-1]
+  center = grid[1..height,   1..width]
+  north  = grid[0..height-1, 1..width]
+  south  = grid[2..height,   1..width]
+  east   = grid[1..height,   0..width-1]
+  west   = grid[1..height,   2..width]
 
   delta = epsilon+1
   while delta > epsilon
     tmp = 0.2 * (center + north + south + east + west)
-    # delta = (tmp-center).absolute.add_reduce
-    delta = (tmp-center).absolute.to_ary.flatten.reduce(:+)
-    grid[1..height-1, 1..width-1] = tmp
+    delta = (tmp-center).absolute.add_reduce(0).add_reduce(0).to_ary[0]
+    grid[1..height, 1..width] = tmp
   end
 
   center
 end
 
-result = heat2d(100, 100)
+def mprint(ary)
+  puts "["
+  ary.each do |row|
+    print "["
+    print row.map { |f| f.round(3).to_s.rjust(8) }.join(", ")
+    puts "]"
+  end
+  puts "]"
+end
+
+size = ARGV[0].to_i
+result = heat2d(size, size)
+result.to_ary
+# mprint result.to_ary
