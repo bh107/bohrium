90baff79356c8b6a2a8bdbaf0c100654d19ec0d5
Mads R. B. Kristensen <madsbk@gmail.com>
2018-02-22 15:50:26 +0100

    hash of a block now ignores the order of BH_FREEs within a block

--- core/jitk/codegen_cache.cpp
+++ core/jitk/codegen_cache.cpp
@@ -78,10 +78,22 @@ void hash_stream(const bh_instruction &instr, const SymbolTable &symbols, std::s
  */
 void hash_stream(const Block &block, const SymbolTable &symbols, std::stringstream &ss) {
     if (block.isInstr()) {
-        hash_stream(*block.getInstr(), symbols, ss);
+        if (block.getInstr()->opcode != BH_FREE) {
+            hash_stream(*block.getInstr(), symbols, ss);
+        }
     } else {
         ss << "rank: " << block.rank();
         ss << "size: " << block.getLoop().size;
+        {  // The order of BH_FREE within a block doesn't matter, thus we sort the freed base IDs here
+            ss << "freed: ";
+            set<uint64_t >sorted_freed_bases;
+            for (const bh_base *b: block.getLoop()._frees) {
+                sorted_freed_bases.insert(symbols.baseID(b));
+            }
+            for(uint64_t b_id: sorted_freed_bases) {
+                ss << b_id << ",";
+            }
+        }
         for (const Block &b: block.getLoop()._block_list) {
             hash_stream(b, symbols, ss);
         }
