cc6a3649ecfaf7f98f72d35bbf7313e7dab66ef8
Mads R. B. Kristensen <madsbk@gmail.com>
2018-03-20 20:07:38 +0100

    cmake: cython detection now uses the module
    Before it looked for the `cython` binary

--- cmake/Modules/FindCython.cmake
+++ cmake/Modules/FindCython.cmake
@@ -3,14 +3,36 @@
 # defines
 #
 # CYTHON_FOUND - system has cython
-# CYTHON_EXECUTABLE - where to find 'cython'
+# CYTHON_VERSION - cython version
 
-FIND_PROGRAM (CYTHON_EXECUTABLE cython)
 
-SET (CYTHON_FOUND FALSE CACHE INTERNAL "")
-
-if(CYTHON_EXECUTABLE)
-    SET (CYTHON_FOUND TRUE CACHE INTERNAL "")
+# Finding cython involves calling the Python interpreter
+if(CYTHON_FIND_REQUIRED)
+    find_package(PythonInterp REQUIRED)
+else()
+    find_package(PythonInterp)
 endif()
 
-MARK_AS_ADVANCED(CYTHON_EXECUTABLE)
+if(NOT PYTHONINTERP_FOUND)
+    set(CYTHON_FOUND FALSE)
+    return()
+endif()
+
+execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c" "import cython; print(cython.__version__);"
+        RESULT_VARIABLE _CYTHON_VERSION_RESULT
+        OUTPUT_VARIABLE CYTHON_VERSION
+        ERROR_VARIABLE _CYTHON_ERROR_VALUE
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+if(NOT _CYTHON_VERSION_RESULT MATCHES 0)
+    if(CYTHON_FIND_REQUIRED)
+        message(FATAL_ERROR
+                "CYTHON import failure:\n${_CYTHON_ERROR_VALUE}")
+    endif()
+    set(CYTHON_FOUND FALSE)
+    return()
+else()
+    set(CYTHON_FOUND TRUE)
+endif()
+
+find_package_message(CYTHON "Found Cython: version \"${CYTHON_VERSION}\"" "${CYTHON_VERSION}")
