4ce104c302215d01666c6e14f8c9d5b3be7ace88
Mads R. B. Kristensen <madsbk@gmail.com>
2018-05-16 21:05:01 +0200

    visualizer: fixed wrong cast

--- bridge/npbackend/bohrium/visualization.py
+++ bridge/npbackend/bohrium/visualization.py
@@ -36,9 +36,8 @@ def plot_surface(ary, mode, colormap, lowerbound, upperbound):
     else:
         raise ValueError("Unsupported mode '%s' " % mode)
 
-    if bhary.check(ary):  # Must be a Bohrium array
-        ary = array_create.array(ary)
-
+    # Must be a Bohrium array of float32
+    ary = array_create.array(ary, dtype=np.float32)
     args = array_create.array([  # Construct arguments
         np.float32(colormap),
         np.float32(flat),
@@ -46,6 +45,7 @@ def plot_surface(ary, mode, colormap, lowerbound, upperbound):
         np.float32(lowerbound),
         np.float32(upperbound)
     ],
-        bohrium=True
+        bohrium=True,
+        dtype=np.float32
     )
     ufuncs.extmethod("visualizer", ary, args, ary)  # Send to extension
