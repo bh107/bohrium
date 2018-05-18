f355c99957b4ff81af51c6030d0c5a4832812ec0
Mads R. B. Kristensen <madsbk@gmail.com>
2018-05-18 14:06:26 +0200

    npbackend: Added param argument to plot_surface()_

--- bridge/npbackend/bohrium/visualization.py
+++ bridge/npbackend/bohrium/visualization.py
@@ -5,10 +5,11 @@ Visualization
 Common functions for visualization.
 """
 import bohrium as np
-from . import ufuncs, bhary, array_create
+from . import ufuncs, array_create
+from bohrium import _bh
 
 
-def plot_surface(ary, mode, colormap, lowerbound, upperbound):
+def plot_surface(ary, mode, colormap, lowerbound, upperbound, param=None):
     mode = mode.lower()
 
     ranks = [2, 3]
@@ -36,8 +37,6 @@ def plot_surface(ary, mode, colormap, lowerbound, upperbound):
     else:
         raise ValueError("Unsupported mode '%s' " % mode)
 
-    # Must be a Bohrium array of float32
-    ary = array_create.array(ary, dtype=np.float32)
     args = array_create.array([  # Construct arguments
         np.float32(colormap),
         np.float32(flat),
@@ -48,4 +47,17 @@ def plot_surface(ary, mode, colormap, lowerbound, upperbound):
         bohrium=True,
         dtype=np.float32
     )
+
+    # When param is used, we normalize `ary` to uint8 and use `mem_copy()` to compress `ary`
+    if param is not None:
+        ary_min = ary.min()
+        ary_range = ary.max() - ary.min()
+        ary = (ary-ary_min) * 255 / ary_range
+        assert(ary.min() >= 0)
+        assert(ary.max() < 256)
+        ary = np.array(ary, dtype=np.uint8)
+        ary = _bh.mem_copy(ary, param=param)
+        ary = array_create.array(ary, dtype=np.float32)
+        ary = (ary * ary_range + ary_min) / 255.0
+
     ufuncs.extmethod("visualizer", ary, args, ary)  # Send to extension
