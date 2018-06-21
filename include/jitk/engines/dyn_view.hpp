e8610cc90ace04c7f9edad0f1e57a3c60308def6
sebastian <sebastian@skjoenberg.com>
2018-06-21 10:24:53 +0200

    Cleaned up and added grid functionality

--- include/jitk/engines/dyn_view.hpp
+++ include/jitk/engines/dyn_view.hpp
@@ -19,31 +19,55 @@ If not, see <http://www.gnu.org/licenses/>.
 */
 
 void slide_views(BhIR *bhir) {
-
     // Iterate through all instructions and slide the relevant views
     for (bh_instruction &instr : bhir->instr_list) {
         for (bh_view &view : instr.operand) {
             if (not view.slide.empty()) {
-                //                printf("WALLAAAAAAAA\n");
+                bool first_iter = view.iteration_counter == 0;
+
                 // The relevant dimension in the view is updated by the given stride
                 for (size_t i = 0; i < view.slide.size(); i++) {
                     int dim = view.slide_dim.at(i);
+
                     int dim_stride = view.slide_dim_stride.at(i);
-                    int change = view.slide.at(i)*dim_stride;
-                    int max_rel_idx = dim_stride*view.slide_dim_shape.at(i);
-                    int rel_idx = view.start%(dim_stride*view.slide_dim_shape.at(i));
-                    rel_idx += change;
 
-                    if (rel_idx < 0) {
-                        change += max_rel_idx;
-                    } else if (rel_idx >= max_rel_idx) {
-                        change -= max_rel_idx;
+                    int dim_step_delay = view.slide_dim_step_delay.at(i);
+
+                    int dim_step_delay_counter = view.slide_dim_step_delay.at(i);
+                    if (dim_step_delay == 1 ||
+                        (view.iteration_counter % dim_step_delay == dim_step_delay-1)) {
+                        int change = view.slide.at(i)*dim_stride;
+
+                        int max_rel_idx = dim_stride*view.slide_dim_shape.at(i);
+
+                        int rel_idx = view.start % (dim_stride*view.slide_dim_shape.at(i));
+
+                        auto search = view.resets.find(dim);
+
+                        rel_idx += change;
+
+                        if (rel_idx < 0) {
+                            change += max_rel_idx;
+                        } else if (rel_idx >= max_rel_idx) {
+                            change -= max_rel_idx;
+                        }
+                        view.changes_since_reset[dim] += change;
+
+                        view.start += (int64_t) change;
+                        view.shape[dim] += (int64_t) view.slide_dim_shape_change.at(i);
+
+                        if (!first_iter && search != view.resets.end() &&
+                            (view.iteration_counter / dim_step_delay) % search->second == search->second-1) {
+
+                            int64_t reset = search->second;
+
+                            view.start -= view.changes_since_reset[dim];
+                            view.changes_since_reset[dim] = 0;
+                            view.shape[dim] -= (int64_t) reset*view.slide_dim_shape_change.at(i);
+                        }
                     }
-
-                    view.start += change;
-                    printf("WAAAA %d\n", view.slide_dim_shape_change.at(i));
-                    view.shape[dim] += view.slide_dim_shape_change.at(i);
                 }
+                view.iteration_counter += 1;
             }
         }
     }
