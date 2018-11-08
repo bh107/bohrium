82f30a7ee21ea970334e7f465eaa266f69ff4bdc
Mads R. B. Kristensen <madsbk@gmail.com>
2018-11-08 14:58:18 +0100

    cmake-npbackend: now setting LD_LIBRARY_PATH

--- bridge/npbackend/build.bash
+++ bridge/npbackend/build.bash
@@ -8,15 +8,16 @@ CMAKE_BINARY_DIR=$3
 CMAKE_SOURCE_DIR=$4
 CMAKE_CURRENT_SOURCE_DIR=$5
 
-echo "===================== NPBACKEND ======================"
+export NPBACKEND_SRC_ROOT=${CMAKE_SOURCE_DIR}
+export NPBACKEND_BUILD_ROOT=${CMAKE_BINARY_DIR}
+export LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}/bridge/c:${CMAKE_BINARY_DIR}/bridge/cxx:${CMAKE_BINARY_DIR}/core:${LD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${DYLD_LIBRARY_PATH}
 
+echo "===================== NPBACKEND ======================"
 
 source ${VR_PATH}/bin/activate
 pip install numpy cython
 rm -Rf ${VR_PATH}/wheel_npbackend
-
-export NPBACKEND_SRC_ROOT=${CMAKE_SOURCE_DIR}
-export NPBACKEND_BUILD_ROOT=${CMAKE_BINARY_DIR}
 pip wheel -w ${VR_PATH}/wheel_npbackend ${CMAKE_CURRENT_SOURCE_DIR}
 WHEEL_NAME=`echo ${VR_PATH}/wheel_npbackend/*`
 echo "WHEEL_NAME: \"$WHEEL_NAME\""
