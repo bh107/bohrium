8d27936cb62662b803a524be3a41f13ae1275d8f
Mads R. B. Kristensen <madsbk@gmail.com>
2018-12-21 15:22:19 +0100

    bhc: added bhc_user_kernel()

--- bridge/c/gen_specials.py
+++ bridge/c/gen_specials.py
@@ -285,6 +285,24 @@ def main(args):
 
 """ % t
 
+    doc = "\n// Send and receive a message through the component stack\n"
+    doc += "// NB: the returned string is invalidated on the next call to bhc_user_kernel()\n"
+    impl += doc; head += doc
+    decl = "const char* bhc_user_kernel(const char* kernel, int nop, void *operands[], char* compile_cmd)"
+    head += "%s;\n" % decl
+    impl += "%s" % decl
+    impl += """
+{
+    static std::string ret_msg;
+    std::vector<bhxx::BhArrayUnTypedCore*> ops;
+    for (int i=0; i<nop; ++i) {
+        ops.push_back(static_cast<bhxx::BhArrayUnTypedCore*>(operands[i]));
+    }
+    ret_msg = bhxx::Runtime::instance().userKernel(kernel, ops, compile_cmd);
+    return ret_msg.c_str();
+}
+"""
+
     #Let's add header and footer
     head = """/* Bohrium C Bridge: special functions. Auto generated! */
 #pragma once
