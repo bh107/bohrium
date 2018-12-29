13ad467296482aad4c85b030cb71ad6a62fa8a24
Mads R. B. Kristensen <madsbk@gmail.com>
2018-12-29 16:15:30 +0100

    userKernel: added a simple test

---
+++ test/python/tests/test_user_kernel.py
@@ -0,0 +1,26 @@
+
+class test_simple:
+    def init(self):
+        yield ""
+
+    def test_add(self, _):
+        bh_cmd = '''     
+kernel = r"""
+#include <stdint.h>
+void execute(double *a, double *b, double *c) {
+    for(uint64_t i=0; i<100; ++i) {
+        c[i] = a[i] + b[i] + i;
+    }
+}
+"""
+a = bh.ones(100, bh.double)
+b = bh.ones(100, bh.double)
+res = bh.empty_like(a)
+bh.user_kernel.execute(kernel, [a, b, res])
+'''
+        np_cmd = '''
+a = np.ones(100, bh.double)
+b = np.ones(100, bh.double)
+res = a + b + np.arange(100)
+'''
+        return (np_cmd, bh_cmd)
