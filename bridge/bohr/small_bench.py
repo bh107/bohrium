4b53772905ccb40e4edeed1d54b014e7bf4b75e5
Mads R. B. Kristensen <madsbk@gmail.com>
2019-08-11 16:35:43 +0200

    Added a small benchmark

---
+++ bridge/bohr/small_bench.py
@@ -0,0 +1,44 @@
+import time
+import numpy as np
+import bohrium as bh
+import example as bohr
+
+ITER = 10000
+
+def compute(m, a, b):
+    return m.add(m.add(m.add(a,b), 42), b)
+
+def numpy_bench():
+    a = np.full((2000,), 42)
+    b = np.full((2000,), 43)
+    for _ in range(ITER):
+        compute(np, a, b)
+
+def bohr_bench():
+    a = bohr.full((2000,), 42)
+    b = bohr.full((2000,), 43)
+    for _ in range(ITER):
+        compute(bohr, a, b)
+        bohr.flush()
+
+def bh_bench():
+    a = bh.full((2000,), 42)
+    b = bh.full((2000,), 43)
+    for _ in range(ITER):
+        compute(bh, a, b)
+        bh.flush()        
+
+t1 = time.time()
+numpy_bench()
+t2 = time.time()
+print("Numpy:   ", t2-t1)
+
+t1 = time.time()
+bohr_bench()
+t2 = time.time()
+print("Bohr:    ", t2-t1)
+
+t1 = time.time()
+bh_bench()
+t2 = time.time()
+print("Bohrium: ", t2-t1)
