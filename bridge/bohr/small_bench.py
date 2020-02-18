d41ab8e43fcc3097b9e6e568ccc87ec339523840
Mads R. B. Kristensen <madsbk@gmail.com>
2020-02-18 11:04:04 +0100

    clean up

--- bridge/bohr/small_bench.py
+++ bridge/bohr/small_bench.py
@@ -4,41 +4,47 @@ import bohrium as bh
 import example as bohr
 
 ITER = 10000
+SHAPE = (10000,)
+
 
 def compute(m, a, b):
-    return m.add(m.add(m.add(a,b), 42), b)
+    return m.add(m.add(m.add(a, b), 42), b)
+
 
 def numpy_bench():
-    a = np.full((2000,), 42)
-    b = np.full((2000,), 43)
+    a = np.full(SHAPE, 42)
+    b = np.full(SHAPE, 43)
     for _ in range(ITER):
         compute(np, a, b)
 
+
 def bohr_bench():
-    a = bohr.full((2000,), 42)
-    b = bohr.full((2000,), 43)
+    a = bohr.full(SHAPE, 42)
+    b = bohr.full(SHAPE, 43)
     for _ in range(ITER):
         compute(bohr, a, b)
         bohr.flush()
 
+
 def bh_bench():
-    a = bh.full((2000,), 42)
-    b = bh.full((2000,), 43)
+    a = bh.full(SHAPE, 42)
+    b = bh.full(SHAPE, 43)
     for _ in range(ITER):
         compute(bh, a, b)
-        bh.flush()        
+        bh.flush()
+
 
 t1 = time.time()
 numpy_bench()
 t2 = time.time()
-print("Numpy:   ", t2-t1)
+print("Numpy:   ", t2 - t1)
 
 t1 = time.time()
 bohr_bench()
 t2 = time.time()
-print("Bohr:    ", t2-t1)
+print("Bohr:    ", t2 - t1)
 
 t1 = time.time()
 bh_bench()
 t2 = time.time()
-print("Bohrium: ", t2-t1)
+print("Bohrium: ", t2 - t1)
